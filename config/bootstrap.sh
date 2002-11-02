78dbc96b684bab57442e8081513c63f67bf28c43
Jim Callahan <jim@temerity.us>
2002-11-02 00:51:28 +0000

    initial revision

---
+++ config/bootstrap.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+rm -rf i686-pc-linux-gnu-dbg i686-pc-linux-gnu-opt i686-pc-linux-gnu-prof 
+mkdir  i686-pc-linux-gnu-dbg i686-pc-linux-gnu-opt i686-pc-linux-gnu-prof
+
+pushd /home/$USER/code/src/phoenix
+  ./autogen.sh
+popd
+
+pushd i686-pc-linux-gnu-dbg
+  /home/$USER/code/src/phoenix/configure \
+    --with-compiler=GNU \
+    --disable-opt \
+    --with-base=/base \
+    --with-plserver=linuxserv1
+popd
+
+pushd i686-pc-linux-gnu-opt
+  /home/$USER/code/src/phoenix/configure \
+    --with-compiler=GNU \
+    --enable-opt \
+    --with-base=/base \
+    --with-plserver=linuxserv1
+popd
+
+pushd i686-pc-linux-gnu-prof
+  /home/$USER/code/src/phoenix/configure \
+    --with-compiler=GNU \
+    --disable-opt \
+    --enable-prof \
+    --with-base=/base \
+    --with-plserver=linuxserv1
+popd
