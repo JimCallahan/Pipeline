f836ddc9d539a563ff5a3f7e7e1f30dc01728ca8
Jim Callahan <jim@temerity.us>
2002-12-14 02:57:44 +0000

    removed --verbose option to automake

--- autogen.sh
+++ autogen.sh
@@ -7,7 +7,7 @@ echo "Building config header."
 autoheader
 
 echo "Building makefiles."
-automake --verbose --add-missing --copy
+automake --add-missing --copy
 
 echo "Building configure."
 autoconf
