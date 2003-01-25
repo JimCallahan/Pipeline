8f99e94ec1f8870667158441eae46166027f00c9
Jim Callahan <jim@temerity.us>
2003-01-25 01:35:04 +0000

    added MD5 support

--- src/utils/src/PlPut.cc
+++ src/utils/src/PlPut.cc
@@ -1,4 +1,4 @@
-// $Id: PlPut.cc,v 1.3 2003/01/10 17:53:36 jim Exp $
+// $Id: PlPut.cc,v 1.4 2003/01/25 01:35:04 jim Exp $
 
 #ifdef HAVE_CONFIG_H
 #  include "config.h"
@@ -17,16 +17,35 @@
   using std::ofstream;
 #endif
 
-/* REQUIRED by configure */ 
-#include <list> 
-#include <set>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <unistd.h>
-#include <errno.h>
-/* REQUIRED by configure */ 
+#ifdef HAVE_LIST
+#  include <list>
+#endif
 
-#include <FB.hh>
+#ifdef HAVE_SET
+#  include <set>
+#endif
+
+#ifdef HAVE_SYS_TYPES_H
+#  include <sys/types.h>
+#endif
+
+#ifdef HAVE_SYS_STAT_H
+#  include <sys/stat.h>
+#endif
+
+#ifdef HAVE_UNISTD_H
+#  include <unistd.h>
+#endif
+
+#ifdef HAVE_ERRNO_H
+#  include <errno.h>
+#endif
+
+#ifdef HAVE_LIBGEN_H
+#  include <libgen.h>
+#endif
+
+#include <PackageInfo.hh>
 
 using namespace Phoenix;
 using namespace Phoenix::Core;
@@ -51,6 +70,9 @@ public:
   {
     assert(uWork);
     assert(uRepo);
+
+    uWorkMd5 = md5path(uWork);
+    uRepoMd5 = md5path(uRepo);
   }
 
   ~PathPair()
@@ -60,11 +82,42 @@ public:
 
     assert(uRepo);
     delete[] uRepo;
+
+    assert(uWorkMd5);
+    delete[] uWorkMd5;
+
+    assert(uRepoMd5);
+    delete[] uRepoMd5;
+  }
+
+protected:
+  /* generate paths to the MD5 checksum file from the file path */ 
+  char* 
+  md5path
+  (
+   const char* path
+  ) 
+  {
+    char* tmp1 = strdup(path);
+    char* tmp2 = strdup(path);
+    char* dir  = dirname(tmp1);
+    char* file = basename(tmp2);
+
+    char md5[1024];
+    sprintf(md5, "%s/.md5sum/%s", dir, file);
+
+    delete[] tmp1;
+    delete[] tmp2;
+
+    return strdup(md5);
   }
 
 public:
-  const char* uWork;
-  const char* uRepo;
+  const char* uWork;          /* absolute path to working area file */ 
+  const char* uRepo;          /* absolute path to repository file */ 
+
+  const char* uWorkMd5;       /* absolute path to working area MD5 checksum file */ 
+  const char* uRepoMd5;       /* absolute path to repository MD5 checksum file */
 };
 
 
@@ -81,18 +134,109 @@ struct StringCmp
 };
 
 
+
+/* usage message */ 
 void
 usage()
 {
-  cerr << "usage: plput [--verbose] listfile\n"
-       << "       plput --help\n"
-       << "       plput --html-help\n"
-       << "       plput --version\n"
-       << "       plput --release\n";
+  std::cerr << "usage: plput [--verbose] [--md5] listfile\n"
+	    << "       plput --help\n"
+	    << "       plput --html-help\n"
+	    << "       plput --version\n"
+	    << "       plput --release-date\n"
+	    << "       plput --copyright\n" 
+	    << std::flush;
 }
 
 
+/* make sure repository file does not exist (as a file or directory) */ 
+void
+checkRepoFiles
+(
+ list<PathPair*>& pairs,    /* IN: list of work/repo pairs */ 
+ bool md5                         /* IN: check MD5 checksums as well? */ 
+)
+{
+  char msg[1024];
 
+  list<PathPair*>::iterator iter;
+  for(iter=pairs.begin(); iter != pairs.end(); iter++) {
+    const char* repo = (*iter)->uRepo;
+    FB::stageMsg(repo);
+    
+    struct stat buf;
+    if(stat(repo, &buf) == 0) {
+      if(S_ISDIR(buf.st_mode)) {
+	sprintf(msg, "bad path, directory exists with the name: %s", repo);
+	FB::error(msg);	    
+      }
+      else {
+	sprintf(msg, "attempted to overwrite repository file: %s", repo);
+	FB::error(msg);
+      }
+    }
+    
+    if(md5) {
+      const char* md5 = (*iter)->uRepoMd5;
+      if(stat(md5, &buf) == 0) {
+	if(S_ISDIR(buf.st_mode)) {
+	  sprintf(msg, "bad path, directory exists with the name: %s", md5);
+	  FB::error(msg);	    
+	}
+	else {
+	  sprintf(msg, "attempted to overwrite repository MD5 file: %s", md5);
+	  FB::error(msg);
+	}
+      }
+    }
+  }
+}
+
+
+/* copy a single file to the repository, 
+     returns false if copy failed */ 
+bool
+copyFileToRepo
+(
+ const char* work,           /* IN: working area file */ 
+ const char* repo,           /* IN: repository file */ 
+ list<const char*>& copied   /* IN/OUT: list of succesfully copied files */ 
+)
+{
+  char msg[1024];
+
+  /* open work file */ 
+  ifstream in(work);
+  if(!in) {
+    sprintf(msg, "unable to open working file: %s", work);
+    FB::warn(msg);
+    return false;
+  }
+  
+  /* create/open repository file */ 
+  ofstream out(repo, ios::out | ios::noreplace, 0444);
+  if(!in) {
+    sprintf(msg, "unable to open repository file: %s", repo);
+    FB::warn(msg);
+    return false;
+  }
+  
+  /* copy it byte-by-byte */   
+  out << in.rdbuf(); 
+  
+  /* close the files */ 
+  in.close();
+  out.close();
+  
+  /* success! */ 
+  sprintf(msg, "copied: %s to %s", work, repo);
+  FB::stageMsg(msg);
+
+  return true;
+}
+
+
+  
 void
 main
 (
@@ -105,31 +249,50 @@ main
   FB::setWarnings(false);
   FB::setStageStats(false);
 
+  bool md5 = false;
   if(argc == 2) {
     if(strcmp(argv[1], "--help") == 0) {
       usage();
       exit(EXIT_SUCCESS);
     }
     else if(strcmp(argv[1], "--html-help") == 0) {
+      char buf[1024];
+      sprintf(buf, "openURL(file:%s/plput.html, new-window)", Pipeline::PackageInfo::sDocsDir); 
 
-      exit(EXIT_SUCCESS);
+      char* args[4]; 
+      args[0] = strdup("mozilla");
+      args[1] = strdup("-remote");
+      args[2] = strdup(buf);
+      args[3] = NULL;
+
+      execv(Pipeline::PackageInfo::sMozilla, args);
     }
     else if(strcmp(argv[1], "--version") == 0) {
-
+      std::cerr << Pipeline::PackageInfo::sVersion << "\n";
       exit(EXIT_SUCCESS);
     }
-    else if(strcmp(argv[1], "--release") == 0) {
-
+    else if(strcmp(argv[1], "--release-date") == 0) {
+      std::cerr << Pipeline::PackageInfo::sRelease << "\n";
+      exit(EXIT_SUCCESS);
+    }
+    else if(strcmp(argv[1], "--copyright") == 0) {
+      std::cerr << Pipeline::PackageInfo::sCopyright << "\n";
       exit(EXIT_SUCCESS);
     }
   }
-  else if(argc == 3) {
-    if(strcmp(argv[1], "--verbose") == 0) {
-      FB::setWarnings(true);
-      FB::setStageStats(true);
+  else if((argc > 2) && (argc < 5)) {
+    int wk;
+    for(wk=1; wk<3; wk++) {
+      if(strcmp(argv[wk], "--verbose") == 0) {
+	FB::setWarnings(true);
+	FB::setStageStats(true);
+      }
+      else if(strcmp(argv[wk], "--md5") == 0) {
+	md5 = true;
+      }
     }
   }
-  else if(argc != 1) {
+  else {
     usage();
     exit(EXIT_FAILURE);
   }
@@ -144,10 +307,17 @@ main
   Pairs pairs;
   FB::stageBegin("Reading File List: ");
   {
+    FB::stageMsg(flist);
     char work[1024];
     char repo[1024];
 
     ifstream in(flist);
+    if(!in) {
+      char msg[1024];
+      sprintf(msg, "Unable to read file list from: \"%s\"!", flist);
+      FB::error(msg);
+    }
+
     while(in) {
       in >> work >> repo;
       if(!in)
@@ -173,6 +343,15 @@ main
 	sprintf(msg, "missing working area file: %s", work);
 	FB::error(msg);
       }
+
+      if(md5) {
+	const char* md5 = (*iter)->uWorkMd5;
+	FB::stageMsg(md5);
+	if(access(md5, F_OK) != 0) {
+	  sprintf(msg, "missing working area MD5 checksum file: %s", md5);
+	  FB::error(msg);
+	}
+      }
     }
   }
   FB::stageEnd();
@@ -181,23 +360,7 @@ main
   /* make sure the repository files DO NOT already exist */ 
   FB::stageBegin("Checking Repository Files:");
   {
-    Pairs::iterator iter;
-    for(iter=pairs.begin(); iter != pairs.end(); iter++) {
-      const char* repo = (*iter)->uRepo;
-      FB::stageMsg(repo);
-
-      struct stat buf;
-      if(stat(repo, &buf) == 0) {
-	if(S_ISDIR(buf.st_mode)) {
-	  sprintf(msg, "bad path, directory exists with the name: %s", repo);
-	  FB::error(msg);	    
-	}
-	else {
-	  sprintf(msg, "attempted to overwrite repository file: %s", repo);
-	  FB::error(msg);
-	}
-      }
-    }
+    checkRepoFiles(pairs, md5);
   }
   FB::stageEnd();
 
@@ -211,7 +374,12 @@ main
     {
       Pairs::iterator iter;
       for(iter=pairs.begin(); iter != pairs.end(); iter++) {
-	char* repo = strdup((*iter)->uRepo);
+	char* repo = NULL;
+	if(md5) 
+	  repo = strdup((*iter)->uRepoMd5);
+	else 
+	  repo = strdup((*iter)->uRepo);
+
 	char* p = repo;
 	p += strlen(repo)-1;
 
@@ -287,26 +455,10 @@ main
   FB::stageEnd();
 
 
-  /* make sure the repository files DO NOT already exist */ 
+  /* make sure the repository files DO NOT already exist (just to be absolutely safe!) */ 
   FB::stageBegin("Rechecking Repository Files:");
   {
-    Pairs::iterator iter;
-    for(iter=pairs.begin(); iter != pairs.end(); iter++) {
-      const char* repo = (*iter)->uRepo;
-      FB::stageMsg(repo);
-
-      struct stat buf;
-      if(stat(repo, &buf) == 0) {
-	if(S_ISDIR(buf.st_mode)) {
-	  sprintf(msg, "bad path, directory exists with the name: %s", repo);
-	  FB::error(msg);	    
-	}
-	else {
-	  sprintf(msg, "attempted to overwrite repository file: %s", repo);
-	  FB::error(msg);
-	}
-      }
-    }
+    checkRepoFiles(pairs, md5);
   }
   FB::stageEnd();
 
@@ -317,40 +469,17 @@ main
   list<const char*> copied;
   {
     Pairs::iterator iter;
-    for(iter=pairs.begin(); iter!=pairs.end() && !aborted; iter++) {
-      const char* work = (*iter)->uWork;
-      const char* repo = (*iter)->uRepo;
-      
-      /* open work file */ 
-      ifstream in(work);
-      if(!in) {
-	sprintf(msg, "unable to open working file: %s", work);
-	FB::warn(msg);
+    for(iter=pairs.begin(); iter!=pairs.end() && !aborted; iter++) {      
+      if(!copyFileToRepo((*iter)->uWork, (*iter)->uRepo, copied)) {
+	aborted = true;
+	break;
+      }     
+
+      if(!copyFileToRepo((*iter)->uWorkMd5, (*iter)->uRepoMd5, copied)) {
 	aborted = true;
 	break;
       }
-
-      /* create/open repository file */ 
-      ofstream out(repo, ios::out | ios::noreplace, 0444);
-      if(!in) {
-	sprintf(msg, "unable to open repository file: %s", repo);
-	FB::warn(msg);
-	aborted = true;
-	break;
-      }
-      
-      /* copy it byte-by-byte */   
-      out << in.rdbuf(); 
-
-      /* close the files */ 
-      in.close();
-      out.close();
-
-      /* success! */ 
-      sprintf(msg, "copied: %s to %s", work, repo);
-      FB::stageMsg(msg);
     }
-
 
     /* a failure occured while copying, 
          remove any successfully copied files */ 
@@ -368,6 +497,7 @@ main
       }
       FB::stageEnd();
     }
+
   }
   FB::stageEnd();
   
