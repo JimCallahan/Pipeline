ca1e4b73eb7591fb7e00faa8941719068d071bed
Jim Callahan <jim@temerity.us>
2003-06-29 14:15:19 +0000

    added javamake support, commented opt and profiling builds for now

--- config/bootstrap.sh
+++ config/bootstrap.sh
@@ -9,28 +9,29 @@ popd
 
 pushd i686-pc-linux-gnu-dbg
   /home/$USER/code/src/pipeline/configure \
-    --with-compiler=GNU \
     --disable-opt \
+    --with-compiler=GNU \
     --with-base=/base \
     --with-pl-server=localhost \
-    --with-pb-sserver=localhost
+    --with-pb-sserver=localhost \
+    --with-javamake=/usr/java/javamake/lib/javamake.jar
 popd
 
-pushd i686-pc-linux-gnu-opt
-  /home/$USER/code/src/pipeline/configure \
-    --with-compiler=GNU \
-    --enable-opt \
-    --with-base=/base \
-    --with-pl-server=localhost \
-    --with-pbs-server=localhost
-popd
+# pushd i686-pc-linux-gnu-opt
+#   /home/$USER/code/src/pipeline/configure \
+#     --enable-opt \
+#     --with-compiler=GNU \
+#     --with-base=/base \
+#     --with-pl-server=localhost \
+#     --with-pbs-server=localhost
+# popd
 
-pushd i686-pc-linux-gnu-prof
-  /home/$USER/code/src/pipeline/configure \
-    --with-compiler=GNU \
-    --disable-opt \
-    --enable-prof \
-    --with-base=/base \
-    --with-pl-server=localhost \
-    --with-pbs-server=localhost 
-popd
+# pushd i686-pc-linux-gnu-prof
+#   /home/$USER/code/src/pipeline/configure \
+#     --disable-opt \
+#     --enable-prof \
+#     --with-compiler=GNU \
+#     --with-base=/base \
+#     --with-pl-server=localhost \
+#     --with-pbs-server=localhost 
+# popd
