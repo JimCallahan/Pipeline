ebd9af6245cb7e0ac3c4398571a9c413808e479a
Jim Callahan <jim@temerity.us>
2003-07-05 16:00:55 +0000

    updated to match java PackageInfo

--- src/utils/src/PackageInfo.hh
+++ src/utils/src/PackageInfo.hh
@@ -1,4 +1,4 @@
-// $Id: PackageInfo.hh,v 1.2 2003/02/10 16:11:12 jim Exp $
+// $Id: PackageInfo.hh,v 1.3 2003/07/05 16:00:55 jim Exp $
 
 #ifndef PIPELINE_PACKAGE_INFO_HH
 #define PIPELINE_PACKAGE_INFO_HH
@@ -27,14 +27,33 @@ public:
   /* when the package was released */ 
   static const char* sRelease;
 
+
   /* server hostname */ 
   static const char* sServer;
 
   /* server port number */ 
   static const int sPort;
 
+
+  /* PBS server hostname */ 
+  static const char* sPbsServer;
+ 
+
   /* root directory of the production heirarchy */ 
   static const char* sBaseDir;
+
+  /* base repository directory */ 
+  static const char* sRepoDir;
+  
+  /* base working area directory */ 
+  static const char* sWorkDir;
+  
+  /* installed toolset directory */ 
+  static const char* sToolsetDir;
+  
+  /* installed location of the toolset-exec program */ 
+  static const char* sToolsetExec;
+
 
   /* installed documentation directory */ 
   static const char* sDocsDir;
@@ -42,20 +61,25 @@ public:
   /* location of the Mozilla web browser */ 
   static const char* sMozilla;
 
-  /* installed location of the toolset-exec program */ 
-  static const char* sToolsetExec;
 
-  /* installed location of the java interpreter */ 
-  static const char* sJava;
+  /* installed location of the GNU Bourne Again SHell */ 
+  static const char* sBash;
   
-  /* installed location of the Java Database JAR file */ 
-  static const char* sJDBC;
-
   /* installed location of the GNU make program */ 
   static const char* sMake;
-
+  
+  /* installed location of the md5sum program */ 
+  static const char* sMd5sum;
+  
   /* installed location of the diff program */ 
   static const char* sDiff;
+  
+  /* installed location of the kill program */ 
+  static const char* sKill;
+  
+  /* installed location of the SSH program */ 
+  static const char* sSsh;
+
 
   /* copyright message */ 
   static const char* sCopyright;
