94ea8671d09697a5f4c7e20b0ccc1f80e38a7910
Jim Callahan <jim@temerity.us>
2003-07-28 23:11:13 +0000

    latest

--- config/bootstrap.sh
+++ config/bootstrap.sh
@@ -3,38 +3,47 @@
 rm -rf i686-pc-linux-gnu-dbg i686-pc-linux-gnu-opt i686-pc-linux-gnu-prof 
 mkdir  i686-pc-linux-gnu-dbg i686-pc-linux-gnu-opt i686-pc-linux-gnu-prof
 
-pushd /home/$USER/code/src/pipeline
-  ./autogen.sh
+pushd /code/src/pipeline
+  sh autogen.sh
 popd
 
 pushd i686-pc-linux-gnu-dbg
-  /home/$USER/code/src/pipeline/configure \
+  /code/src/pipeline/configure \
     --prefix=/base/pipeline \
     --datadir=/usr/share/pipeline \
     --disable-opt \
     --with-compiler=GNU \
     --with-base=/base \
+    --with-code=/code \
     --with-sql-server=linuxserv1 \
     --with-pbs-server=linuxserv1 \
     --with-javamake=/usr/java/javamake/lib/javamake.jar
 popd
 
-# pushd i686-pc-linux-gnu-opt
-#   /home/$USER/code/src/pipeline/configure \
-#     --with-compiler=GNU \
-#     --enable-opt \
-#     --with-base=/base \
-#     --with-sql-server=linuxserv1 \
-#     --with-pbs-server=linuxserv1 \
-#     --with-javamake=/usr/java/javamake/lib/javamake.jar
-# popd
+pushd i686-pc-linux-gnu-opt
+  /code/src/pipeline/configure \
+    --prefix=/base/pipeline \
+    --datadir=/usr/share/pipeline \
+    --enable-opt \
+    --with-compiler=GNU \
+    --with-base=/base \
+    --with-code=/code \
+    --with-sql-server=linuxserv1 \
+    --with-pbs-server=linuxserv1 \
+    --with-javamake=/usr/java/javamake/lib/javamake.jar
+popd
 
-# pushd i686-pc-linux-gnu-prof
-#   /home/$USER/code/src/pipeline/configure \
-#     --with-compiler=GNU \
-#     --disable-opt \
-#     --enable-prof \
-#     --with-base=/base \
-#     --with-sql-server=linuxserv1 \
-#     --with-pbs-server=linuxserv1 
-# popd
+pushd i686-pc-linux-gnu-prof
+  /code/src/pipeline/configure \
+    --prefix=/base/pipeline \
+    --datadir=/usr/share/pipeline \
+    --enable-opt \
+    --enable-prof \
+    --with-compiler=GNU \
+    --with-base=/base \
+    --with-code=/code \
+    --with-sql-server=linuxserv1 \
+    --with-pbs-server=linuxserv1 \
+    --with-javamake=/usr/java/javamake/lib/javamake.jar
+popd
+
