3e6209f2a1bf49d1f6cbe6dbd6f0fb64bfb5258b
Jim Callahan <jim@temerity.us>
2003-08-06 04:02:26 +0000

    change comments now work

--- config/bootstrap.sh
+++ config/bootstrap.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-rm -rf i686-pc-linux-gnu-dbg i686-pc-linux-gnu-opt i686-pc-linux-gnu-prof 
-mkdir  i686-pc-linux-gnu-dbg i686-pc-linux-gnu-opt i686-pc-linux-gnu-prof
+#rm -rf i686-pc-linux-gnu-dbg i686-pc-linux-gnu-opt i686-pc-linux-gnu-prof 
+#mkdir  i686-pc-linux-gnu-dbg i686-pc-linux-gnu-opt i686-pc-linux-gnu-prof
 
 pushd /code/src/pipeline
   sh autogen.sh
@@ -20,30 +20,30 @@ pushd i686-pc-linux-gnu-dbg
     --with-javamake=/usr/java/javamake/lib/javamake.jar
 popd
 
-pushd i686-pc-linux-gnu-opt
-  /code/src/pipeline/configure \
-    --prefix=/base/pipeline \
-    --datadir=/usr/share/pipeline \
-    --enable-opt \
-    --with-compiler=GNU \
-    --with-base=/base \
-    --with-code=/code \
-    --with-sql-server=linuxserv1 \
-    --with-pbs-server=linuxserv1 \
-    --with-javamake=/usr/java/javamake/lib/javamake.jar
-popd
+# pushd i686-pc-linux-gnu-opt
+#   /code/src/pipeline/configure \
+#     --prefix=/base/pipeline \
+#     --datadir=/usr/share/pipeline \
+#     --enable-opt \
+#     --with-compiler=GNU \
+#     --with-base=/base \
+#     --with-code=/code \
+#     --with-sql-server=linuxserv1 \
+#     --with-pbs-server=linuxserv1 \
+#     --with-javamake=/usr/java/javamake/lib/javamake.jar
+# popd
 
-pushd i686-pc-linux-gnu-prof
-  /code/src/pipeline/configure \
-    --prefix=/base/pipeline \
-    --datadir=/usr/share/pipeline \
-    --enable-opt \
-    --enable-prof \
-    --with-compiler=GNU \
-    --with-base=/base \
-    --with-code=/code \
-    --with-sql-server=linuxserv1 \
-    --with-pbs-server=linuxserv1 \
-    --with-javamake=/usr/java/javamake/lib/javamake.jar
-popd
+# pushd i686-pc-linux-gnu-prof
+#   /code/src/pipeline/configure \
+#     --prefix=/base/pipeline \
+#     --datadir=/usr/share/pipeline \
+#     --enable-opt \
+#     --enable-prof \
+#     --with-compiler=GNU \
+#     --with-base=/base \
+#     --with-code=/code \
+#     --with-sql-server=linuxserv1 \
+#     --with-pbs-server=linuxserv1 \
+#     --with-javamake=/usr/java/javamake/lib/javamake.jar
+# popd
 
