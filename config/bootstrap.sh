9594301072a8b33ee6ece700db8ff5a251f381c6
Jim Callahan <jim@temerity.us>
2003-08-26 01:01:36 +0000

    replaced /code with /home/jim/code

--- config/bootstrap.sh
+++ config/bootstrap.sh
@@ -1,49 +1,49 @@
 #!/bin/sh
 
-#rm -rf i686-pc-linux-gnu-dbg i686-pc-linux-gnu-opt i686-pc-linux-gnu-prof 
-#mkdir  i686-pc-linux-gnu-dbg i686-pc-linux-gnu-opt i686-pc-linux-gnu-prof
+rm -rf i686-pc-linux-gnu-dbg i686-pc-linux-gnu-opt i686-pc-linux-gnu-prof 
+mkdir  i686-pc-linux-gnu-dbg i686-pc-linux-gnu-opt i686-pc-linux-gnu-prof
 
-pushd /code/src/pipeline
+pushd $HOME/code/src/pipeline
   sh autogen.sh
 popd
 
 pushd i686-pc-linux-gnu-dbg
-  /code/src/pipeline/configure \
+  $HOME/code/src/pipeline/configure \
     --prefix=/base/pipeline \
     --datadir=/usr/share/pipeline \
     --disable-opt \
     --with-compiler=GNU \
     --with-base=/base \
-    --with-code=/code \
-    --with-sql-server=linuxserv1 \
-    --with-pbs-server=linuxserv1 \
+    --with-code=$HOME/code \
+    --with-sql-server=localhost \
+    --with-pbs-server=localhost \
     --with-javamake=/usr/java/javamake/lib/javamake.jar
 popd
 
-# pushd i686-pc-linux-gnu-opt
-#   /code/src/pipeline/configure \
-#     --prefix=/base/pipeline \
-#     --datadir=/usr/share/pipeline \
-#     --enable-opt \
-#     --with-compiler=GNU \
-#     --with-base=/base \
-#     --with-code=/code \
-#     --with-sql-server=linuxserv1 \
-#     --with-pbs-server=linuxserv1 \
-#     --with-javamake=/usr/java/javamake/lib/javamake.jar
-# popd
+pushd i686-pc-linux-gnu-opt
+  $HOME/code/src/pipeline/configure \
+    --prefix=/base/pipeline \
+    --datadir=/usr/share/pipeline \
+    --enable-opt \
+    --with-compiler=GNU \
+    --with-base=/base \
+    --with-code=$HOME/code \
+    --with-sql-server=localhost \
+    --with-pbs-server=localhost \
+    --with-javamake=/usr/java/javamake/lib/javamake.jar
+popd
 
-# pushd i686-pc-linux-gnu-prof
-#   /code/src/pipeline/configure \
-#     --prefix=/base/pipeline \
-#     --datadir=/usr/share/pipeline \
-#     --enable-opt \
-#     --enable-prof \
-#     --with-compiler=GNU \
-#     --with-base=/base \
-#     --with-code=/code \
-#     --with-sql-server=linuxserv1 \
-#     --with-pbs-server=linuxserv1 \
-#     --with-javamake=/usr/java/javamake/lib/javamake.jar
-# popd
+pushd i686-pc-linux-gnu-prof
+  $HOME/code/src/pipeline/configure \
+    --prefix=/base/pipeline \
+    --datadir=/usr/share/pipeline \
+    --enable-opt \
+    --enable-prof \
+    --with-compiler=GNU \
+    --with-base=/base \
+    --with-code=$HOME/code \
+    --with-sql-server=localhost \
+    --with-pbs-server=localhost \
+    --with-javamake=/usr/java/javamake/lib/javamake.jar
+popd
 
