7213cb740694464f8eca723f9789022333806ef6
Jim Callahan <jim@temerity.us>
2003-10-11 02:50:07 +0000

    initial revision

---
+++ src/utils/src/FileSeq.hh
@@ -0,0 +1,413 @@
+// $Id: FileSeq.hh,v 1.1 2003/10/11 02:50:07 jim Exp $
+
+#ifndef PIPELINE_FILE_SEQ_HH
+#define PIPELINE_FILE_SEQ_HH
+
+#ifdef HAVE_CONFIG_H
+#  include "config.h"
+#endif
+
+#ifdef CSTRING_H
+#  include <cstring>
+#else 
+#  ifdef HAVE_STRING_H
+#    include <string.h>
+#  endif
+#endif
+
+#ifdef HAVE_SET
+#  include <set>
+#endif
+
+using namespace std;
+
+namespace Pipeline {
+
+/*------------------------------------------------------------------------------------------*/
+/*   F I L E   S E Q                                                                        */
+/*                                                                                          */
+/*                                                                                          */
+/*------------------------------------------------------------------------------------------*/
+
+class FileSeq
+{
+protected:
+  /*----------------------------------------------------------------------------------------*/
+  /*  I N T E R N A L   T Y P E S                                                           */
+  /*----------------------------------------------------------------------------------------*/
+  
+  struct ltint
+  {
+    bool 
+    operator()
+    (
+     int a, 
+     int b
+    ) const
+    {
+      return (a < b);
+    }
+  };
+  
+  typedef std::set<int, ltint>   FrameSet; 
+  
+  
+public:
+  /*----------------------------------------------------------------------------------------*/
+  /*  C O N S T R U C T O R                                                                 */
+  /*----------------------------------------------------------------------------------------*/
+
+  /* File sequence constructor. */ 
+  FileSeq
+  (
+   string& prefix,   /* IN: file sequence prefix */ 
+   int padding,      /* IN: minumum number of frame characters */ 
+   string& suffix,   /* IN: file sequence suffix */
+   int frame         /* IN: initial frame number */ 
+  ) :
+    pPrefix(prefix), 
+    pPadding(padding), 
+    pSuffix(suffix)
+  {
+    assert(pPadding > 0);
+    addFrame(frame);
+  }
+
+
+  /* Single file constructor. */ 
+  FileSeq
+  (
+   string& filename   /* IN: single file */ 
+  ) :
+    pPrefix(filename), 
+    pPadding(-1), 
+    pSuffix("")
+  {}
+
+
+  /*----------------------------------------------------------------------------------------*/
+  /*  D E S T R U C T O R                                                                   */
+  /*----------------------------------------------------------------------------------------*/
+
+  ~FileSeq()
+  {
+  }
+  
+
+  /*----------------------------------------------------------------------------------------*/
+  /*  A C C E S S                                                                           */
+  /*----------------------------------------------------------------------------------------*/
+
+  /* File sequence prefix */ 
+  const string& 
+  getPrefix() const
+  {
+    return pPrefix;
+  }
+
+  /* Minumum number of frame characters. */ 
+  int 
+  getPadding() const 
+  {
+    return pPadding;
+  }
+
+  /* File sequence suffix */ 
+  const string& 
+  getSuffix() const
+  {
+    return pSuffix;
+  }
+
+
+  /*----------------------------------------------------------------------------------------*/
+  /*  F R A M E   N U M B E R S                                                             */
+  /*----------------------------------------------------------------------------------------*/
+
+  /* Add a frame number to the sequence. */ 
+  void 
+  addFrame
+  (
+   int frame  /* IN: frame number */ 
+  ) 
+  {
+    pFrames.insert(frame);
+  }
+  
+
+  /* Add a bad frame number to the sequence. */ 
+  void 
+  addBadFrame
+  (
+   int frame  /* IN: frame number */ 
+  ) 
+  {
+    pBadFrames.insert(frame);
+  }
+  
+
+  /*----------------------------------------------------------------------------------------*/
+  /*  C O N V E R S I O N                                                                   */
+  /*----------------------------------------------------------------------------------------*/
+
+  /* Convert to a string representation. */ 
+  void
+  toString
+  (
+   string& out   /* OUT: returned string representation */ 
+  ) const 
+  {
+    /* single filename */ 
+    if(pPadding == -1) {
+      out = pPrefix;
+      return;
+    }
+
+    /* single file sequence */ 
+    if(pFrames.size() == 1) {
+      char format[1024];
+      sprintf(format, "%%s%%0%dd%%s", pPadding);
+
+      FrameSet::iterator iter = pFrames.begin();
+      int frame = (*iter);
+
+      char tmp[1024];
+      sprintf(tmp, format, pPrefix.c_str(), frame, pSuffix.c_str());
+
+      out = tmp;
+
+      return;
+    }
+
+    /* file sequence pattern */ 
+    {
+      out = pPrefix;
+
+      if(pPadding == 4) 
+	out += "#";
+      else {
+	int wk;
+	for(wk=0; wk<pPadding; wk++)
+	  out += "@";
+      }
+      
+      out += (pSuffix + " ");
+    }
+
+
+    /* the frame range and increment */ 
+    int by;
+    {
+      int start, end;
+      compFrameRange(start, end, by);
+
+      char range[1024];
+      sprintf(range, "(%d-%dx%d", start, end, by);
+      out += range;
+    }
+
+    
+    /* the contiguous sequences in range */ 
+    {
+      FrameSet::iterator iter = pFrames.begin();
+      int last  = (*iter);
+      int start = last;
+      iter++;
+
+      char tmp[1024];
+      sprintf(tmp, "%d", last);
+      string seq = tmp;
+
+      bool missing = false;
+      for(; iter != pFrames.end(); iter++) {
+	if(((*iter) - last) > by) {
+	  missing = true;
+
+	  if(last > start) 
+	    sprintf(tmp, "-%d,%d", last, *iter);
+	  else 
+	    sprintf(tmp, ",%d", *iter);
+	  seq += tmp;
+
+	  start = *iter;
+	}
+	
+	last = (*iter);
+      }
+
+      if(last > start) {
+	sprintf(tmp, "-%d", last);
+	seq += tmp;
+      }
+	  
+      if(missing) 
+	out += (": " + seq + ")");
+      else 
+	out += ")";
+    }
+
+
+    /* bad frames */ 
+    if(pBadFrames.size() > 0) {
+      FrameSet::iterator iter = pBadFrames.begin();
+      int last  = (*iter);
+      int start = last;
+      iter++;
+
+      char tmp[1024];
+      sprintf(tmp, "%d", last);
+      string seq = tmp;
+
+      for(; iter != pBadFrames.end(); iter++) {
+	if(((*iter) - last) > by) {
+	  if(last > start) 
+	    sprintf(tmp, "-%d,%d", last, *iter);
+	  else 
+	    sprintf(tmp, ",%d", *iter);
+	  seq += tmp;
+
+	  start = *iter;
+	}
+	
+	last = (*iter);
+      }
+
+      if(last > start) {
+	sprintf(tmp, "-%d", last);
+	seq += tmp;
+      }
+	  
+      out += (" <BAD: " +seq + ">");
+    }
+  }
+
+ 
+  /* Convert to a fcheck command-line representation. */ 
+  void
+  toFcheckCommand
+  (
+   string& out   /* OUT: command-line */ 
+  ) const 
+  {
+    out = "fcheck ";
+
+    /* single filename */ 
+    if(pPadding == -1) {
+      out += pPrefix;
+      return;
+    }
+
+    /* single file sequence */ 
+    if(pFrames.size() == 1) {
+      char format[1024];
+      sprintf(format, "%%s%%0%dd%%s", pPadding);
+
+      FrameSet::iterator iter = pFrames.begin();
+      int frame = (*iter);
+
+      char tmp[1024];
+      sprintf(tmp, format, pPrefix.c_str(), frame, pSuffix.c_str());
+
+      out += tmp;
+
+      return;
+    }
+
+    /* the frame range and increment */ 
+    {
+      int start, end, by;
+      compFrameRange(start, end, by);
+
+      char range[1024];
+      sprintf(range, "-n %d %d %d ", start, end, by);
+      out += range;
+    }
+
+    /* file pattern */ 
+    {
+      out += pPrefix;
+
+      if(pPadding == 4) 
+	out += "#";
+      else 
+	out += "@";
+
+      out += pSuffix;
+      out += ";";
+    }    
+  }
+
+
+
+protected:
+  /*----------------------------------------------------------------------------------------*/
+  /*  H E L P E R S                                                                         */
+  /*----------------------------------------------------------------------------------------*/
+
+  /* Compute the frame range and increment. */ 
+  void 
+  compFrameRange
+  (
+   int& start,   /* OUT: start frame */ 
+   int& end,     /* OUT: end frame */ 
+   int& by       /* OUT: frame increment */ 
+  ) const 
+  {    
+    assert(pFrames.size() > 1);
+
+    FrameSet::iterator iter = pFrames.begin();
+    start = (*iter);
+    int last = (*iter);
+    iter++;
+
+    by = 0;    
+    for(; iter != pFrames.end(); iter++) {
+      if(by == 0)
+	by = (*iter) - last;
+      else 
+	by = min(by, (*iter) - last);
+      
+      last = (*iter);
+    }
+    
+    end = last;
+  }
+      
+
+
+protected:
+  const string  pPrefix;     /* File sequence prefix. */ 
+  int           pPadding;    /* Minumum number of frame characters. */ 
+  const string  pSuffix;     /* File sequence suffix. */ 
+
+  FrameSet      pFrames;     /* Existing frame numbers. */ 
+  FrameSet      pBadFrames;  /* Bad frame numbers. */ 
+
+};
+  
+
+/* Less-than operator for FileSeqs */ 
+struct ltFileSeq
+{
+  bool operator()(const FileSeq* a, const FileSeq* b) const
+  {
+    if(a->getPrefix() < b->getPrefix())
+      return true;
+    else if(a->getPrefix() > b->getPrefix())
+      return false;
+    else {
+      int padding = a->getPadding() - b->getPadding();
+      if(padding < 0)
+	return true;
+      else if(padding > 0) 
+	return false;
+      else 
+	return (a->getSuffix() < b->getSuffix());	
+    }
+  }
+};
+  
+
+} // namespace Pipeline
+
+#endif
---
+++ src/utils/src/Pls.cc
@@ -0,0 +1,304 @@
+// $Id: Pls.cc,v 1.1 2003/10/11 02:50:07 jim Exp $
+
+#ifdef HAVE_CONFIG_H
+#  include "config.h"
+#endif
+
+#ifdef HAVE_SYS_TYPES_H
+#  include <sys/types.h>
+#endif
+
+#ifdef HAVE_DIRENT_H
+#  include <dirent.h>
+#endif
+
+#ifdef HAVE_UNISTD_H
+#  include <unistd.h>
+#endif
+
+#ifdef HAVE_CTYPE_H
+#  include <ctype.h>
+#endif
+
+#ifdef HAVE_SYS_STAT_H
+#  include <sys/stat.h>
+#endif
+
+#ifdef HAVE_ERRNO_H
+#  include <errno.h>
+#endif
+
+#ifdef HAVE_LIST
+#  include <list>
+#endif
+
+#include <PackageInfo.hh>
+#include <FileSeq.hh>
+
+using namespace Pipeline;
+
+/*------------------------------------------------------------------------------------------*/
+/*   P L S                                                                                  */
+/*                                                                                          */
+/*     A directory listing program which lists file sequences.                              */
+/*------------------------------------------------------------------------------------------*/
+
+/* usage message */ 
+void
+usage()
+{
+  std::cerr << "usage: pls [options][directory]\n"
+	    << "\n" 
+	    << "       pls --help\n"
+	    << "       pls --html-help\n"
+	    << "       pls --version\n"
+	    << "       pls --release-date\n"
+	    << "       pls --copyright\n" 
+	    << "\n" 
+	    << "OPTIONS:\n" 
+	    << "  [--fcheck][--zero][--size=bytes]\n" 
+	    << "\n"
+	    << "\n"
+	    << "Use \"pls --html-help\" to browse the full documentation.\n" 
+	    << std::flush;
+}
+
+int
+main
+(
+ int argc, 
+ char **argv
+)
+{
+  char msg[2048];
+
+  /* parse command line args */ 
+  FB::init(std::cout);
+  FB::setWarnings(false);
+  FB::setStageStats(false);
+
+  bool fcheck = false;
+  long int minSize = -1;
+  char* dir = NULL;
+  switch(argc) {
+  case 1:
+    break;
+
+  case 2:
+    {
+      if(strcmp(argv[1], "--help") == 0) {
+	usage();
+	exit(EXIT_SUCCESS);
+      }
+      else if(strcmp(argv[1], "--html-help") == 0) {
+	char buf[1024];
+	sprintf(buf, "openURL(file:%s/plget.html, new-window)", 
+		PackageInfo::sDocsDir); 
+	
+	char* args[4]; 
+	args[0] = strdup("mozilla");
+	args[1] = strdup("-remote");
+	args[2] = strdup(buf);
+	args[3] = NULL;
+	
+	execv(PackageInfo::sMozilla, args);
+      }
+      else if(strcmp(argv[1], "--version") == 0) {
+	std::cerr << PackageInfo::sVersion << "\n";
+	exit(EXIT_SUCCESS);
+      }
+      else if(strcmp(argv[1], "--release-date") == 0) {
+	std::cerr << PackageInfo::sRelease << "\n";
+	exit(EXIT_SUCCESS);
+      }
+      else if(strcmp(argv[1], "--copyright") == 0) {
+	std::cerr << PackageInfo::sCopyright << "\n";
+	exit(EXIT_SUCCESS);
+      }
+      else if(strcmp(argv[1], "--fcheck") == 0) 
+	fcheck = true;
+      else if(strcmp(argv[1], "--zero") == 0) 
+	minSize = 0;
+      else if(strncmp(argv[1], "--size=", 7) == 0) 
+	minSize = atol(argv[1]+7);
+      else if(strncmp(argv[1], "--", 2) == 0) {
+	sprintf(msg, "Illegal option: %s", argv[1]);
+	FB::error(msg);
+      }
+      else 
+	dir = argv[1];
+    }
+    break;
+
+  case 3:
+    {
+      if(strcmp(argv[1], "--fcheck") == 0)
+	fcheck = true;
+      else if(strcmp(argv[1], "--zero") == 0) 
+	minSize = 0;
+      else if(strncmp(argv[1], "--size=", 7) == 0) 
+	minSize = atol(argv[1]+7);
+      else if(strncmp(argv[1], "--", 2) == 0) {
+	sprintf(msg, "Illegal option: %s", argv[1]);
+	FB::error(msg);
+      }
+      
+      dir = argv[2];
+    }
+    break;
+
+  default:
+    usage();
+    exit(EXIT_FAILURE);
+  }
+
+
+  /* if no directory was given, determine the current directory */ 
+  if(dir == NULL) {
+    dir = new char[2048];
+    if(getcwd(dir, 2048) == NULL) { 
+      switch(errno) {
+      case EACCES:
+	FB::error("No permission to read the current directory.");
+	break;
+
+      case ENOENT:
+	FB::error("The current working directory has been unlinked.");
+	break;
+	 
+      default:
+	FB::error("Internal error!");
+      }
+    }
+  }
+  assert(dir != NULL);
+
+	
+  /* build a list of filenames the current diretory */ 
+  typedef std::list<string*> FileList;
+  FileList files;
+  FileList bad;
+  {
+    struct dirent **namelist;
+    int n;
+    if((n = scandir(dir, &namelist, NULL, alphasort)) == -1) {
+      sprintf(msg, "Unable to read the directory: %s", dir);
+      FB::error(msg);
+    }
+
+    while(n--) {
+      string file;
+      file += dir;
+      file += "/";
+      file += namelist[n]->d_name;
+
+      if(access(file.c_str(), F_OK) == -1) {  
+	sprintf(msg, "Unable to access: %s", file.c_str());
+	FB::error(msg);	
+      }
+
+      struct stat buf;
+      if(stat(file.c_str(), &buf) == 0) {
+	if(S_ISREG(buf.st_mode) || S_ISLNK(buf.st_mode)) {
+	  files.push_back(new string(file));
+	  if(buf.st_size <= minSize) 
+	    bad.push_back(new string(file));
+	}
+      }
+
+      free(namelist[n]);
+    }
+    free(namelist);
+  }
+
+
+  /* sort found files into file sequences and single files */ 
+  typedef std::set<FileSeq*, ltFileSeq>  FileSeqSet;
+  FileSeqSet fseqs;
+  {
+    FileList::iterator iter;
+    for(iter=files.begin(); iter != files.end(); iter++) {
+      string* str = (*iter);
+
+      /* check if this is a bad frame */ 
+      bool badframe = false;
+      {
+	FileList::iterator biter;
+	for(biter=bad.begin(); biter != bad.end(); biter++) {
+	  if((*(*biter)) == (*(*iter))) {
+	    badframe = true;
+	    break;
+	  }
+	}	
+      }
+
+      /* chop filename up into prefix, padding, suffix and frame number components */ 
+      int wk;
+      string::size_type size = str->size();
+      for(wk=size-1; wk>=0; wk--) {
+	if(isdigit((int) ((*str)[wk]))) 
+	  break;
+      }
+      
+      if(wk == -1) {
+	FileSeq* fseq = new FileSeq(*str);
+	fseqs.insert(fseq);
+      }
+      else {
+	string suffix;
+	if(wk < (size-1)) 
+	  suffix = str->substr(wk+1, size - (wk+1));
+	
+	int frameEnd = wk;
+	for(; wk>=0; wk--) {
+	  if(!isdigit((int) ((*str)[wk]))) 
+	    break;
+	}
+
+	int padding = frameEnd - wk;
+	assert(padding > 0);
+
+	string fr = str->substr(wk+1, padding);
+	int frame = atoi(fr.c_str());
+	
+	string prefix;
+	if(wk > 0) 
+	  prefix = str->substr(0, wk+1);
+
+	FileSeq* fseq = new FileSeq(prefix, padding, suffix, frame);
+	{
+	  FileSeqSet::iterator fiter = fseqs.find(fseq);
+	  if(fiter == fseqs.end()) {
+	    fseqs.insert(fseq);
+	    if(badframe) 
+	      fseq->addBadFrame(frame);
+	  }
+	  else {
+	    (*fiter)->addFrame(frame);
+	    if(badframe) 
+	      (*fiter)->addBadFrame(frame);
+	  }
+	}
+
+	delete str;
+      }
+    }
+  }
+  
+  { 
+    FileSeqSet::iterator iter;
+    for(iter=fseqs.begin(); iter != fseqs.end(); iter++) {
+      FileSeq* fseq = (*iter);
+
+      string str;
+      if(fcheck) 
+	fseq->toFcheckCommand(str);
+      else
+	fseq->toString(str);
+
+      std::cout << str << "\n";
+    }
+  }
+
+  return EXIT_SUCCESS;
+}
