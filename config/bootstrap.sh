9a10195318347b6c73476898f4676bcc0a8f175d
Jim Callahan <jim@temerity.us>
2003-12-17 04:21:08 +0000

    latest

--- config/bootstrap.sh
+++ config/bootstrap.sh
@@ -1,34 +1,36 @@
 #!/bin/sh
 
-rm -rf i686-pc-linux-gnu-dbg i686-pc-linux-gnu-opt
-mkdir  i686-pc-linux-gnu-dbg i686-pc-linux-gnu-opt
+#rm -rf i686-pc-linux-gnu-dbg i686-pc-linux-gnu-opt
+#mkdir  i686-pc-linux-gnu-dbg i686-pc-linux-gnu-opt
 
-pushd $HOME/code/src/pipeline
+pushd $HOME/code2/src/pipeline
   sh autogen.sh
 popd
 
 pushd i686-pc-linux-gnu-dbg
-  $HOME/code/src/pipeline/configure \
+  $HOME/code2/src/pipeline/configure \
     --disable-opt \
     --with-compiler=GNU \
     --with-javamake=/usr/java/javamake/lib/javamake.jar \
-    --prefix=/base/apps/i686-pc-linux-gnu-dbg/pipeline-031212/pipeline \
-    --datadir=/base/apps/i686-pc-linux-gnu-dbg/pipeline-031212/pipeline/share \
-    --with-prod=/fxrhino1/ATTO2/prod \
+    --prefix=/base/apps/i686-pc-linux-gnu-dbg/pipeline-031214/pipeline \
+    --datadir=/base/apps/i686-pc-linux-gnu-dbg/pipeline-031214/pipeline/share \
+    --with-prod=/fxrhino1/ATTO2/prod2 \
     --with-toolset=/base/toolset \
     --with-sql-server=linuxserv1 \
-    --with-pbs-server=linuxserv1
+    --with-sql-port=53138 \
+    --with-max-keys=40
 popd
 
 pushd i686-pc-linux-gnu-opt
-  $HOME/code/src/pipeline/configure \
+  $HOME/code2/src/pipeline/configure \
     --enable-opt \
     --with-compiler=GNU \
     --with-javamake=/usr/java/javamake/lib/javamake.jar \
-    --prefix=/base/apps/i686-pc-linux-gnu-opt/pipeline-031212/pipeline \
-    --datadir=/base/apps/i686-pc-linux-gnu-opt/pipeline-031212/pipeline/share \
-    --with-prod=/fxrhino1/ATTO2/prod \
+    --prefix=/base/apps/i686-pc-linux-gnu-opt/pipeline-031214/pipeline \
+    --datadir=/base/apps/i686-pc-linux-gnu-opt/pipeline-031214/pipeline/share \
+    --with-prod=/fxrhino1/ATTO2/prod2 \
     --with-toolset=/base/toolset \
     --with-sql-server=linuxserv1 \
-    --with-pbs-server=linuxserv1 
+    --with-sql-port=53138 \
+    --with-max-keys=40
 popd
