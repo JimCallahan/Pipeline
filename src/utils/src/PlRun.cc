586250d505c1cfd016628ee7b20b80b20604830c
Jim Callahan <jim@temerity.us>
2003-12-31 20:33:49 +0000

    added a username parameter to plrun to support fixing HOME and USER environmental variables

--- src/utils/src/PlRun.cc
+++ src/utils/src/PlRun.cc
@@ -1,4 +1,4 @@
-// $Id: PlRun.cc,v 1.2 2003/12/17 04:49:12 jim Exp $
+// $Id: PlRun.cc,v 1.3 2003/12/31 20:33:49 jim Exp $
 
 #ifdef HAVE_CONFIG_H
 #  include "config.h"
@@ -36,7 +36,7 @@ using namespace Pipeline;
 void
 usage()
 {
-  std::cerr << "usage: plrun uid command [args...]\n"
+  std::cerr << "usage: plrun username uid command [args...]\n"
 	    << std::flush;
 }
 
@@ -54,7 +54,7 @@ main
   FB::setWarnings(false);
   FB::setStageStats(false);
 
-  if(argc<3) {
+  if(argc<4) {
     usage();
     exit(EXIT_FAILURE);    
   }
@@ -67,12 +67,17 @@ main
       FB::error("plrun can only be run by the \"pipeline.pipeline\" user!");	
   }
 
+  /* unpack arguments */ 
+  char* username = argv[1];
+  char* uidStr   = argv[2];
+  char* cmd      = argv[3];
+
   /* change user */ 
   {
-    if(argv[1] == NULL) 
+    if(uidStr == NULL) 
       FB::error("somehow the UID was NULL!");
       
-    long id = strtol(argv[1], (char**) NULL, 10);
+    long id = strtol(uidStr, (char**) NULL, 10);
     if(id < 0)       
       FB::error("the UID must be positive!");
     else if(id == 0) 
@@ -93,7 +98,7 @@ main
     }
   }
   
-  /* build a new environment (to get around ld.so ignoring LD_LIBRARY_PATH) */ 
+  /* build a new environment: (to get around ld.so ignoring LD_LIBRARY_PATH) */ 
   char** envp2 = NULL;
   {
     int cnt = 0;
@@ -110,10 +115,24 @@ main
       int wk = 0;
       char** p = envp;
       while((*p) != NULL) {
-	if((strlen(*p) > 24) && (strncmp(*p, "PIPELINE_LD_LIBRARY_PATH", 24) == 0)) 
+	size_t len = strlen(*p);
+	if((len > 24) && (strncmp(*p, "PIPELINE_LD_LIBRARY_PATH", 24) == 0)) {
 	  envp2[wk] = strdup((*p)+9);
-	else 
+	}
+	else if(strcmp(*p, "USER=pipeline") == 0) {
+	  envp2[wk] = new char[strlen(username) + 6];
+	  strncpy(envp2[wk], *p, 5);
+	  strcat(envp2[wk], username);
+	}
+	else if(strcmp(*p, "HOME=/home/pipeline") == 0) {
+	  envp2[wk] = new char[strlen(username) + 12];
+	  strncpy(envp2[wk], *p, 11);
+	  strcat(envp2[wk], username);
+	}
+	else {
 	  envp2[wk] = strdup(*p);
+	}
+
 	wk++;
 	p++;
       }
@@ -122,7 +141,7 @@ main
   assert(envp2);
 
   /* execute the command */ 
-  if(execve(argv[2], argv+2, envp2) == -1) {
+  if(execve(cmd, argv+3, envp2) == -1) {
     sprintf(msg, "unable to execute \"%s\": %s", argv[2], strerror(errno));
     FB::error(msg);
   }
