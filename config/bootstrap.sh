ca69e4b97239b3aa800cf61bedbfab97ffd6e48f
Jim Callahan <jim@temerity.us>
2004-01-11 22:48:00 +0000

    v1.0.4, added --with-job-port

--- config/bootstrap.sh
+++ config/bootstrap.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-rm -rf i686-pc-linux-gnu-dbg i686-pc-linux-gnu-opt
-mkdir  i686-pc-linux-gnu-dbg i686-pc-linux-gnu-opt
+#rm -rf i686-pc-linux-gnu-dbg i686-pc-linux-gnu-opt
+#mkdir  i686-pc-linux-gnu-dbg i686-pc-linux-gnu-opt
 
 pushd $HOME/code2/src/pipeline
   sh autogen.sh
@@ -12,13 +12,14 @@ pushd i686-pc-linux-gnu-dbg
     --disable-opt \
     --with-compiler=GNU \
     --with-javamake=/usr/java/javamake/lib/javamake.jar \
-    --prefix=/base/apps/i686-pc-linux-gnu-dbg/pipeline-040107/pipeline \
-    --datadir=/base/apps/i686-pc-linux-gnu-dbg/pipeline-040107/pipeline/share \
+    --prefix=/base/apps/i686-pc-linux-gnu-dbg/pipeline-040111/pipeline \
+    --datadir=/base/apps/i686-pc-linux-gnu-dbg/pipeline-040111/pipeline/share \
     --with-prod=/fxrhino1/ATTO2/prod \
     --with-toolset=/base/toolset \
     --with-sql-server=fxlinux101 \
-    --with-sql-port=53139 \
-    --with-max-keys=40
+    --with-sql-port=53149 \
+    --with-max-keys=40 \
+    --with-job-port=53150
 popd
 
 # pushd i686-pc-linux-gnu-opt
