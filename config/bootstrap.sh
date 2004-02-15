3029863e31357b8a2d40de5e7123d18549dc69ff
Jim Callahan <jim@temerity.us>
2004-02-15 18:31:33 +0000

    post SQL options, salamander build

--- config/bootstrap.sh
+++ config/bootstrap.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-rm -rf i686-pc-linux-gnu-dbg i686-pc-linux-gnu-opt
-mkdir  i686-pc-linux-gnu-dbg i686-pc-linux-gnu-opt
+rm -rf i686-pc-linux-gnu-dbg
+mkdir  i686-pc-linux-gnu-dbg
 
 pushd $HOME/code2/src/pipeline
   sh autogen.sh
@@ -11,27 +11,10 @@ pushd i686-pc-linux-gnu-dbg
   $HOME/code2/src/pipeline/configure \
     --disable-opt \
     --with-compiler=GNU \
-    --with-javamake=/usr/java/javamake/lib/javamake.jar \
-    --prefix=/base/apps/i686-pc-linux-gnu-dbg/pipeline-040118/pipeline \
-    --datadir=/base/apps/i686-pc-linux-gnu-dbg/pipeline-040118/pipeline/share \
-    --with-prod=/fxrhino1/ATTO2/prod \
+    --prefix=/base/apps/i686-pc-linux-gnu-dbg/pipeline-040211/pipeline \
+    --datadir=/base/apps/i686-pc-linux-gnu-dbg/pipeline-040211/pipeline/share \
+    --with-prod=/base/prod \
     --with-toolset=/base/toolset \
-    --with-sql-server=fxlinux101 \
-    --with-sql-port=53149 \
-    --with-max-keys=40 \
-    --with-job-port=53150
+    --with-pl-server=localhost \
+    --with-pl-port=53135
 popd
-
-# pushd i686-pc-linux-gnu-opt
-#   $HOME/code2/src/pipeline/configure \
-#     --enable-opt \
-#     --with-compiler=GNU \
-#     --with-javamake=/usr/java/javamake/lib/javamake.jar \
-#     --prefix=/base/apps/i686-pc-linux-gnu-opt/pipeline-040102/pipeline \
-#     --datadir=/base/apps/i686-pc-linux-gnu-opt/pipeline-040102/pipeline/share \
-#     --with-prod=/fxrhino1/ATTO2/prod \
-#     --with-toolset=/base/toolset \
-#     --with-sql-server=fxlinux101 \
-#     --with-sql-port=53139 \
-#     --with-max-keys=40
-# popd
