93d71c78202d861a66167c522484d51b7bbfd3d0
Jim Callahan <jim@temerity.us>
2004-03-11 11:01:43 +0000

    initial revision

---
+++ src/java/us/temerity/pipeline/message/FileRequest.java
@@ -0,0 +1,59 @@
+// $Id: FileRequest.java,v 1.1 2004/03/11 11:01:43 jim Exp $
+
+package us.temerity.pipeline;
+
+/*------------------------------------------------------------------------------------------*/
+/*   F I L E   R E Q U E S T                                                                */
+/*------------------------------------------------------------------------------------------*/
+
+/**
+ * The set of identifiers for file request messages which may be sent over a network 
+ * connection from the {@link FileMgrClient FileMgrClient} to the 
+ * {@link FileMgrServer FileMgrServer}.  <P> 
+ * 
+ * The protocol of communicaton between these file manager classes is for a 
+ * <CODE>FileRequest</CODE> to be sent followed by a corresponding <CODE>File*Req</CODE> 
+ * object which contains the needed request data.  The purpose of this enumeration is to 
+ * make testing for which request is being send more efficient and cleaner on the 
+ * <CODE>FileMgrServer</CODE> side of the connection.
+ * 
+ * @see FileCheckInReq
+ * @see FileCheckOutReq
+ * @see FileCheckSumReq
+ * @see FileFreezeReq
+ * @see FileUnfreezeReq
+ * @see FileStateRsp
+ */
+public
+enum FileRequest
+{  
+  /**
+   * An instance of <CODE>FileCheckInReq</CODE> is next.
+   */
+  CheckIn, 
+  
+  /**
+   * An instance of <CODE>FileCheckOutReq</CODE> is next.
+   */
+  CheckOut, 
+  
+  /**
+   * An instance of <CODE>FileCheckSumReq</CODE> is next.
+   */
+  CheckSum, 
+  
+  /**
+   * An instance of <CODE>FileFreezeReq</CODE> is next.
+   */
+  Freeze, 
+  
+  /**
+   * An instance of <CODE>FileUnfreezeReq</CODE> is next.
+   */
+  Unfreeze, 
+  
+  /**
+   * An instance of <CODE>FileStateReq</CODE> is next.
+   */
+  State;  
+}
