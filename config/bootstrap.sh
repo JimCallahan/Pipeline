667ca4c3dd7d21403457de78f44a5a299d981d3e
Jim Callahan <jim@temerity.us>
2004-03-12 15:11:12 +0000

    changed --prefix/--datadir to remove obsolete "pipeline" subdir

--- config/bootstrap.sh
+++ config/bootstrap.sh
@@ -11,8 +11,8 @@ pushd i686-pc-linux-gnu-dbg
   $HOME/code2/src/pipeline/configure \
     --disable-opt \
     --with-compiler=GNU \
-    --prefix=/base/apps/i686-pc-linux-gnu-dbg/pipeline-040211/pipeline \
-    --datadir=/base/apps/i686-pc-linux-gnu-dbg/pipeline-040211/pipeline/share \
+    --prefix=/base/apps/i686-pc-linux-gnu-dbg/pipeline-040211 \
+    --datadir=/base/apps/i686-pc-linux-gnu-dbg/pipeline-040211/share \
     --with-prod=/base/prod \
     --with-toolset=/base/toolset \
     --with-pl-server=localhost \
