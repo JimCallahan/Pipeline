bd04c89d04237a29da8b527baca72c67f96fb114
Jim Callahan <jim@temerity.us>
2004-03-21 00:57:17 +0000

    now fully uses CryptoApp and the customer profile to generate configure params

--- config/bootstrap.sh
+++ config/bootstrap.sh
@@ -7,14 +7,11 @@ pushd $HOME/code2/src/pipeline
   sh autogen.sh
 popd
 
+
 pushd i686-pc-linux-gnu-dbg
   $HOME/code2/src/pipeline/configure \
     --disable-opt \
     --with-compiler=GNU \
-    --prefix=/base/apps/i686-pc-linux-gnu-dbg/pipeline-040211 \
-    --datadir=/base/apps/i686-pc-linux-gnu-dbg/pipeline-040211/share \
-    --with-prod=/base/prod \
-    --with-toolset=/base/toolset \
-    --with-pl-server=localhost \
-    --with-pl-port=53135
+    --with-crypto-app=$HOME/code2/src/pipeline/plconfig \
+    --with-customer-profile=/base/apps/i686-pc-linux-gnu-dbg/pipeline-040319/pipeline.profile
 popd
