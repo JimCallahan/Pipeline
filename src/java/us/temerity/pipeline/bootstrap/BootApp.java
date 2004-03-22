0a036e7c12c152a008bc8063dd17ffdf1d31bb3e
Jim Callahan <jim@temerity.us>
2004-03-22 22:52:53 +0000

    moved to bootstrap package

--- src/java/us/temerity/pipeline/bootstrap/BootApp.java
+++ src/java/us/temerity/pipeline/bootstrap/BootApp.java
@@ -1,13 +1,17 @@
-// $Id: BootApp.java,v 1.1 2004/03/22 03:12:53 jim Exp $
+// $Id: BootApp.java,v 1.1 2004/03/22 22:52:53 jim Exp $
 
-package us.temerity.pipeline;
+package us.temerity.pipeline.bootstrap;  
 
 /*------------------------------------------------------------------------------------------*/
 /*   B O O T   A P P                                                                        */
 /*------------------------------------------------------------------------------------------*/
 
 /**
- * An abstract base class used to bootstrap all Pipeline applications
+ * An abstract base class used to bootstrap all Pipeline applications.
+ * 
+ * Any application which requires access to internal Pipeline classes which are license 
+ * protected must be launched by a subclass of this class.  See the {@link Main Main} class
+ * for details about how this works.
  */ 
 public abstract 
 class BootApp
@@ -29,7 +33,11 @@ class BootApp
   /*----------------------------------------------------------------------------------------*/
 
   /**
-   * Run the application with the given command-line arguments.
+   * Run the application with the given command-line arguments. <P> 
+   * 
+   * Subclasses should implement this method as the top-level entry function for the 
+   * Pipeline application.  This method performs the same function as the standard 
+   * <CODE>main(String[])</CODE> function of general Java applications.
    * 
    * @param args [<B>in</B>]
    *   The command-line arguments.
