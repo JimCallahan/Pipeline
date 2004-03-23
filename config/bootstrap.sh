8a26516e003ed229b17180aab4798525946ed0c4
Jim Callahan <jim@temerity.us>
2004-03-23 03:30:58 +0000

    moved config files into the <root-dir>/config directory

--- config/bootstrap.sh
+++ config/bootstrap.sh
@@ -7,11 +7,12 @@ pushd $HOME/code2/src/pipeline
   sh autogen.sh
 popd
 
+plprofile=/base/apps/i686-pc-linux-gnu-dbg/pipeline-040319/config/pipeline.profile
 
 pushd i686-pc-linux-gnu-dbg
   $HOME/code2/src/pipeline/configure \
     --disable-opt \
     --with-compiler=GNU \
     --with-crypto-app=$HOME/code2/src/pipeline/plconfig \
-    --with-customer-profile=/base/apps/i686-pc-linux-gnu-dbg/pipeline-040319/pipeline.profile
+    --with-customer-profile=$plprofile
 popd
