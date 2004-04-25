ccba71fb190db2e9c3a49cab7f542bd1e896e8e2
Jim Callahan <jim@temerity.us>
2004-04-25 13:47:48 +0000

    initial revision

---
+++ src/java/us/temerity/pipeline/laf/PipelineLookAndFeelLoader.java
@@ -0,0 +1,15 @@
+// $Id: PipelineLookAndFeelLoader.java,v 1.1 2004/04/25 13:47:48 jim Exp $
+
+package us.temerity.pipeline.laf;
+
+/*------------------------------------------------------------------------------------------*/
+/*   P I P E L I N E   L O O K   A N D   F E E L   L O A D E R                              */
+/*------------------------------------------------------------------------------------------*/
+
+/**
+ * Used only for convenient loading of the Pipeline look-and-feel resources.
+ */
+public
+class PipelineLookAndFeelLoader
+{  
+}
