14399c1e03112b90b774cf8efcc827df25b42543
Jim Callahan <jim@temerity.us>
2004-05-14 02:36:26 +0000

    initial revision

---
+++ src/java/us/temerity/pipeline/ui/HotKey.java
@@ -0,0 +1,264 @@
+// $Id: HotKey.java,v 1.1 2004/05/14 02:36:26 jim Exp $
+
+package us.temerity.pipeline.ui;
+
+import us.temerity.pipeline.glue.*;
+
+import java.awt.event.*;
+
+/*------------------------------------------------------------------------------------------*/
+/*   H O T   K E Y                                                                          */
+/*------------------------------------------------------------------------------------------*/
+
+/**
+ * A keyboard event which triggers the execution of an action. <P> 
+ */
+public 
+class HotKey
+  implements Glueable
+{
+  /*----------------------------------------------------------------------------------------*/
+  /*   C O N S T R U C T O R                                                                */
+  /*----------------------------------------------------------------------------------------*/
+
+  public 
+  HotKey() 
+  {
+    pKeyCode = KeyEvent.VK_UNDEFINED;
+  }
+
+  /**
+   * Constuct a new hot key.
+   */ 
+  public 
+  HotKey
+  (
+   boolean shiftDown, 
+   boolean altDown, 
+   boolean ctrlDown, 
+   int keyCode
+  ) 
+  {
+    pShiftDown = shiftDown;
+    pAltDown   = altDown;
+    pCtrlDown  = ctrlDown;
+
+    pKeyCode = keyCode;
+  }
+
+  /**
+   * Construct a hot key from a key event. <P> 
+   * 
+   * @param e
+   *   The key event passed as the parameter of the {@link KeyEvent#keyPressed keyPressed}
+   *   method.
+   */ 
+  public 
+  HotKey
+  (
+   KeyEvent e
+  ) 
+  {
+    int code = e.getKeyCode();
+    switch(code) {
+    case KeyEvent.VK_SHIFT:
+    case KeyEvent.VK_ALT:
+    case KeyEvent.VK_CONTROL:
+      throw new IllegalArgumentException
+	("The " + KeyEvent.getKeyText(code) + " key cannot be used as a hot key!");
+    }
+    
+    int mods = e.getModifiersEx();    
+    pShiftDown = ((mods & KeyEvent.SHIFT_DOWN_MASK) == KeyEvent.SHIFT_DOWN_MASK);
+    pAltDown   = ((mods & KeyEvent.ALT_DOWN_MASK)   == KeyEvent.ALT_DOWN_MASK);
+    pCtrlDown  = ((mods & KeyEvent.CTRL_DOWN_MASK)  == KeyEvent.CTRL_DOWN_MASK);
+    
+    pKeyCode = code;
+  }
+
+
+
+  /*----------------------------------------------------------------------------------------*/
+  /*   A C C E S S                                                                          */
+  /*----------------------------------------------------------------------------------------*/
+  
+  /**
+   * Whether the SHIFT key must be down when the key is pressed.
+   */ 
+  public boolean 
+  withShiftDown() 
+  {
+    return pShiftDown;
+  }
+
+  /**
+   * Whether the ALT key must be down when the key is pressed.
+   */ 
+  public boolean 
+  withAltDown() 
+  {
+    return pAltDown;
+  }
+
+  /**
+   * Whether the CTRL key must be down when the key is pressed.
+   */ 
+  public boolean 
+  withCtrlDown() 
+  {
+    return pCtrlDown;
+  }
+  
+  /**
+   * Get the key code as defined by {@link KeyEvent KeyEvent}.
+   */ 
+  public int
+  getKeyCode() 
+  {
+    return pKeyCode;
+  }
+
+
+
+  /*----------------------------------------------------------------------------------------*/
+  /*   C O M P A R I S O N                                                                  */
+  /*----------------------------------------------------------------------------------------*/
+
+  /**
+   * Does the given key event correspond to the hot key? <P> 
+   * 
+   * The <CODE>e</CODE> argument must be the parameter of the 
+   * {@link KeyEvent#keyPressed keyPressed} method.
+   */ 
+  public boolean 
+  wasPressed
+  (
+   KeyEvent e
+  ) 
+  {
+    int on  = 0;
+    int off = 0;
+
+    if(pShiftDown) 
+      on |= KeyEvent.SHIFT_DOWN_MASK;
+    else 
+      off |= KeyEvent.SHIFT_DOWN_MASK;
+    
+    if(pAltDown) 
+      on |= KeyEvent.ALT_DOWN_MASK;
+    else 
+      off |= KeyEvent.ALT_DOWN_MASK;
+    
+    if(pCtrlDown) 
+      on |= KeyEvent.CTRL_DOWN_MASK;
+    else 
+      off |= KeyEvent.CTRL_DOWN_MASK;
+    
+    if((e.getModifiersEx() & (on | off)) != on) 
+      return false;
+    
+    return (pKeyCode == e.getKeyCode());
+  }
+
+
+
+  /*----------------------------------------------------------------------------------------*/
+  /*   C O N V E R S I O N                                                                  */
+  /*----------------------------------------------------------------------------------------*/
+
+  /** 
+   * Convert the hot key into a human readable form.
+   */ 
+  public String
+  toString() 
+  {
+    StringBuffer buf = new StringBuffer();
+
+    if(pShiftDown) 
+      buf.append("SHIFT+");
+
+    if(pAltDown) 
+      buf.append("ALT+");
+
+    if(pCtrlDown) 
+      buf.append("CTRL+");
+
+    buf.append(KeyEvent.getKeyText(pKeyCode));
+
+    return (buf.toString());
+  }
+  
+  
+
+  /*----------------------------------------------------------------------------------------*/
+  /*   G L U E A B L E                                                                      */
+  /*----------------------------------------------------------------------------------------*/
+
+  public void 
+  toGlue
+  ( 
+   GlueEncoder encoder   
+  ) 
+    throws GlueException
+  {
+    encoder.encode("ShiftDown", pShiftDown);
+    encoder.encode("AltDown", pAltDown);
+    encoder.encode("CtrlDown", pCtrlDown);
+    encoder.encode("KeyCode", pKeyCode);
+  }
+
+  public void 
+  fromGlue
+  (
+   GlueDecoder decoder 
+  ) 
+    throws GlueException
+  {
+    Boolean shift = (Boolean) decoder.decode("ShiftDown");
+    if(shift == null) 
+      throw new GlueException("The \"ShiftDown\" was missing or (null)!");
+    pShiftDown = shift;
+
+    Boolean alt = (Boolean) decoder.decode("AltDown");
+    if(alt == null) 
+      throw new GlueException("The \"AltDown\" was missing or (null)!");
+    pAltDown = alt;
+
+    Boolean ctrl = (Boolean) decoder.decode("CtrlDown");
+    if(ctrl == null) 
+      throw new GlueException("The \"CtrlDown\" was missing or (null)!");
+    pCtrlDown = ctrl;
+
+    Integer keyCode = (Integer) decoder.decode("KeyCode");
+    if(keyCode == null) 
+      throw new GlueException("The \"KeyCode\" was missing or (null)!");
+    pKeyCode = keyCode;
+  }
+  
+
+
+  /*----------------------------------------------------------------------------------------*/
+  /*   I N T E R N A L S                                                                    */
+  /*----------------------------------------------------------------------------------------*/
+
+  /**
+   * Whether the SHIFT key must be down when the key is pressed.
+   */ 
+  private boolean  pShiftDown;
+
+  /**
+   * Whether the ALT key must be down when the key is pressed.
+   */ 
+  private boolean  pAltDown;
+
+  /**
+   * Whether the CTRL key must be down when the key is pressed.
+   */ 
+  private boolean  pCtrlDown;
+
+  /**
+   * The key code as defined by {@link KeyEvent KeyEvent}.
+   */ 
+  private int  pKeyCode;
+
+}
---
+++ src/java/us/temerity/pipeline/ui/JHotKeyField.java
@@ -0,0 +1,196 @@
+// $Id: JHotKeyField.java,v 1.1 2004/05/14 02:36:26 jim Exp $
+
+package us.temerity.pipeline.ui;
+
+import us.temerity.pipeline.glue.*;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+/*------------------------------------------------------------------------------------------*/
+/*   H O T   K E Y   F I E L D                                                              */
+/*------------------------------------------------------------------------------------------*/
+
+/**
+ * An editor of {@link HotKey HotKey} values.
+ */
+public 
+class JHotKeyField
+  extends JLabel
+  implements FocusListener, MouseListener, KeyListener
+{
+  public 
+  JHotKeyField()
+  {
+    super();  
+
+    setFocusable(true);
+    addFocusListener(this);
+    addMouseListener(this);
+    addKeyListener(this);
+
+    setText("-");
+  }
+
+
+  /*----------------------------------------------------------------------------------------*/
+  /*   A C C E S S                                                                          */
+  /*----------------------------------------------------------------------------------------*/
+ 
+  /**
+   * Set the hot key to display.
+   */ 
+  public void 
+  setHotKey
+  (
+   HotKey key
+  ) 
+  {
+    pHotKey = key;
+    setText((key != null) ? key.toString() : "-");
+  }
+
+  /**
+   * Get the hot key displayed.
+   * 
+   * @return 
+   *   The hot key or <CODE>null</CODE> if unset.
+   */ 
+  public HotKey
+  getHotKey() 
+  {
+    return pHotKey;
+  }
+
+
+  
+  /*----------------------------------------------------------------------------------------*/
+  /*   L I S T E N E R S                                                                    */
+  /*----------------------------------------------------------------------------------------*/
+
+  /*-- FOCUS LISTENER METHODS --------------------------------------------------------------*/
+
+  /**
+   * Invoked when a component gains the keyboard focus.
+   */ 
+  public void 	
+  focusGained
+  (
+   FocusEvent e
+  )
+  {
+    repaint();
+  }
+
+  /**
+   * Invoked when a component loses the keyboard focus.
+   */ 
+  public void 	
+  focusLost
+  (
+   FocusEvent e
+  )
+  {
+    repaint();
+  }
+
+
+  /*-- MOUSE LISTENER METHODS --------------------------------------------------------------*/
+
+  /**
+   * Invoked when the mouse button has been clicked (pressed and released) on a component. 
+   */ 
+  public void 
+  mouseClicked(MouseEvent e) {}
+    
+  /**
+   * Invoked when the mouse enters a component. 
+   */
+  public void 
+  mouseEntered(MouseEvent e) {}
+
+  /**
+   * Invoked when the mouse exits a component. 
+   */ 
+  public void 
+  mouseExited(MouseEvent e) {}
+
+  /**
+   * Invoked when a mouse button has been pressed on a component. 
+   */
+  public void 
+  mousePressed
+  (
+   MouseEvent e
+  )
+  {
+    requestFocusInWindow();
+  }
+
+  /**
+   * Invoked when a mouse button has been released on a component. 
+   */ 
+  public void 
+  mouseReleased(MouseEvent e) {}
+
+
+  /*-- KEY LISTENER METHODS ----------------------------------------------------------------*/
+
+  /**
+   * voked when a key has been pressed.
+   */   
+  public void 
+  keyPressed
+  (
+   KeyEvent e
+  )
+  {
+    switch(e.getKeyCode()) {
+    case KeyEvent.VK_SHIFT:
+    case KeyEvent.VK_ALT:
+    case KeyEvent.VK_CONTROL:
+      break;
+
+    case KeyEvent.VK_BACK_SPACE:
+      setHotKey(null);
+      KeyboardFocusManager.getCurrentKeyboardFocusManager().clearGlobalFocusOwner();
+      break;
+      
+    default:
+      setHotKey(new HotKey(e));    
+    }
+  }
+
+  /**
+   * Invoked when a key has been released.
+   */ 
+  public void 	
+  keyReleased(KeyEvent e) {}
+
+  /**
+   * Invoked when a key has been typed.
+   */ 
+  public void 	
+  keyTyped(KeyEvent e) {} 
+
+  
+
+  /*----------------------------------------------------------------------------------------*/
+  /*   S T A T I C   I N T E R N A L S                                                      */
+  /*----------------------------------------------------------------------------------------*/
+
+  private static final long serialVersionUID = 2977925171510698518L;
+
+
+  
+  /*----------------------------------------------------------------------------------------*/
+  /*   I N T E R N A L S                                                                    */
+  /*----------------------------------------------------------------------------------------*/
+
+  /**
+   * The hot key being displayed.
+   */ 
+  private HotKey  pHotKey;
+
+}
