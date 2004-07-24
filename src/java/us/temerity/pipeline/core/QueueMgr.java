0067bf3bac19fb0def100318c180c96c9c2f8b7d
Jim Callahan <jim@temerity.us>
2004-07-24 18:24:32 +0000

    added license key operations, added queue/job fields

--- src/java/us/temerity/pipeline/core/QueueMgr.java
+++ src/java/us/temerity/pipeline/core/QueueMgr.java
@@ -1,13 +1,14 @@
-// $Id: QueueMgr.java,v 1.1 2004/07/21 07:15:01 jim Exp $
+// $Id: QueueMgr.java,v 1.2 2004/07/24 18:24:32 jim Exp $
 
 package us.temerity.pipeline.core;
 
+import us.temerity.pipeline.glue.*;
 import us.temerity.pipeline.message.*;
 import us.temerity.pipeline.*;
 
 import java.io.*;
 import java.util.*;
-import java.util.concurrent.locks.*;
+import java.util.concurrent.*;
 
 /*------------------------------------------------------------------------------------------*/
 /*   Q U E U E   M G R                                                                      */
@@ -47,8 +48,6 @@ class QueueMgr
    int jobPort
   )
   { 
-    pMakeDirLock = new Object();
-
     if(dir == null)
       throw new IllegalArgumentException("The root queue directory cannot be (null)!");
     pQueueDir = dir;
@@ -56,13 +55,379 @@ class QueueMgr
     if(jobPort < 0) 
       throw new IllegalArgumentException("Illegal port number (" + jobPort + ")!");
     pJobPort = jobPort;
+
+    init();
+  }
+
+
+  /*-- CONSTRUCTION HELPERS ----------------------------------------------------------------*/
+
+  /**
+   * Initialize a new instance.
+   */ 
+  private void 
+  init()
+  { 
+    /* initialize the fields */ 
+    {
+      pMakeDirLock = new Object();
+      pLicenseKeys = new TreeMap<String,LicenseKey>();
+    }
+
+    makeRootDirs();
+
+    /* create the lock file */ 
+    {
+      File file = new File(pQueueDir, "queue/lock");
+      if(file.exists()) 
+	throw new IllegalStateException
+	  ("Another queue manager is already running!\n" + 
+	   "If you are certain this is not the case, remove the lock file (" + file + ")!");
+
+      try {
+	FileWriter out = new FileWriter(file);
+	out.close();
+      }
+      catch(IOException ex) {
+	throw new IllegalStateException
+	  ("Unable to create lock file (" + file + ")!");
+      }
+    }
+
+    /* load the license keys if any exist */ 
+    try {
+      readLicenseKeys();
+    }
+    catch(PipelineException ex) {
+      throw new IllegalArgumentException(ex.getMessage());
+    }
+  }
+
+  /**
+   * Make sure that the root queue directories exist.
+   */ 
+  private void 
+  makeRootDirs() 
+  {
+    if(!pQueueDir.isDirectory()) 
+      throw new IllegalArgumentException
+	("The root node directory (" + pQueueDir + ") does not exist!");
+    
+    ArrayList<File> dirs = new ArrayList<File>();
+    dirs.add(new File(pQueueDir, "queue"));
+    dirs.add(new File(pQueueDir, "queue/hosts"));
+    dirs.add(new File(pQueueDir, "queue/jobs"));
+    dirs.add(new File(pQueueDir, "queue/job-info"));
+    dirs.add(new File(pQueueDir, "queue/job-groups"));
+
+    synchronized(pMakeDirLock) {
+      for(File dir : dirs) {
+	if(!dir.isDirectory())
+	  if(!dir.mkdirs()) 
+	    throw new IllegalArgumentException
+	      ("Unable to create the directory (" + dir + ")!");
+      }
+    }
+  }
+
+
+  /*----------------------------------------------------------------------------------------*/
+  /*   S H U T D O W N                                                                      */
+  /*----------------------------------------------------------------------------------------*/
+
+  /**
+   * Shutdown the node manager. <P> 
+   * 
+   * Also sends a shutdown request to all of the <B>pljobmgr</B>(1) daemons for which there
+   * are live network connections. <P> 
+   * 
+   * It is crucial that this method be called when only a single thread is able to access
+   * this instance!  In other words, after all request threads have already exited or by a 
+   * restart during the construction of this instance.
+   */
+  public void  
+  shutdown() 
+  {
+
+    // ... 
+
+    /* remove the lock file */ 
+    {
+      File file = new File(pQueueDir, "queue/lock");
+      file.delete();
+    }
+  }
+
+
+  /*----------------------------------------------------------------------------------------*/
+  /*   L I C E N S E   K E Y S                                                              */
+  /*----------------------------------------------------------------------------------------*/
+
+  /**
+   * Get the names of the currently defined license keys. <P>  
+   * 
+   * @return
+   *   <CODE>QueueGetLicenseKeyNamesRsp</CODE> if successful or 
+   *   <CODE>FailureRsp</CODE> if unable to get the key names.
+   */
+  public Object
+  getLicenseKeyNames() 
+  {
+    TaskTimer timer = new TaskTimer();
+    timer.aquire();
+    synchronized(pLicenseKeys) {
+      timer.resume();
+      
+      TreeSet<String> names = new TreeSet<String>(pLicenseKeys.keySet());
+      
+      return new QueueGetLicenseKeyNamesRsp(timer, names);
+    }
+  }
+
+  /**
+   * Get the currently defined license keys. <P>  
+   * 
+   * @return
+   *   <CODE>QueueGetLicenseKeysRsp</CODE> if successful or 
+   *   <CODE>FailureRsp</CODE> if unable to get the keys.
+   */
+  public Object
+  getLicenseKeys() 
+  {
+    TaskTimer timer = new TaskTimer();
+    timer.aquire();
+    synchronized(pLicenseKeys) {
+      timer.resume();
+      
+      ArrayList<LicenseKey> keys = new ArrayList<LicenseKey>(pLicenseKeys.values());
+      
+      return new QueueGetLicenseKeysRsp(timer, keys);
+    }
+  }
+
+  /**
+   * Add the given license key to the currently defined license keys. <P> 
+   * 
+   * If a license key already exists which has the same name as the given key, it will be 
+   * silently overridden by this operation. <P> 
+   * 
+   * @param req
+   *   The request.
+   * 
+   * @return
+   *   <CODE>SuccessRsp</CODE> if successful or 
+   *   <CODE>FailureRsp</CODE> if unable to add the key.
+   */ 
+  public Object
+  addLicenseKey
+  (
+   QueueAddLicenseKeyReq req
+  ) 
+  {
+    LicenseKey key = req.getLicenseKey();
+
+    TaskTimer timer = new TaskTimer("QueueMgr.addLicenseKey(): " + key.getName());
+    timer.aquire();
+    try {
+      synchronized(pLicenseKeys) {
+	timer.resume();
+
+	pLicenseKeys.put(key.getName(), key);
+	writeLicenseKeys();
+
+	return new SuccessRsp(timer);
+      }
+    }
+    catch(PipelineException ex) {
+      return new FailureRsp(timer, ex.getMessage());	  
+    }
+  }
+
+  /**
+   * Remove the license key with the given name from currently defined license keys. <P> 
+   * 
+   * @param req
+   *   The request.
+   * 
+   * @return
+   *   <CODE>SuccessRsp</CODE> if successful or 
+   *   <CODE>FailureRsp</CODE> if unable to remove the key.
+   */ 
+  public Object
+  removeLicenseKey
+  (
+   QueueRemoveLicenseKeyReq req
+  ) 
+  {
+    String kname = req.getKeyName();
+
+    TaskTimer timer = new TaskTimer("QueueMgr.removeLicenseKey(): " + kname); 
+    timer.aquire();
+    try {
+      synchronized(pLicenseKeys) {
+	timer.resume();
+	
+	pLicenseKeys.remove(kname);
+	writeLicenseKeys();
+
+	return new SuccessRsp(timer);
+      }
+    }
+    catch(PipelineException ex) {
+      return new FailureRsp(timer, ex.getMessage());	  
+    }
+  }  
+  
+  /**
+   * Set the total number of licenses associated with the named license key. <P> 
+   * 
+   * @param req
+   *   The request.
+   * 
+   * @return
+   *   <CODE>SuccessRsp</CODE> if successful or 
+   *   <CODE>FailureRsp</CODE> if unable to set the total licenses.
+   */ 
+  public Object
+  setTotalLicenses
+  (
+   QueueSetTotalLicensesReq req
+  ) 
+  {
+    String kname = req.getKeyName();
+    int total = req.getTotal();
+
+    TaskTimer timer = 
+      new TaskTimer("QueueMgr.setTotalLicenses(): " + kname + "[" + total + "]"); 
+    timer.aquire();
+    try {
+      synchronized(pLicenseKeys) {
+	timer.resume();
+	
+	LicenseKey key = pLicenseKeys.get(kname);
+	if(key == null) 
+	  throw new PipelineException
+	    ("Unable to set the total number of licenses because no license key " + 
+	     "named (" + kname + ") exists!");
+	
+	try {
+	  key.setTotal(total);
+	}
+	catch(IllegalArgumentException ex) {
+	  throw new PipelineException(ex.getMessage());	  
+	}   
+
+	writeLicenseKeys();
+
+	return new SuccessRsp(timer);
+      }
+    }
+    catch(PipelineException ex) {
+      return new FailureRsp(timer, ex.getMessage());	  
+    }   
   }
 
 
 
+
   /*----------------------------------------------------------------------------------------*/
-  /*   O P S                                                                                */
+  /*   I / O   H E L P E R S                                                                */
   /*----------------------------------------------------------------------------------------*/
+
+  /**
+   * Write the license keys to disk. <P> 
+   * 
+   * @throws PipelineException
+   *   If unable to write the license keys file.
+   */ 
+  private void 
+  writeLicenseKeys() 
+    throws PipelineException
+  {
+    synchronized(pLicenseKeys) {
+      File file = new File(pQueueDir, "queue/license-keys");
+      if(file.exists()) {
+	if(!file.delete())
+	  throw new PipelineException
+	    ("Unable to remove the old license keys file (" + file + ")!");
+      }
+      
+      if(!pLicenseKeys.isEmpty()) {
+	Logs.ops.finer("Writing License Keys.");
+
+	try {
+	  String glue = null;
+	  try {
+	    ArrayList<LicenseKey> keys = new ArrayList<LicenseKey>(pLicenseKeys.values());
+	    GlueEncoder ge = new GlueEncoderImpl("LicenseKeys", keys);
+	    glue = ge.getText();
+	  }
+	  catch(GlueException ex) {
+	    Logs.glu.severe
+	      ("Unable to generate a Glue format representation of the license keys!");
+	    Logs.flush();
+	    
+	    throw new IOException(ex.getMessage());
+	  }
+	  
+	  {
+	    FileWriter out = new FileWriter(file);
+	    out.write(glue);
+	    out.flush();
+	    out.close();
+	  }
+	}
+	catch(IOException ex) {
+	  throw new PipelineException
+	    ("I/O ERROR: \n" + 
+	     "  While attempting to write the license keys file (" + file + ")...\n" + 
+	     "    " + ex.getMessage());
+	}
+      }
+    }
+  }
+  
+  /**
+   * Read the license keys from disk. <P> 
+   * 
+   * @throws PipelineException
+   *   If unable to read the license keys file.
+   */ 
+  private void 
+  readLicenseKeys() 
+    throws PipelineException
+  {
+    synchronized(pLicenseKeys) {
+      pLicenseKeys.clear();
+
+      File file = new File(pQueueDir, "queue/license-keys");
+      if(file.isFile()) {
+	Logs.ops.finer("Reading License Keys.");
+
+	ArrayList<LicenseKey> keys = null;
+	try {
+	  FileReader in = new FileReader(file);
+	  GlueDecoder gd = new GlueDecoderImpl(in);
+	  keys = (ArrayList<LicenseKey>) gd.getObject();
+	  in.close();
+	}
+	catch(Exception ex) {
+	  Logs.glu.severe
+	    ("The license keys file (" + file + ") appears to be corrupted!");
+	  Logs.flush();
+	  
+	  throw new PipelineException
+	    ("I/O ERROR: \n" + 
+	     "  While attempting to read the license keys file (" + file + ")...\n" + 
+	   "    " + ex.getMessage());
+	}
+	assert(keys != null);
+	
+	for(LicenseKey key : keys) 
+	  pLicenseKeys.put(key.getName(), key);
+      }
+    }
+  }
 
 
 
@@ -85,8 +450,103 @@ class QueueMgr
   /**
    * The network port listened to by the <B>pljobmgr</B>(1) daemon.
    */ 
-  private int pJobPort;
+  private int  pJobPort;
 
+
+  /*----------------------------------------------------------------------------------------*/
+
+  /**
+   * The cached table of license keys indexed by license key name.
+   * 
+   * Access to this field should be protected by a synchronized block.
+   */ 
+  private TreeMap<String,LicenseKey>  pLicenseKeys; 
+
+
+  /*----------------------------------------------------------------------------------------*/
+  
+  /**
+   * The jobs waiting on one or more source (upstream) jobs to complete before they can be 
+   * added to the ready queue.  This list also contains jobs newly submitted to the queue
+   * which may not have any source nodes. <P> 
+   * 
+   * No locking is required.
+   */ 
+  private ConcurrentLinkedQueue<QueueJob>  pWaiting;
+
+  /**
+   * The job which are ready to be run in decending priority order.  If a ready job has 
+   * any source (upstream) jobs, they all must have a QueueState of Finished before the 
+   * job will be added to this queue. <P> 
+   * 
+   * Access to this field should be protected by a synchronized block.
+   */ 
+  private LinkedList<QueueJob>  pReady;
+
+  /**
+   * The IDs of jobs which should be killed as soon as possible. 
+   * 
+   * No locking is required.
+   */
+  private ConcurrentLinkedQueue<Long>  pHitList;
+  
+
+  /**
+   * The table of all existing QueueJob's indexed by job ID.  If a job exists, its job ID
+   * will be a member of the key set of this table.  If the value for a valid key is 
+   * <CODE>null</CODE>, then the QueueJob is currently stored on disk only and needs to 
+   * be reloaded.  <P> 
+   * 
+   * Access to this field should be protected by a synchronized block.
+   */ 
+  private TreeMap<Long,QueueJob>  pJobs; 
+
+  /**
+   * The table of status information for all existing QueueJob's indexed by job ID.  
+   * If job information exists, its job ID will be a member of the key set of this table.  
+   * If the value for a valid key is <CODE>null</CODE>, then the QueueJob is currently 
+   * stored on disk only and needs to be reloaded. <P> 
+   * 
+   * Access to this field should be protected by a synchronized block.
+   */ 
+  private TreeMap<Long,QueueJobInfo>  pJobInfo; 
+
+
+  /**
+   * The IDs of the latest jobs associated with each working version indexed by node ID and 
+   * primary file name.  If the node ID for a working version is missing from the key
+   * set, then no jobs exist which are associated with that working version.  If the 
+   * job ID for a particular primary filename is missing then no job exists which will
+   * regenerate that file. <P> 
+   * 
+   * Note that it is possible for several files to be generated by the same QueueJob, 
+   * therefore job IDs may be repeated in per-file table for a particular working version 
+   * of a node.  Also, only the newest job for a particular file is stored in this table
+   * and the association with older jobs is not retained. <P> 
+   * 
+   * This table is always populated regardless of whether the job and/or job information 
+   * are currently loaded for a particular job. <P> 
+   * 
+   * Access to this field should be protected by a synchronized block.
+   */ 
+  private TreeMap<NodeID,TreeMap<String,Long>>  pNodeJobIDs;
+
+  
+  /**
+   * The groups of jobs indexed by job group ID. <P> 
+   * 
+   * Access to this field should be protected by a synchronized block.
+   */
+  private TreeMap<Long,QueueJobGroup>  pJobGroups; 
+
+  
+  /**
+   * The per-host resource and selection bias information indexed by fully resolved 
+   * host name.
+   * 
+   * Access to this field should be protected by a synchronized block.
+   */
+  private TreeMap<String,QueueHost>  pHosts; 
 
 }
 
