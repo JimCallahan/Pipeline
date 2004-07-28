ef86019ace91d9bd53b6201d6d331b3b137503a7
Jim Callahan <jim@temerity.us>
2004-07-28 19:13:57 +0000

    combined seperate results fields into QueueResults

--- src/java/us/temerity/pipeline/QueueJobInfo.java
+++ src/java/us/temerity/pipeline/QueueJobInfo.java
@@ -1,4 +1,4 @@
-// $Id: QueueJobInfo.java,v 1.1 2004/07/24 18:28:45 jim Exp $
+// $Id: QueueJobInfo.java,v 1.2 2004/07/28 19:13:57 jim Exp $
 
 package us.temerity.pipeline;
 
@@ -124,121 +124,18 @@ class QueueJobInfo
   }
   
   /**
-   * The literal command line used to execute the job's regeneration action.
+   * The results of executing the job's regeneration action.
    * 
    * @return 
-   *   The command line or <CODE>null</CODE> if the job was never executed.
+   *   The results or <CODE>null</CODE> if the job was never executed.
    */ 
-  public String 
-  getCommand() 
+  public QueueJobResults
+  getResults() 
   {
-    return pCommand;
-  }
-
-  /**
-   * The exit code of the subprocess generated to execute the job's regeneration action.
-   * 
-   * @return 
-   *   The exit code or <CODE>null</CODE> if the job was never executed.
-   */ 
-  public Integer 
-  getExitCode() 
-  {
-    return pExitCode;
+    return pResults; 
   }
   
 
-  /** 
-   * The number of seconds the regeneration action subprocess was running in user space.
-   * 
-   * @return 
-   *    The user seconds or <CODE>null</CODE> if the job was never executed.
-   */ 
-  public Double
-  getUserSecs() 
-  {
-    return pUserSecs;
-  }
-
-  /** 
-   * The number of seconds the regeneration action subprocess was running in system 
-   * (kernel) space.
-   * 
-   * @return
-   *   The system seconds or <CODE>null</CODE> if the job was never executed.
-   */ 
-  public Double  
-  getSystemSecs() 
-  {
-    return pSystemSecs;
-  }
-
-
-  /**
-   * The average resident memory size of the regeneration action subprocess in kilobytes.
-   * 
-   * @return 
-   *   The memory size or <CODE>null</CODE> if the job was never executed.
-   */ 
-  public Long
-  getAverageResidentSize() 
-  {
-    return pAvgResidentSize;
-  }
-
-  /**
-   * The maximum resident memory size of the regeneration action subprocess in kilobytes.
-   * 
-   * @return 
-   *    The memory size or <CODE>null</CODE> if the job was never executed.
-   */ 
-  public Long 
-  getMaximumResidentSize() 
-  {
-    return pMaxResidentSize;
-  }
-  
-
-  /**
-   * The average virtual memory size of the regeneration action subprocess in kilobytes.
-   * 
-   * @return 
-   *   The memory size or <CODE>null</CODE> if the job was never executed.
-   */ 
-  public Long
-  getAverageVirtualSize() 
-  {
-    return pAvgVirtualSize;
-  }
-
-  /**
-   * The maximum virtual memory size of the regeneration action subprocess in kilobytes.
-   * 
-   * @return 
-   *    The memory size or <CODE>null</CODE> if the job was never executed.
-   */ 
-  public Long 
-  getMaximumVirtualSize() 
-  {
-    return pMaxVirtualSize;
-  }
-  
-
-  /**
-   * The total number of hard page faults during execution of the regeneration action 
-   * subprocess. <P> 
-   * 
-   * A hard page fault is a memory fault that required I/O operations.
-   * 
-   * @return 
-   *   The number of page faults or <CODE>null</CODE> if the job was never executed.
-   */ 
-  public Long
-  getPageFaults() 
-  {
-    return pPageFaults; 
-  }
-  
 
   /*----------------------------------------------------------------------------------------*/
   /*   S T A G E S                                                                          */
@@ -249,15 +146,11 @@ class QueueJobInfo
    * 
    * @param hostname
    *   The full name of the host executing the job.
-   * 
-   * @param command
-   *   The literal command line used to execute the job's regeneration action.
    */ 
   public void 
   started
   (
-   String hostname, 
-   String command
+   String hostname
   ) 
   {
     if(hostname == null) 
@@ -265,18 +158,13 @@ class QueueJobInfo
 	("The hostname cannot be (null)!");
     pHostname = hostname; 
 
-    if(command == null) 
-      throw new IllegalArgumentException
-	("The command cannot be (null)!");
-    pCommand = command; 
-
     pStartedStamp = Dates.now();
     pState = QueueState.Running;
   }
   
-
   /**
-   * Records that the job was aborted (cancelled) prematurely by the user.
+   * Records that the job was aborted (cancelled) before it could be assigned to a 
+   * host for execution.
    */ 
   public void 
   aborted() 
@@ -286,60 +174,27 @@ class QueueJobInfo
   }
 
   /**
-   * Records that the job has exited normally.
+   * Records the results of executing the job's regeneration action.
    * 
-   * @param exitCode
-   *   The exit code of the subprocess.
-   * 
-   * @param userSecs 
-   *   The number of seconds the subprocess was running in user space.
-   * 
-   * @param systemSecs
-   *   The number of seconds the subprocess was running in system space.
-   * 
-   * @param avgResidentSize
-   *   The average resident memory size of the subprocess in kilobytes.
-   * 
-   * @param maxResidentSize
-   *   The maximum resident memory size of the subprocess in kilobytes.
-   * 
-   * @param avgVirtualSize
-   *   The average virtual memory size of the subprocess in kilobytes.
-   * 
-   * @param maxVirtualSize
-   *   The maximum virtual memory size of the subprocess in kilobytes.
-   * 
-   * @param pageFaults
-   *   The total number of hard page faults during execution of the subprocess.
+   * @param results
+   *   The execution results.
    */ 
   public void 
   exited
   (
-   int exitCode, 
-   double userSecs, 
-   double systemSecs, 
-   long avgResidentSize, 
-   long maxResidentSize, 
-   long avgVirtualSize, 
-   long maxVirtualSize, 
-   long pageFaults   
+   QueueJobResults results
   ) 
   {
-    pExitCode = exitCode;
-
-    pUserSecs   = userSecs;
-    pSystemSecs = systemSecs; 
-    
-    pAvgResidentSize = avgResidentSize;
-    pMaxResidentSize = maxResidentSize;
-
-    pAvgVirtualSize = avgVirtualSize;
-    pMaxVirtualSize = maxVirtualSize;
-
-    pPageFaults = pageFaults;
+    pResults = results; 
 
     pCompletedStamp = Dates.now();
-    pState = (pExitCode == SubProcess.SUCCESS) ? QueueState.Finished : QueueState.Failed;
+
+    if(pResults == null) 
+      pState = QueueState.Aborted;
+    else if(pResults.getExitCode() == SubProcess.SUCCESS)
+      pState = QueueState.Finished;
+    else 
+      pState = QueueState.Failed;
   }
 
 
@@ -369,37 +224,8 @@ class QueueJobInfo
 	encoder.encode("CompletedStamp", pCompletedStamp);
     }
     
-    {
-      if(pHostname != null) 
-	encoder.encode("Hostname", pHostname);
-      
-      if(pCommand != null) 
-	encoder.encode("Command", pCommand);
-            
-      if(pExitCode != null) 
-	encoder.encode("ExitCode", pExitCode);
-      
-      if(pUserSecs != null) 
-	encoder.encode("UserSecs", pUserSecs);
-      
-      if(pSystemSecs != null) 
-	encoder.encode("SystemSecs", pSystemSecs);
-      
-      if(pAvgResidentSize != null) 
-	encoder.encode("AvgResidentSize", pAvgResidentSize);
-      
-      if(pMaxResidentSize != null) 
-	encoder.encode("MaxResidentSize", pMaxResidentSize);
-
-      if(pAvgVirtualSize != null) 
-	encoder.encode("AvgVirtualSize", pAvgVirtualSize);
-      
-      if(pMaxVirtualSize != null) 
-	encoder.encode("MaxVirtualSize", pMaxVirtualSize);
-      
-      if(pPageFaults != null) 
-	encoder.encode("PageFaults", pPageFaults);
-    }
+    if(pResults != null) 
+      encoder.encode("Results", pResults);
   }
 
   public void 
@@ -445,61 +271,9 @@ class QueueJobInfo
     }
 
     {
-      String cmd = (String) decoder.decode("Command"); 
-      if(cmd != null) 
-	pCommand = cmd;
-    }
-    
-    {
-      Integer code = (Integer) decoder.decode("ExitCode"); 
-      if(code != null) 
-	pExitCode = code;
-    }
-
-
-    {
-      Double secs = (Double) decoder.decode("UserSecs"); 
-      if(secs != null) 
-	pUserSecs = secs;
-    }
-
-    {
-      Double secs = (Double) decoder.decode("SystemSecs"); 
-      if(secs != null) 
-	pSystemSecs = secs;
-    }
-
-    
-    {
-      Long size = (Long) decoder.decode("AvgResidentSize"); 
-      if(size != null) 
-	pAvgResidentSize = size;
-    }
-
-    {
-      Long size = (Long) decoder.decode("MaxResidentSize"); 
-      if(size != null) 
-	pMaxResidentSize = size;
-    }
-    
-
-    {
-      Long size = (Long) decoder.decode("AvgVirtualSize"); 
-      if(size != null) 
-	pAvgVirtualSize = size;
-    }
-
-    {
-      Long size = (Long) decoder.decode("MaxVirtualSize"); 
-      if(size != null) 
-	pMaxVirtualSize = size;
-    }
-
-    
-    {
-      Long faults = (Long) decoder.decode("PageFaults"); 
-      if(faults != null) 
-	pPageFaults = faults;
+      QueueJobResults results = (QueueJobResults) decoder.decode("Results"); 
+      if(results != null) 
+	pResults = results;
     }
   }
   
@@ -558,64 +332,10 @@ class QueueJobInfo
   private String pHostname;   
 
   /**
-   * The literal command line used to execute the job's regeneration action or 
-   * <CODE>null</CODE> if the job was never executed.
+   * The results of executing the job's regeneration action or <CODE>null</CODE> if the 
+   * job was never executed.
    */ 
-  private String pCommand;
-
-  /**
-   * The exit code of the subprocess generated to execute the job's regeneration action or
-   * <CODE>null</CODE> if the job was never executed.
-   */ 
-  private Integer pExitCode;
-
-
-  /** 
-   * The number of seconds the regeneration action subprocess was running in user space or
-   * <CODE>null</CODE> if the job was never executed.
-   */ 
-  private Double  pUserSecs;
-
-  /** 
-   * The number of seconds the regeneration action subprocess was running in system 
-   * (kernel) space or <CODE>null</CODE> if the job was never executed.
-   */ 
-  private Double  pSystemSecs;
-
-
-  /**
-   * The average resident memory size of the regeneration action subprocess in kilobytes or
-   * <CODE>null</CODE> if the job was never executed.
-   */ 
-  private Long  pAvgResidentSize;
-
-  /**
-   * The maximum resident memory size of the regeneration action subprocess in kilobytes or
-   * <CODE>null</CODE> if the job was never executed.
-   */ 
-  private Long  pMaxResidentSize;
-  
-
-  /**
-   * The average virtual memory size of the regeneration action subprocess in kilobytes or
-   * <CODE>null</CODE> if the job was never executed.
-   */ 
-  private Long  pAvgVirtualSize;
-
-  /**
-   * The maximum virtual memory size of the regeneration action subprocess in kilobytes or
-   * <CODE>null</CODE> if the job was never executed.
-   */ 
-  private Long  pMaxVirtualSize;
-  
-
-  /**
-   * The total number of hard page faults during execution of the regeneration action 
-   * subprocess or <CODE>null</CODE> if the job was never executed. <P> 
-   * 
-   * A hard page fault is a memory fault that required I/O operations.
-   */ 
-  private Long  pPageFaults; 
+  private QueueJobResults  pResults;
 
   
 }
