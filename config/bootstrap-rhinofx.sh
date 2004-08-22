4402b14a2f77cb0606d9affbf95ec5641d19aeaa
Jim Callahan <jim@temerity.us>
2004-08-22 23:04:41 +0000

    initial revision

---
+++ config/bootstrap-rhinofx.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+rm -rf i686-pc-linux-gnu-dbg
+mkdir  i686-pc-linux-gnu-dbg
+
+plsrcdir=$HOME/code-rhinofx/src/pipeline
+
+pushd $plsrcdir
+  sh autogen.sh
+popd
+
+plprofile=$plsrcdir/plconfig/customers/rhinofx/040820
+
+pushd i686-pc-linux-gnu-dbg
+  $plsrcdir/configure \
+    --disable-opt \
+    --with-compiler=GNU \
+    --with-crypto-app=$plsrcdir/plconfig \
+    --with-customer=rhinofx \
+    --with-customer-profile=$plprofile
+popd
+
+# pushd i686-pc-linux-gnu-opt
+#  $plsrcdir/configure \
+#     --ensable-opt \
+#     --with-compiler=GNU \
+#     --with-crypto-app=$plsrcdir/plconfig \
+#     --with-customer-profile=$plprofile
+# popd
