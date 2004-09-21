b871b4d01ed63041a1cadc8b9276bc414465841f
Jim Callahan <jim@temerity.us>
2004-09-21 23:50:54 +0000

    added working --set and --get-info

--- src/java/us/temerity/pipeline/core/ScriptApp.java
+++ src/java/us/temerity/pipeline/core/ScriptApp.java
@@ -1,4 +1,4 @@
-// $Id: ScriptApp.java,v 1.5 2004/09/20 05:39:43 jim Exp $
+// $Id: ScriptApp.java,v 1.6 2004/09/21 23:50:54 jim Exp $
 
 package us.temerity.pipeline.core;
 
@@ -730,6 +730,447 @@ class ScriptApp
 
 
   /*----------------------------------------------------------------------------------------*/
+  /*   W O R K I N G   N O D E   V E R S I O N   C O M M A N D S                            */
+  /*----------------------------------------------------------------------------------------*/
+  
+  /**
+   * Print a text representation of the given working version.
+   */ 
+  public void
+  printWorkingVersion
+  (
+   NodeID nodeID, 
+   TreeSet sections, 
+   MasterMgrClient mclient, 
+   QueueMgrClient qclient
+  ) 
+    throws PipelineException
+  {
+    NodeMod mod = mclient.getWorkingVersion(nodeID);
+    BaseAction action = mod.getAction();
+    JobReqs jreqs = mod.getJobRequirements();
+    
+    StringBuffer buf = new StringBuffer(); 
+    
+    buf.append
+      (tbar(80) + "\n" +
+       "Working Node      : " + mod.getName() + "\n" + 
+       "Primary Files     : " + mod.getPrimarySequence());
+    
+    if(sections.contains("version")) {
+      String vstr = "(pending)";
+      if(mod.getWorkingID() != null) 
+	vstr = mod.getWorkingID().toString();
+      
+      buf.append
+	("\n\n" + 
+	 pad("-- Versions ", '-', 80) + "\n" +
+	 "Revision Number   : " + vstr);
+    }
+    
+    if(sections.contains("prop")) {
+      buf.append
+	("\n\n" + 
+	 pad("-- Properties ", '-', 80) + "\n" +
+	 "Toolset           : " + mod.getToolset() + "\n" +
+	 "Editor            : " + mod.getEditor());
+    }
+       
+    if(sections.contains("action")) {
+      buf.append
+	("\n\n" + 
+	 pad("-- Regeneration Action ", '-', 80) + "\n");
+
+      if(action != null) {
+	buf.append
+	  ("Action            : " + action.getName() + "\n" +
+	   "Version           : v" + action.getVersionID() + "\n" + 
+	   "Enabled           : " + (mod.isActionEnabled() ? "YES" : "no"));
+	
+	if(action.hasSingleParams()) {
+	  buf.append("\n\n" +
+		     pad("-- Action Parameters ", '-', 80));
+	  for(String pname : action.getSingleLayout()) {
+	    buf.append("\n");	 
+	    if(pname != null) 
+	      buf.append(pad(pname, 18) + ": " + action.getSingleParamValue(pname));
+	  }
+	}
+
+	if(action.supportsSourceParams() && (action.getSourceNames().size() > 0)) {
+	  buf.append("\n\n" +
+		     pad("-- Per-Source Action Parameters ", '-', 80) + "\n");
+
+	  boolean first = true;
+	  for(String sname : action.getSourceNames()) {
+	    buf.append((first ? "" : "\n\n") + 
+		       "Source Node       : " + sname);
+	    first = false;
+
+	    for(String pname : action.getSourceLayout()) {
+	      buf.append
+		("\n" + 
+		 pad(pname, 18) + ": " + action.getSourceParamValue(sname, pname));
+	    }
+	  }
+	}
+      }
+      else {
+	buf.append
+	  ("Action            : (none)");	
+      }	 
+    }
+
+    if(sections.contains("jreq") && (action != null)) {
+      String batchSize = "-";
+      if(mod.getBatchSize() != null) 
+	batchSize = mod.getBatchSize().toString();
+
+      buf.append
+	("\n\n" +
+	 pad("-- Job Requirements ", '-', 80) + "\n" +
+	 "Overflow Policy   : " + mod.getOverflowPolicy() + "\n" + 
+	 "\n" +
+	 "Execution Method  : " + mod.getExecutionMethod() + "\n" + 
+	 "Batch Size        : " + batchSize + "\n" + 
+	 "\n" + 
+	 "Priority          : " + jreqs.getPriority() + "\n" + 
+	 "\n" + 
+	 "Maximum Load      : " + String.format("%1$.2f", jreqs.getMaxLoad()) + "\n" +
+	 "Minimum Memory    : " + formatLong(jreqs.getMinMemory()) + "\n" +
+	 "Minimum Disk      : " + formatLong(jreqs.getMinDisk()));
+    }
+
+    if(sections.contains("key") && (action != null)) {
+      {
+	buf.append
+	  ("\n\n" +
+	   pad("-- License Keys ", '-', 80));
+
+	Set<String> keys = jreqs.getLicenseKeys();
+	for(String kname : qclient.getLicenseKeyNames()) {
+	  buf.append
+	    ("\n" + 
+	     pad(kname, 18) + ": " + (keys.contains(kname) ? "YES" : "no"));
+	}
+      }
+
+      {
+	buf.append
+	  ("\n\n" +
+	   pad("-- Selection Keys ", '-', 80));
+
+	Set<String> keys = jreqs.getSelectionKeys();
+	for(String kname : qclient.getSelectionKeyNames()) {
+	  buf.append
+	    ("\n" + 
+	     pad(kname, 18) + ": " + (keys.contains(kname) ? "YES" : "no"));
+	}
+      }
+    }
+
+    if(sections.contains("link") && mod.hasSources()) {
+      buf.append
+	("\n\n" +
+	 pad("-- Upstream Links ", '-', 80));
+      
+      int wk = mod.getSourceNames().size();
+      for(String sname : mod.getSourceNames()) {
+	LinkMod link = mod.getSource(sname);
+	buf.append
+	  ("\n" +
+	   "Source Node       : " + sname + "\n" +
+	   "Link Policy       : " + link.getPolicy());
+
+	switch(link.getPolicy()) {
+	case Reference:
+	case Dependency:
+	  buf.append
+	    ("\n" + 
+	     "Link Relationship : " + link.getRelationship());
+	}
+	
+	switch(link.getRelationship()) {
+	case OneToOne:
+	  buf.append
+	    ("\n" + 
+	     "Frame Offset      : " + link.getFrameOffset()); 
+	}
+
+	if(wk > 0) 
+	  buf.append("\n");
+	wk--;
+      }
+    }
+
+    Logs.ops.info(buf.toString());
+    Logs.flush();    
+  }
+
+  /**
+   * Set the properties of the given working version.
+   */ 
+  public void 
+  workingVersionSet
+  (
+   NodeID nodeID, 
+   String toolset, 
+   String editor, 
+   Boolean noAction, 
+   String actionName, 
+   VersionID actionVersionID, 
+   Boolean actionEnabled, 
+   TreeMap params, 
+   TreeMap sourceParams, 
+   OverflowPolicy overflowPolicy,
+   ExecutionMethod executionMethod,
+   Integer batchSize,
+   Integer priority,
+   Float maxLoad,
+   Long minMemory,
+   Long minDisk,
+   TreeMap licenseKeys,
+   TreeMap selectionKeys,
+   MasterMgrClient client
+  ) 
+    throws PipelineException
+  {
+    NodeMod mod = client.getWorkingVersion(nodeID);
+    try {
+      /* properties */ 
+      {
+	if(toolset != null) {
+	  if(!client.getToolsetNames().contains(toolset)) 
+	    throw new PipelineException 
+	      ("No toolset named (" + toolset + ") exists!");
+	  mod.setToolset(toolset);
+	}
+	
+	if(editor != null) {
+	  if(!PluginMgr.getInstance().getEditors().keySet().contains(editor))
+	    throw new PipelineException 
+	      ("No Editor plugin named (" + editor + ") exists!");
+	  mod.setEditor(editor);
+	}
+      }
+
+      /* previous job requirements (if any) */ 
+      JobReqs prevJobReqs = mod.getJobRequirements();
+      OverflowPolicy prevOverflowPolicy = mod.getOverflowPolicy();
+      ExecutionMethod prevExecutionMethod = mod.getExecutionMethod();
+      Integer prevBatchSize = mod.getBatchSize(); 
+
+      /* actions */ 
+      if((noAction != null) && noAction) {
+	mod.setAction(null);	
+      }
+      else {
+	if(actionName != null) {
+	  TreeSet<VersionID> table = PluginMgr.getInstance().getActions().get(actionName);
+	  if(table == null) 
+	    throw new PipelineException 
+	      ("No Action plugin named (" + actionName + ") exists!");
+	  
+	  if((actionVersionID != null) && !table.contains(actionVersionID)) 
+	    throw new PipelineException 
+	      ("No version (v" + actionVersionID + ") of Action plugin " + 
+	       "(" + actionName + ") exists!");
+
+	  BaseAction action = PluginMgr.getInstance().newAction(actionName, actionVersionID);
+	  mod.setAction(action);
+	}
+	
+	if(actionEnabled != null) 
+	  mod.setActionEnabled(actionEnabled);
+	
+	/* action parameters */ 
+	if(!params.isEmpty() || !sourceParams.isEmpty()) {
+	  BaseAction action = mod.getAction();
+	  if(action == null) 
+	    throw new PipelineException
+	      ("No node (" + nodeID.getName() + ") has no Action plugin and therefore " + 
+	       "cannot have its parameters set!");
+	  
+	  TreeMap<String,String> pparams = (TreeMap<String,String>) params;
+	  for(String pname : pparams.keySet()) {
+	    String value = pparams.get(pname);
+	    try {
+	      BaseActionParam aparam = action.getSingleParam(pname);
+	      if(aparam == null)
+		throw new PipelineException 
+		  ("No parameter named (" + pname + ") exists for Action " + 
+		   "(" + action.getName() + ")!");
+
+	      if(aparam instanceof BooleanActionParam) {
+		action.setSingleParamValue(pname, new Boolean(value));
+	      }
+	      else if(aparam instanceof IntegerActionParam) {
+		action.setSingleParamValue(pname, new Integer(value));
+	      }
+	      else if(aparam instanceof DoubleActionParam) {
+		action.setSingleParamValue(pname, new Double(value));
+	      }
+	      else if(aparam instanceof StringActionParam) {
+		action.setSingleParamValue(pname, value);
+	      }
+	      else if(aparam instanceof LinkActionParam) {
+		if(!mod.getSourceNames().contains(value))
+		  throw new PipelineException
+		    ("The node (" + value + ") is not an upstream source of node " + 
+		     "(" + nodeID.getName() + ") and is therefore not legal for the value " + 
+		     "of parameter (" + pname + ")!");
+		
+		action.setSingleParamValue(pname, value);
+	      }
+	      else if(aparam instanceof EnumActionParam) {
+		EnumActionParam eparam = (EnumActionParam) aparam;
+		if(!eparam.getValues().contains(value))
+		  throw new PipelineException
+		    ("The value (" + value + ") is not one of the enumerations of " +
+		     "parameter (" + pname + ")!");
+		
+		action.setSingleParamValue(pname, value);
+	      }
+	    }
+	    catch(NumberFormatException ex) {
+	      throw new PipelineException
+		("The value (" + value + ") is not legal for parameter (" + pname + ")!");
+	    }
+	  }
+	  
+	  TreeMap<String,TreeMap<String,String>> ssourceParams = 
+	    (TreeMap<String,TreeMap<String,String>>) sourceParams;
+	  for(String sname : ssourceParams.keySet()) {
+	    if(!mod.getSourceNames().contains(sname)) 
+	      throw new PipelineException
+		("The node (" + sname + ") is not an upstream source of node " + 
+		 "(" + nodeID.getName() + ") and therefore cannot have per-source " + 
+		 "parameters!");
+	    
+	    TreeMap<String,String> sparams = ssourceParams.get(sname);
+	    for(String pname : sparams.keySet()) {
+	      if(!action.hasSourceParams(sname)) 
+		action.initSourceParams(sname);
+	      
+	      String value = sparams.get(pname);
+	      try {
+		BaseActionParam aparam = action.getSourceParam(sname, pname);
+		if(aparam instanceof BooleanActionParam) {
+		  action.setSourceParamValue(sname, pname, new Boolean(value));
+		}
+		else if(aparam instanceof IntegerActionParam) {
+		  action.setSourceParamValue(sname, pname, new Integer(value));
+		}
+		else if(aparam instanceof DoubleActionParam) {
+		  action.setSourceParamValue(sname, pname, new Double(value));
+		}
+		else if(aparam instanceof StringActionParam) {
+		  action.setSourceParamValue(sname, pname, value);
+		}
+		else if(aparam instanceof LinkActionParam) {
+		  if(!mod.getSourceNames().contains(value))
+		    throw new PipelineException
+		      ("The node (" + value + ") is not an upstream source of node " + 
+		       "(" + nodeID.getName() + ") and is therefore not legal for the " + 
+		       "value of per-source parameter (" + pname + ")!");
+		  
+		  action.setSourceParamValue(sname, pname, value);
+		}
+		else if(aparam instanceof EnumActionParam) {
+		  EnumActionParam eparam = (EnumActionParam) aparam;
+		  if(!eparam.getValues().contains(value))
+		    throw new PipelineException
+		      ("The value (" + value + ") is not one of the enumerations of " +
+		       "per-source parameter (" + pname + ")!");
+		  
+		  action.setSourceParamValue(sname, pname, value);
+		}
+	      }
+	      catch(NumberFormatException ex) {
+		throw new PipelineException
+		  ("The value (" + value + ") is not legal for per-source parameter " +
+		   "(" + pname + ")!");
+	      }
+	    }
+	  }
+	  
+	  mod.setAction(action);
+	}
+	
+	/* job requirements */ 
+	{
+	  if(overflowPolicy != null) 
+	    mod.setOverflowPolicy(overflowPolicy);
+	  else if(prevOverflowPolicy != null)
+	    mod.setOverflowPolicy(prevOverflowPolicy);
+	  
+	  if(executionMethod != null) 
+	    mod.setExecutionMethod(executionMethod);
+	  else if(prevExecutionMethod != null)
+	    mod.setExecutionMethod(prevExecutionMethod);
+
+	  if(batchSize != null) 
+	    mod.setBatchSize(batchSize);
+	  else if((prevBatchSize != null) && 
+		  (mod.getExecutionMethod() == ExecutionMethod.Parallel))
+	    mod.setBatchSize(prevBatchSize);
+	  
+	  JobReqs jreqs = mod.getJobRequirements();
+	  if(prevJobReqs != null) 
+	    jreqs = prevJobReqs;
+	  
+	  if(priority != null) 
+	    jreqs.setPriority(priority);
+
+	  if(maxLoad != null) 
+	    jreqs.setMaxLoad(maxLoad);
+	  
+	  if(minMemory != null) 
+	    jreqs.setMinMemory(minMemory);
+	  
+	  if(minDisk != null) 
+	    jreqs.setMinDisk(minDisk);
+	  
+	  {
+	    TreeMap<String,Boolean> keys = (TreeMap<String,Boolean>) licenseKeys;
+	    for(String kname : keys.keySet()) {
+	      boolean hasKey = keys.get(kname); 
+	      if(hasKey) 
+		jreqs.addLicenseKey(kname);
+	      else 
+	      jreqs.removeLicenseKey(kname);
+	    }
+	  }
+	  
+	  {
+	    TreeMap<String,Boolean> keys = (TreeMap<String,Boolean>) selectionKeys;
+	    for(String kname : keys.keySet()) {
+	      boolean hasKey = keys.get(kname); 
+	      if(hasKey) 
+		jreqs.addSelectionKey(kname);
+	      else 
+	      jreqs.removeSelectionKey(kname);
+	    }
+	  }
+	  
+	  mod.setJobRequirements(jreqs);
+	}
+      }
+    }
+    catch(IllegalArgumentException ex) {
+      throw new PipelineException(ex.getMessage());
+    }
+
+    client.modifyProperties(nodeID.getAuthor(), nodeID.getView(), mod);
+  }
+
+  /**
+   *
+   */ 
+  
+
+
+  /*----------------------------------------------------------------------------------------*/
   /*   H E L P E R S                                                                        */
   /*----------------------------------------------------------------------------------------*/
   
@@ -782,6 +1223,7 @@ class ScriptApp
     case ScriptOptsParserConstants.UNKNOWN6:
     case ScriptOptsParserConstants.UNKNOWN7:
     case ScriptOptsParserConstants.UNKNOWN8:
+    case ScriptOptsParserConstants.UNKNOWN9:
       return "an unknown argument";
 
     case ScriptOptsParserConstants.UNKNOWN_OPTION1:
@@ -792,13 +1234,32 @@ class ScriptApp
     case ScriptOptsParserConstants.UNKNOWN_OPTION6:
     case ScriptOptsParserConstants.UNKNOWN_OPTION7:
     case ScriptOptsParserConstants.UNKNOWN_OPTION8:
+    case ScriptOptsParserConstants.UNKNOWN_OPTION9:
       return "an unknown option";
+
+    case ScriptOptsParserConstants.TRUE:
+      return "\"true\"";
+
+    case ScriptOptsParserConstants.FALSE:
+      return "\"false\"";
+
+    case ScriptOptsParserConstants.INTEGER:
+      return "an integer";
 
     case ScriptOptsParserConstants.PORT_NUMBER:
       return "a port number";
 
-    case ScriptOptsParserConstants.INTEGER:
-      return "an integer";
+    case ScriptOptsParserConstants.BYTE_SIZE:
+      return "a byte size";
+
+    case ScriptOptsParserConstants.KILO:
+      return "\"K\" kilobytes";
+
+    case ScriptOptsParserConstants.MEGA:
+      return "\"M\" megabytes";
+
+    case ScriptOptsParserConstants.GIGA:
+      return "\"G\" gigabytes";
 
     case ScriptOptsParserConstants.STRING:
       return "an string";
@@ -812,30 +1273,68 @@ class ScriptApp
     case ScriptOptsParserConstants.HOST_NAME:
       return "a hostname";
       
-    case ScriptOptsParserConstants.NODE_ARG:
+    case ScriptOptsParserConstants.NODE_NAME:
       return "a fully resolved node name";
       
-    case ScriptOptsParserConstants.USER_NAME:
-      return "a user name";
-
     case ScriptOptsParserConstants.TOOLSET_NAME:
       return "a toolset name";
 
+    case ScriptOptsParserConstants.USER_NAME:
+      return "a user name";
+
     case ScriptOptsParserConstants.KEY_BIAS_NAME:
-    case ScriptOptsParserConstants.KEY_NAME:
+    case ScriptOptsParserConstants.KEY_NAME1:
+    case ScriptOptsParserConstants.KEY_NAME2:
       return "a key name";
 
     case ScriptOptsParserConstants.KEY_BIAS:
       return "a selection bias";
 
-    case ScriptOptsParserConstants.SUFFIX:
+    case ScriptOptsParserConstants.SUFFIX1:
       return "a filename suffix";
 
     case ScriptOptsParserConstants.EDITOR_NAME:
       return "an Editor plugin name";
 
+    case ScriptOptsParserConstants.ACTION_NAME:
+      return "an Action plugin name";
+
+    case ScriptOptsParserConstants.REVISION_NUMBER:
+      return "a revision number";
+
     case ScriptOptsParserConstants.VIEW_NAME:
       return "a working area view name";
+
+    case ScriptOptsParserConstants.PARAM_NAME: 
+      return "an Action parameter name";
+
+    case ScriptOptsParserConstants.PARAM_VALUE: 
+      return "an Action parameter value";
+      
+    case ScriptOptsParserConstants.EMPTY_PARAM_VALUE:
+      return "an empty Action parameter value"; 
+
+    case ScriptOptsParserConstants.SOURCE_NAME: 
+    case ScriptOptsParserConstants.LINK_NAME: 
+      return "an upstream source node name";
+
+    case ScriptOptsParserConstants.LINK_ALL:
+      return "\"all\" a all-to-all link relationship";
+
+    case ScriptOptsParserConstants.OFFSET:
+      return "a one-to-one link relationship frame offset";
+      
+    case ScriptOptsParserConstants.PREFIX: 
+      return "a file sequence prefix";
+
+    case ScriptOptsParserConstants.SUFFIX2: 
+      return "a file sequence suffix";
+
+    case ScriptOptsParserConstants.FRAME_NUMBER:
+      return "a frame number";
+
+    case ScriptOptsParserConstants.FRAME_INDEX:
+      return "a frame index";
 
     default: 
       if(printLiteral) { 
