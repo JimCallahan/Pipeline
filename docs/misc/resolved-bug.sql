2c4916edb60f0b382cea4c88f4eab876ad9d73dd
Jim Callahan <jim@temerity.us>
2004-10-20 04:24:04 +0000

    latest

--- docs/misc/resolved-bug.sql
+++ docs/misc/resolved-bug.sql
@@ -7,4 +7,5 @@ SELECT bug_id, title, version_name, status_name FROM
   phpbt_bug,phpbt_version,phpbt_status 
   WHERE closed_in_version_id = phpbt_version.version_id 
   AND phpbt_status.status_id = phpbt_bug.status_id 
-  AND status_name = "Closed"
+  AND status_name = "Closed" 
+  AND version_name = "1.8.6"
