3e3e0985717f1be7e7ed37744c3d5645a45d933c
Jim Callahan <jim@temerity.us>
2004-10-25 17:46:19 +0000

    added formatInterval()

--- src/java/us/temerity/pipeline/Dates.java
+++ src/java/us/temerity/pipeline/Dates.java
@@ -1,4 +1,4 @@
-// $Id: Dates.java,v 1.1 2004/07/14 20:58:04 jim Exp $
+// $Id: Dates.java,v 1.2 2004/10/25 17:46:19 jim Exp $
 
 package us.temerity.pipeline;
 
@@ -40,6 +40,33 @@ class Dates
   ) 
   {
     return sFormat.format(date);
+  }
+
+  /**
+   * Convert a time interval into a standardized string representation.
+   */ 
+  public static String
+  formatInterval
+  (
+   long interval
+  ) 
+  {
+    long time = interval;
+
+    long h = time / 3600000L;
+    time -= h * 3600000L;
+    
+    long m = time / 60000L;
+    time -= m * 60000L;
+
+    double s = ((double) time) / 1000.0;
+    
+    if(interval > 3600000L) 
+      return String.format("%1$dh %2$dm %3$.1fs", h, m, s); 
+    else if(interval > 60000L)
+      return String.format("%1$dm %2$.1fs", m, s); 
+    else 
+      return String.format("%1$.1fs", s);       
   }
 
 
