b1ea6ab45bead0c277969df2c7393160b0f55289
Jim Callahan <jim@temerity.us>
2004-11-04 21:31:00 +0000

    initial revision [Bug #44]

---
+++ src/java/us/temerity/pipeline/plugin/v1_0_0/TouchAction.java
@@ -0,0 +1,103 @@
+// $Id: TouchAction.java,v 1.1 2004/11/04 21:31:00 jim Exp $
+
+package us.temerity.pipeline.plugin.v1_0_0;
+
+import us.temerity.pipeline.*; 
+
+import java.lang.*;
+import java.util.*;
+import java.io.*;
+
+/*------------------------------------------------------------------------------------------*/
+/*   T O U C H   A C T I O N                                                                */
+/*------------------------------------------------------------------------------------------*/
+
+/** 
+ * Simply touches all of the primary and secondary files associated with the target node.
+ */
+public
+class TouchAction
+  extends BaseAction
+{  
+  /*----------------------------------------------------------------------------------------*/
+  /*   C O N S T R U C T O R                                                                */
+  /*----------------------------------------------------------------------------------------*/
+  
+  public
+  TouchAction() 
+  {
+    super("Touch", new VersionID("1.0.0"), 
+	  "Touches all target file sequences.");
+  }
+
+
+
+  /*----------------------------------------------------------------------------------------*/
+  /*   A C T I O N                                                                          */
+  /*----------------------------------------------------------------------------------------*/
+
+  /**
+   * Construct a {@link SubProcessHeavy SubProcessHeavy} instance which when executed will 
+   * fulfill the given action agenda. <P> 
+   * 
+   * @param agenda
+   *   The agenda to be accomplished by the action.
+   * 
+   * @param outFile 
+   *   The file to which all STDOUT output is redirected.
+   * 
+   * @param errFile 
+   *   The file to which all STDERR output is redirected.
+   * 
+   * @return 
+   *   The SubProcess which will fulfill the agenda.
+   * 
+   * @throws PipelineException 
+   *   If unable to prepare a SubProcess due to illegal, missing or imcompatable 
+   *   information in the action agenda or a general failure of the prep method code.
+   */
+  public SubProcessHeavy
+  prep
+  (
+   ActionAgenda agenda,
+   File outFile, 
+   File errFile 
+  )
+    throws PipelineException
+  { 
+    makeTargetDir(agenda);
+
+    /* create the process to run the action */ 
+    try {
+      ArrayList<String> args = new ArrayList<String>();
+
+      for(File file : agenda.getPrimaryTarget().getFiles()) 
+	args.add(file.toString());
+      
+      for(FileSeq fseq : agenda.getSecondaryTargets()) {
+	for(File file : fseq.getFiles())
+	  args.add(file.toString());
+      }
+      
+      return new SubProcessHeavy
+	(agenda.getNodeID().getAuthor(), getName() + "-" + agenda.getJobID(), 
+	 "touch", args, agenda.getEnvironment(), agenda.getWorkingDir(), 
+	 outFile, errFile);
+    }
+    catch(Exception ex) {
+      throw new PipelineException
+	("Unable to generate the SubProcess to perform this Action!\n" +
+	 ex.getMessage());
+    }
+  }
+
+
+
+  /*----------------------------------------------------------------------------------------*/
+  /*   S T A T I C   I N T E R N A L S                                                      */
+  /*----------------------------------------------------------------------------------------*/
+
+  //private static final long serialVersionUID =
+
+}
+
