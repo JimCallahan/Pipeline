3ef413d47779c89be3aad9d56b6d6bc1834008bb
Jim Callahan <jim@temerity.us>
2004-11-06 23:33:26 +0000

    041106 customer profile

--- config/bootstrap.sh
+++ config/bootstrap.sh
@@ -9,7 +9,7 @@ pushd $plsrcdir
   sh autogen.sh
 popd
 
-plprofile=$plsrcdir/plconfig/customers/testing/041020
+plprofile=$plsrcdir/plconfig/customers/testing/041106
 
 pushd i686-pc-linux-gnu-dbg
   $plsrcdir/configure \
