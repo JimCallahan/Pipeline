3d265b54b5b9cffe3b43b198f68958ab412c5279
Jim Callahan <jim@temerity.us>
2005-01-15 02:51:16 +0000

    added underDevelopment(), getCatagory() and toString()

--- src/java/us/temerity/pipeline/BasePlugin.java
+++ src/java/us/temerity/pipeline/BasePlugin.java
@@ -1,4 +1,4 @@
-// $Id: BasePlugin.java,v 1.1 2004/09/08 18:34:51 jim Exp $
+// $Id: BasePlugin.java,v 1.2 2005/01/15 02:51:16 jim Exp $
 
 package us.temerity.pipeline;
 
@@ -12,7 +12,7 @@ import java.io.*;
 /*------------------------------------------------------------------------------------------*/
 
 /**
- * The superclass of all Pipelin plugins. <P>
+ * The superclass of all Pipeline plugins. <P>
  */
 public 
 class BasePlugin
@@ -71,6 +71,48 @@ class BasePlugin
   }
   
 
+  /*----------------------------------------------------------------------------------------*/
+
+  /**
+   * Mark this version of the plugin as currently being modified and tested by the 
+   * plugin developer.  <P> 
+   * 
+   * Plugins with this flag set can be dynamically reloaded by the <B>plpluginmgr</B>(1) 
+   * daemon and distributed to all running Pipeline programs. Subclasses should call this
+   * method in their constructor during the creation and testing phase of developement.
+   */ 
+  protected void 
+  underDevelopment()
+  {
+    pIsUnderDevelopment = true;
+  }
+
+  /** 
+   * Whether this version of the plugin in currently being modified and tested by the 
+   * plugin developer.  <P> 
+   * 
+   * Plugins with this flag set can be dynamically reloaded by the <B>plpluginmgr</B>(1) 
+   * daemon and distributed to all running Pipeline programs.
+   */ 
+  public boolean
+  isUnderDevelopment() 
+  {
+    return pIsUnderDevelopment;
+  }
+
+  
+  /*----------------------------------------------------------------------------------------*/
+
+  /**
+   * Get the name of the catagory of this plugin.
+   */ 
+  public String 
+  getPluginCatagory() 
+  {
+    return "Plugin";
+  }
+    
+
 
   /*----------------------------------------------------------------------------------------*/
   /*   O B J E C T   O V E R R I D E S                                                      */
@@ -95,6 +137,22 @@ class BasePlugin
     }
     return false;
   }
+
+  /**
+   * Generate a string representation of this plugin.
+   */ 
+  public String
+  toString()
+  {
+    return 
+     ("Name        : " + getName() + "\n" + 
+      "Version     : " + getVersionID() + "\n" + 
+      "Description : " + wordWrap(getDescription(), 14, 80) + "\n" + 
+      "Catagory    : " + getPluginCatagory() + "\n" +
+      "Status      : " + (isUnderDevelopment() ? "Under Development" : "Permanent") + "\n" + 
+      "Class       : " + getClass().getName());
+  }
+
 
 
   /*----------------------------------------------------------------------------------------*/
@@ -131,6 +189,75 @@ class BasePlugin
 
 
   /*----------------------------------------------------------------------------------------*/
+  /*   H E L P E R S                                                                        */
+  /*----------------------------------------------------------------------------------------*/
+   
+  /**
+   * Generate a string consisting the the given character repeated N number of times.
+   */ 
+  private String
+  repeat
+  (
+   char c,
+   int size
+  ) 
+  {
+    StringBuffer buf = new StringBuffer();
+    int wk;
+    for(wk=0; wk<size; wk++) 
+      buf.append(c);
+    return buf.toString();
+  }
+
+  /**
+   * Line wrap the given String at word boundries.
+   */ 
+  private String
+  wordWrap
+  (
+   String str,
+   int indent, 
+   int size
+  ) 
+  {
+    if(str.length() + indent < size) 
+      return str;
+
+    StringBuffer buf = new StringBuffer();
+    String words[] = str.split("\\p{Blank}");
+    int cnt = indent;
+    int wk;
+    for(wk=0; wk<words.length; wk++) {
+      int ws = words[wk].length();
+      if(ws > 0) {
+	if((size - cnt - ws) > 0) {
+	  buf.append(words[wk]);
+	  cnt += ws;
+	}
+	else {
+	  buf.append("\n" + repeat(' ', indent) + words[wk]);
+	  cnt = indent + ws;
+	}
+
+	if(wk < (words.length-1)) {
+	  if((size - cnt) > 0) {
+	    buf.append(' ');
+	    cnt++;
+	  }
+	  else {
+	    buf.append("\n" + repeat(' ', indent));
+	    cnt = indent;
+	  }
+	}
+      }
+    }
+
+    return buf.toString();
+  }  
+
+
+
+  /*----------------------------------------------------------------------------------------*/
   /*   S T A T I C   I N T E R N A L S                                                      */
   /*----------------------------------------------------------------------------------------*/
 
@@ -147,4 +274,10 @@ class BasePlugin
    */ 
   protected VersionID  pVersionID;
 
+  /**
+   * Whether this version of the plugin in currently being modified and tested by the 
+   * plugin developer.  Plugins with this flag set will be dynamically reloaded by the
+   * plugin manager daemon whenever new copies of the plugin version are registered.
+   */ 
+  private boolean  pIsUnderDevelopment;
 }
