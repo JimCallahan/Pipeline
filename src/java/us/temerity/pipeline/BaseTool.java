fbceeb33fe9aaa5d4cbe58a84ed8f38a076f2216
Jim Callahan <jim@temerity.us>
2005-02-20 20:53:57 +0000

    new multi-phase execution reorganization of methods

--- src/java/us/temerity/pipeline/BaseTool.java
+++ src/java/us/temerity/pipeline/BaseTool.java
@@ -1,4 +1,4 @@
-// $Id: BaseTool.java,v 1.2 2005/01/15 02:50:46 jim Exp $
+// $Id: BaseTool.java,v 1.3 2005/02/20 20:53:57 jim Exp $
 
 package us.temerity.pipeline;
 
@@ -57,8 +57,8 @@ class BaseTool
   ) 
   {
     super(name, vid, desc);
-    setHeaderText(name + ":");
   }
+
 
   
   /*----------------------------------------------------------------------------------------*/
@@ -76,81 +76,9 @@ class BaseTool
 
  
   /*----------------------------------------------------------------------------------------*/
- 
-  /**
-   * Whether the tool uses a dialog to interactively query the user for input.
-   */ 
-  public boolean
-  isInteractive() 
-  {
-    return (pDialogBody != null);
-  }
-
-
-  /*----------------------------------------------------------------------------------------*/
- 
-  /**
-   * Get the text displayed in the header of the tool dialog.
-   */ 
-  public String
-  getHeaderText()
-  {
-    return pHeaderText;
-  }
-
-  /**
-   * Set the text displayed in the header of the tool dialog. 
-   */ 
-  protected void 
-  setHeaderText
-  (
-   String text
-  ) 
-  {
-    pHeaderText = text; 
-  }
-
-
-  /*----------------------------------------------------------------------------------------*/
- 
-  /**
-   * Get UI components which make up the body of the dialog used to interactively query 
-   * the user for input. <P> 
-   * 
-   * @return 
-   *   The component containing the body of the dialog or <CODE>null</CODE> if 
-   *   the tool is non-interactive.
-   */ 
-  public JComponent
-  getDialogBody() 
-  {
-    return pDialogBody;
-  }
-
-  /**
-   * Set UI components which make up the body of the dialog used to interactively query 
-   * the user for input. <P> 
-   * 
-   * If the <CODE>body</CODE> argument is not <CODE>null</CODE>, the tool will become
-   * interactive and display a dialog when triggered.
-   * 
-   * @param body
-   *   The component containing the body of the dialog.
-   */ 
-  protected void 
-  setDialogBody
-  (
-   JComponent body
-  ) 
-  {
-    pDialogBody = body;
-  }
-
-
-  /*----------------------------------------------------------------------------------------*/
   
   /**
-   * Set the currently selected nodes. <P> 
+   * Initialize the execution phases with the currently node selection. <P> 
    * 
    * The primary selected node is the node under the mouse when the tool menu was shown 
    * (highlighted cyan).  The selected nodes include the primary selected node as well as 
@@ -167,7 +95,7 @@ class BaseTool
    *   The last known status of the selected nodes indexed by fully resolved node name.
    */
   public void 
-  setSelectedNodes
+  initExecution
   (
    String primary, 
    TreeMap<String,NodeStatus> selected
@@ -176,90 +104,49 @@ class BaseTool
     pPrimary  = primary;
     pSelected = selected;    
   }
-   
 
 
-  /*----------------------------------------------------------------------------------------*/
-  /*  O P S                                                                                 */
   /*----------------------------------------------------------------------------------------*/
 
   /** 
-   * Update the dialog components based on the current node selection. <P> 
+   * Create and show graphical user interface components to collect information from the 
+   * user to use as input in the next phase of execution for the tool. <P> 
    * 
-   * For interactive tools, this method is called when the user selects the menu item for 
-   * the tool.  The current node selections have already been updated before this method 
-   * is called.  <P> 
+   * This method is executed by the Swing event thread.  Modal dialogs can be created and 
+   * shown to collect user input.  The collected input should be validated and stored in 
+   * fields of the tool subclass by this method before returning. <P> 
    * 
-   * This method is executed in the Swing event thread immediately after the tool menu item
-   * is selected.  Interactive tool subclasses override this method to update any of the 
-   * components used in the body of the tool dialog before it is shown to the user (see 
-   * {@link #setDialogBody setDialogBody}). <P> 
+   * If this method return <CODE>null</CODE>, the execution of the tool will be immediately 
+   * aborted without any notification to the user. Otherwise, the 
+   * {@link #executePhase executePhase} method will be run in a seperate thread to perform 
+   * action based on the input collected in this method.  The returned <CODE>String</CODE> 
+   * will be used as the progress message shown in the operation status field at the bottom
+   * of the main Pipeline window. 
    * 
-   * This method is never called for non-interactive tools. <P> 
-   * 
-   * All exceptions, including runtime exceptions, thrown in this method should be caught
-   * and rethrown as a {@link PipelineException PipelineException}. <P>
+   * @return 
+   *   The phase progress message or <CODE>null</CODE> to abort early.
    * 
    * @throws PipelineException 
-   *   If unable to update the dialog components based on the current node selection. 
+   *   If unable to validate the given user input.
    */  
-  public synchronized void 
-  update() 
+  public synchronized String
+  collectPhaseInput() 
     throws PipelineException 
-  {}
+  {
+    return "...";
+  }
 
   /**
-   * Validate the node selections and any user input from the tool dialog components. <P> 
+   * Perform one phase in the execution of the tool. <P> 
    * 
-   * For non-interactive tools, this method is called when the user selects the menu item 
-   * for the tool since there is no dialog to be shown or confirmed. <P> 
+   * This method is executed in a seperate thread from the Swing event thread.  No user
+   * interface components should be created or queried by this method.  All information 
+   * used to control the behaviour of this method should be stored in fields of the tool 
+   * previously in the {@link #collectPhaseInput collectPhaseInput} method. <P> 
    * 
-   * For interactive tools, this method is called when the tool dialog is confirmed.  If the 
-   * dialog is closed without pressing the Confirm button or the Cancel button is pressed, 
-   * this method and the {@link #execute execute} method will be skipped. <P> 
-   * 
-   * This method is executed in the Swing event thread.  Interactive tool subclasses should 
-   * override this method to collect and validate any information needed from the dialog 
-   * body components (see {@link #setDialogBody setDialogBody}).  Any information relevant 
-   * to the execution of the tool (see {@link #execute execute}), should be stored in instance
-   * fields by this method. <P> 
-   * 
-   * Both interactive and non-interactive tools should also check whether the set of selected
-   * nodes are valid for the tool in this method. <P> 
-   * 
-   * All exceptions, including runtime exceptions, thrown in this method should be caught
-   * and rethrown as a {@link PipelineException PipelineException}. <P>
-   * 
-   * @throws PipelineException 
-   *   If unable to validate the current node selection or user input from dialog components.
-   */ 
-  public synchronized void 
-  validate() 
-    throws PipelineException 
-  {}
-
-  /**
-   * Execute the tool. <P> 
-   * 
-   * Subclasses should implement this method to perform the actual functionality of the tool 
-   * plugin. No user interface code should be executed from this method as it is run in its 
-   * own thread to avoid locking the UI during the execution of the tool.  Any information 
-   * needed by this method should be stored in the tool instance fields during execution 
-   * of the {@link #validate validate} method. <P> 
-   * 
-   * The connections to the <B>plmaster</B>(1) and <B>plqueuemgr</B>(1) daemons passed as
-   * arguments to this method can be used by the tool to perform any operation supported 
-   * by the Pipeline API.  In addition, local OS level processes and be run and monitored 
-   * using the {@link SubProcessLight SubProcessLight} and 
-   * {@link SubProcessHeavy SubProcessHeavy} classes. <P>
-   * 
-   * When writing tools, it should be kept in mind that users will be prevented from 
-   * performing other Pipeline operations in the UI until this method returns.  Tools with
-   * very long execution times are probably better suited to being standalone programs
-   * using the Pipeline API than interactive tools. <P> 
-   * 
-   * All exceptions, including runtime exceptions, thrown in this method should be caught
-   * and rethrown as a {@link PipelineException PipelineException}. <P>
+   * If this method returns <CODE>true</CODE>, another phase of execution will be initiated
+   * and user input will again be collected by the <CODE>collectPhaseInput</CODE> method.
+   * Otherwise, execution of the tool will end successfully.
    * 
    * @param mclient
    *   The network connection to the plmaster(1) daemon.
@@ -267,19 +154,24 @@ class BaseTool
    * @param qclient
    *   The network connection to the plqueuemgr(1) daemon.
    * 
+   * @return 
+   *   Whether to continue and collect user input for the next phase of the tool.
+   * 
    * @throws PipelineException 
-   *   If unable to sucessfully execute the tool.
+   *   If unable to sucessfully execute this phase of the tool.
    */ 
-  public synchronized void 
-  execute
+  public synchronized boolean
+  executePhase
   (
    MasterMgrClient mclient,
    QueueMgrClient qclient
   ) 
     throws PipelineException
-  {}
+  {
+    return false;
+  }
   
-  
+
 
   /*----------------------------------------------------------------------------------------*/
   /*   S T A T I C   I N T E R N A L S                                                      */
@@ -303,19 +195,6 @@ class BaseTool
    * The last known status of the selected nodes indexed by fully resolved node name.
    */ 
   protected TreeMap<String,NodeStatus>  pSelected; 
-
-
-  /*----------------------------------------------------------------------------------------*/
-
-  /**
-   * The text displayed in the header of the dialog.
-   */ 
-  private String  pHeaderText;
-
-  /**
-   * The components which make up the dialog body of interactive tools.
-   */ 
-  private JComponent  pDialogBody; 
 
 }
 
