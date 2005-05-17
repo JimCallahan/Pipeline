d96da001f3ab3f36be5a6adb41e80e91a56f4a52
Jim Callahan <jim@temerity.us>
2005-05-17 00:46:05 +0000

    latest

--- config/bootstrap.sh
+++ config/bootstrap.sh
@@ -9,7 +9,7 @@ pushd $plsrcdir
   sh autogen.sh
 popd
 
-plprofile=$plsrcdir/plconfig/customers/testing/050505
+plprofile=$plsrcdir/plconfig/customers/testing/050516
 
 pushd i686-pc-linux-gnu-dbg
   $plsrcdir/configure \
