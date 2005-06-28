3ddac5240ee02e0ebfd188c421568b1526ee094a
Jim Callahan <jim@temerity.us>
2005-06-28 20:57:41 +0000

    1.9.22 build

--- config/bootstrap-rhinofx-mac.sh
+++ config/bootstrap-rhinofx-mac.sh
@@ -3,21 +3,21 @@
 rm -rf i686-pc-linux-gnu-dbg
 mkdir  i686-pc-linux-gnu-dbg
 
-plsrcdir=$HOME/code/src/pipeline
+plsrcdir=$HOME/code-rhinofx/src/pipeline
 
 pushd $plsrcdir
   sh autogen.sh
 popd
 
-plprofile=$plsrcdir/plconfig/customers/testing/050622
+plprofile=$plsrcdir/plconfig/customers/rhinofx/050628
 
 pushd i686-pc-linux-gnu-dbg
   $plsrcdir/configure \
-    --enable-foundation \
+    --disable-foundation \
     --disable-opt \
-    --with-debug-base=45000 \
-    --with-prof-base=45100 \
+    --with-debug-base=43000 \
+    --with-prof-base=43100 \
     --with-crypto-app=$plsrcdir/plconfig \
-    --with-customer=testing \
+    --with-customer=rhinofx \
     --with-customer-profile=$plprofile
 popd
