566bc679ec1fe00b9b0264ad630a277d4c71ccf9
Jim Callahan <jim@temerity.us>
2006-01-05 23:25:04 +0000

    051220 site profile

--- config/bootstrap-MacGuff.sh
+++ config/bootstrap-MacGuff.sh
@@ -13,7 +13,7 @@ pushd $plsrcdir
   sh autogen.sh
 popd
 
-plprofile=$plsrcdir/plconfig/customers/MacGuff/051205
+plprofile=$plsrcdir/plconfig/customers/MacGuff/051220
 
 pushd i686-pc-linux-gnu-dbg
   CC=/usr/bin/gcc33 CXX=/usr/bin/g++33 \
