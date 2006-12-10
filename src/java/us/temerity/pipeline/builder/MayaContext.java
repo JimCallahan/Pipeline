2a7253beae6196a72e4431a2a7a2769462bd78aa
Jesse Clemens <jesse@temerity.us>
2006-12-10 22:59:51 +0000

    implemented [Bug #857] - Add Contexts for Builders

---
+++ src/java/us/temerity/pipeline/builder/MayaContext.java
@@ -0,0 +1,232 @@
+/*
+ * Created on Sep 12, 2006 Created by jesse For Use in us.temerity.pipeline.utils
+ */
+package us.temerity.pipeline.builder;
+
+import java.io.Serializable;
+import java.util.*;
+
+import us.temerity.pipeline.PipelineException;
+import us.temerity.pipeline.glue.*;
+
+/*------------------------------------------------------------------------------------------*/
+/*   M A Y A   C O N T E X T                                                                */
+/*------------------------------------------------------------------------------------------*/
+
+public 
+class MayaContext 
+  implements Comparable<MayaContext>, Glueable, Serializable
+{
+  /*----------------------------------------------------------------------------------------*/
+  /*   C O N S T R U C T O R                                                                */
+  /*----------------------------------------------------------------------------------------*/
+ 
+  /**
+   * Creates a new MayaContext
+   * 
+   * @param angularUnit
+   * 	The angular unit value
+   * @param linearUnit
+   * 	The linear unit value
+   * @param timeUnit
+   * 	The time unit value
+   * @throws PipelineException
+   */
+  public 
+  MayaContext
+  (
+    String angularUnit, 
+    String linearUnit, 
+    String timeUnit
+  )
+  throws PipelineException
+  {
+    if(!sAngularUnits.contains(angularUnit))
+      throw new PipelineException("Invalid value passed in for Angular Units.");
+    if(!sLinearUnits.contains(linearUnit))
+      throw new PipelineException("Invalid value passed in for Linear Units.");
+    if(!sTimeUnits.contains(timeUnit))
+      throw new PipelineException("Invalid value passed in for Time Units.");
+    pAngularUnit = angularUnit;
+    pLinearUnit = linearUnit;
+    pTimeUnit = timeUnit;
+  }
+  
+  /**
+   * Creates a new MayaContext
+   */
+  public 
+  MayaContext() 
+    throws PipelineException
+  {
+    this("degrees","centimeter", "Film (24 fps)" );
+  }
+
+  /*----------------------------------------------------------------------------------------*/
+  /*   A C C E S S                                                                          */
+  /*----------------------------------------------------------------------------------------*/
+
+  /**
+   * @return the AngularUnit
+   */
+  public String 
+  getAngularUnit()
+  {
+    return pAngularUnit;
+  }
+
+  /**
+   * @return the LinearUnit
+   */
+  public String 
+  getLinearUnit()
+  {
+    return pLinearUnit;
+  }
+
+  /**
+   * @return the TimeUnit
+   */
+  public String 
+  getTimeUnit()
+  {
+    return pTimeUnit;
+  }
+
+  public static Collection<String> 
+  getAngularUnits()
+  {
+    return Collections.unmodifiableCollection(sAngularUnits);
+  }
+
+  public static Collection<String> 
+  getTimeUnits()
+  {
+    return Collections.unmodifiableCollection(sTimeUnits);
+  }
+
+  public static Collection<String> 
+  getLinearUnits()
+  {
+    return Collections.unmodifiableCollection(sLinearUnits);
+  }
+
+  /*----------------------------------------------------------------------------------------*/
+  /*   O B J E C T   O V E R R I D E S                                                      */
+  /*----------------------------------------------------------------------------------------*/
+
+  public int 
+  compareTo
+  (
+    MayaContext that
+  )
+  {
+    int value = this.pAngularUnit.compareTo(that.getAngularUnit());
+    if(value == 0) {
+      value = this.pLinearUnit.compareTo(that.getLinearUnit());
+    }
+    if(value == 0) {
+      value = this.pTimeUnit.compareTo(that.getTimeUnit());
+    }
+    return value;
+  }
+
+  /*----------------------------------------------------------------------------------------*/
+  /*   G L U E A B L E                                                                      */
+  /*----------------------------------------------------------------------------------------*/
+
+  public void 
+  fromGlue
+  (
+    GlueDecoder decoder
+  ) 
+  throws GlueException
+  {
+    pTimeUnit = (String) decoder.decode("TimeUnit");
+    pAngularUnit = (String) decoder.decode("AngularUnit");
+    pLinearUnit = (String) decoder.decode("LinearUnit");
+  }
+
+  public void 
+  toGlue
+  (
+    GlueEncoder encoder
+  ) 
+  throws GlueException
+  {
+    encoder.encode("TimeUnit", pTimeUnit);
+    encoder.encode("AngularUnit", pAngularUnit);
+    encoder.encode("LinearUnit", pLinearUnit);
+  }
+
+  /*----------------------------------------------------------------------------------------*/
+  /*   S T A T I C   I N T E R N A L S                                                      */
+  /*----------------------------------------------------------------------------------------*/
+
+  private static final long serialVersionUID = 8729924189985169207L;
+
+  private static TreeSet<String> sAngularUnits = new TreeSet<String>();
+  private static TreeSet<String> sLinearUnits = new TreeSet<String>();
+  private static TreeSet<String> sTimeUnits = new TreeSet<String>();
+  {
+    sAngularUnits.add("degrees");
+    sAngularUnits.add("radians");
+
+    sLinearUnits.add("millimeter");
+    sLinearUnits.add("centimeter");
+    sLinearUnits.add("meter");
+    sLinearUnits.add("inch");
+    sLinearUnits.add("foot");
+    sLinearUnits.add("yard");
+
+    sTimeUnits.add("15 fps");
+    sTimeUnits.add("Film (24 fps)");
+    sTimeUnits.add("PAL (25 fps)");
+    sTimeUnits.add("NTSC (30 fps)");
+    sTimeUnits.add("Show (48 fps)");
+    sTimeUnits.add("PAL Field (50 fps)");
+    sTimeUnits.add("NTSC Field (60 fps)");
+    sTimeUnits.add("milliseconds");
+    sTimeUnits.add("seconds");
+    sTimeUnits.add("minutes");
+    sTimeUnits.add("hours");
+    sTimeUnits.add("2fps");
+    sTimeUnits.add("3fps");
+    sTimeUnits.add("4fps");
+    sTimeUnits.add("5fps");
+    sTimeUnits.add("6fps");
+    sTimeUnits.add("8fps");
+    sTimeUnits.add("10fps");
+    sTimeUnits.add("12fps");
+    sTimeUnits.add("16fps");
+    sTimeUnits.add("20fps");
+    sTimeUnits.add("40fps");
+    sTimeUnits.add("75fps");
+    sTimeUnits.add("80fps");
+    sTimeUnits.add("100fps");
+    sTimeUnits.add("120fps");
+    sTimeUnits.add("150fps");
+    sTimeUnits.add("200fps");
+    sTimeUnits.add("240fps");
+    sTimeUnits.add("250fps");
+    sTimeUnits.add("300fps");
+    sTimeUnits.add("375fps");
+    sTimeUnits.add("400fps");
+    sTimeUnits.add("500fps");
+    sTimeUnits.add("600fps");
+    sTimeUnits.add("750fps");
+    sTimeUnits.add("1200fps");
+    sTimeUnits.add("1500fps");
+    sTimeUnits.add("2000fps");
+    sTimeUnits.add("3000fps");
+    sTimeUnits.add("6000fps");
+  }
+
+  /*----------------------------------------------------------------------------------------*/
+  /*   I N T E R N A L S                                                                    */
+  /*----------------------------------------------------------------------------------------*/
+
+  private String pTimeUnit;
+  private String pAngularUnit;
+  private String pLinearUnit;
+}
---
+++ src/java/us/temerity/pipeline/builder/UtilContext.java
@@ -0,0 +1,146 @@
+/*
+ * Created on Aug 30, 2006 Created by jesse For Use in us.temerity.pipeline.utils
+ */
+package us.temerity.pipeline.builder;
+
+import java.io.Serializable;
+
+import us.temerity.pipeline.glue.*;
+
+/*------------------------------------------------------------------------------------------*/
+/*   U T I L   C O N T E X T                                                                */
+/*------------------------------------------------------------------------------------------*/
+
+public class UtilContext
+  implements Comparable<UtilContext>, Glueable, Serializable
+{
+  /*----------------------------------------------------------------------------------------*/
+  /*   C O N S T R U C T O R                                                                */
+  /*----------------------------------------------------------------------------------------*/
+
+  /**
+   * This constructor is required by the {@link GlueDecoder} to instantiate the class when
+   * encountered during the reading of GLUE format files and should not be called from
+   * user code.
+   */
+  public 
+  UtilContext()
+  {
+    pAuthor = null;
+    pView = null;
+    pToolset = null;
+  }
+
+  /**
+   * @param author
+   * @param view
+   * @param toolset
+   */
+  public 
+  UtilContext
+  (
+    String author, 
+    String view, 
+    String toolset
+  )
+  {
+    pAuthor = author;
+    pView = view;
+    pToolset = toolset;
+  }
+
+  /*----------------------------------------------------------------------------------------*/
+  /*   A C C E S S                                                                          */
+  /*----------------------------------------------------------------------------------------*/
+
+  /**
+   * @return the toolset
+   */
+  public String 
+  getToolset()
+  {
+    return pToolset;
+  }
+
+  /**
+   * @return the author
+   */
+  public String 
+  getAuthor()
+  {
+    return pAuthor;
+  }
+
+  /**
+   * @return the view
+   */
+  public String 
+  getView()
+  {
+    return pView;
+  }
+
+  /*----------------------------------------------------------------------------------------*/
+  /*   O B J E C T   O V E R R I D E S                                                      */
+  /*----------------------------------------------------------------------------------------*/
+
+  public int 
+  compareTo
+  (
+    UtilContext that
+  )
+  {
+    int value = this.pAuthor.compareTo(that.getAuthor());
+    if(value == 0) {
+      value = this.pView.compareTo(that.getView());
+    }
+    if(value == 0) {
+      value = this.pToolset.compareTo(that.getToolset());
+    }
+    return value;
+  }
+
+  /*----------------------------------------------------------------------------------------*/
+  /*   G L U E A B L E                                                                      */
+  /*----------------------------------------------------------------------------------------*/
+
+  public void 
+  fromGlue
+  (
+    GlueDecoder decoder
+  ) 
+  throws GlueException
+  {
+    pAuthor = (String) decoder.decode("Author");
+    pView = (String) decoder.decode("View");
+    pToolset = (String) decoder.decode("Toolset");
+  }
+
+  public void 
+  toGlue
+  (
+    GlueEncoder encoder
+  ) 
+  throws GlueException
+  {
+    encoder.encode("Author", pAuthor);
+    encoder.encode("View", pView);
+    encoder.encode("Toolset", pToolset);
+  }
+
+  /*----------------------------------------------------------------------------------------*/
+  /*   S T A T I C   I N T E R N A L S                                                      */
+  /*----------------------------------------------------------------------------------------*/
+
+  private static final long serialVersionUID = -6913346629352411368L;
+
+  /*----------------------------------------------------------------------------------------*/
+  /*   I N T E R N A L S                                                                    */
+  /*----------------------------------------------------------------------------------------*/
+
+  private String pAuthor;
+
+  private String pView;
+
+  private String pToolset;
+}
