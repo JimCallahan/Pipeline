1ca779b9c3ec338ffe775606d25b439c0475c9e2
Jim Callahan <jim@temerity.us>
2007-03-25 04:00:50 +0000

    finished

---
+++ src/java/us/temerity/pipeline/plugin/v2_2_1/GimpEditor.java
@@ -0,0 +1,156 @@
+// $Id: GimpEditor.java,v 1.1 2007/03/25 04:00:50 jim Exp $
+
+package us.temerity.pipeline.plugin.v2_2_1;
+
+import us.temerity.pipeline.*; 
+
+import java.util.*;
+import java.io.*;
+
+/*------------------------------------------------------------------------------------------*/
+/*   G I M P   E D I T O R                                                                  */
+/*------------------------------------------------------------------------------------------*/
+
+/**
+ * The GNU Image Manipuation Program. 
+ */
+public
+class GimpEditor
+  extends BaseAppleScriptEditor
+{  
+  /*----------------------------------------------------------------------------------------*/
+  /*   C O N S T R U C T O R                                                                */
+  /*----------------------------------------------------------------------------------------*/
+  
+  public
+  GimpEditor()
+  {
+    super("Gimp", new VersionID("2.2.1"), "Temerity", 
+	  "The GNU Image Manipulation Program.", 
+	  "Gimp");
+    
+    addSupport(OsType.MacOS);
+    addSupport(OsType.Windows);
+  }
+
+
+
+  /*----------------------------------------------------------------------------------------*/
+  /*   A C C E S S                                                                          */
+  /*----------------------------------------------------------------------------------------*/
+
+  /** 
+   * Gets the name of the editor executable.
+   */
+  public String
+  getProgram() 
+  {
+    if(PackageInfo.sOsType == OsType.Unix)  
+      return "gimp";
+    else if(PackageInfo.sOsType == OsType.Windows) 
+      return "gimp.exe"; 
+    return super.getProgram();
+  }
+
+
+
+  /*----------------------------------------------------------------------------------------*/
+  /*   A C T I O N                                                                          */
+  /*----------------------------------------------------------------------------------------*/
+
+  /** 
+   * Construct a {@link SubProcessLight} instance which when executed will launch an editor
+   * program to view the given file sequence as arguments. <P> 
+   * 
+   * @param author
+   *   The name of the user owning the files. 
+   * 
+   * @param fseq  
+   *   The file sequence to edit.
+   * 
+   * @param env  
+   *   The environment under which the editor is run.  
+   * 
+   * @param dir  
+   *   The working directory where the editor is run.
+   *
+   * @return 
+   *   The controlling <CODE>SubProcessLight</CODE> instance. 
+   * 
+   * @throws PipelineException
+   *   If unable to launch the editor.
+   * 
+   * @see SubProcessLight
+   */  
+  public SubProcessLight
+  prep
+  (
+   String author, 
+   FileSeq fseq,      
+   Map<String, String> env,      
+   File dir        
+  ) 
+    throws PipelineException
+  {
+    if(PackageInfo.sOsType == OsType.MacOS) 
+      return super.prep(author, fseq, env, dir);
+
+    ArrayList<String> args = new ArrayList<String>();
+    for(File file : fseq.getFiles()) 
+      args.add(file.getPath());
+    
+    return new SubProcessLight(author, getName(), getProgram(), args, env, dir);
+  }
+
+  /** 
+   * Launch the editor program (obtained with {@link #getName getName}) under the given 
+   * environmant with all of the files which comprise the given file sequence as 
+   * arguments. The environment <CODE>env</CODE> consists of a table of environmental 
+   * variable name/value pairs.  Typically, this environment is corresponds to a Toolset. <P>
+   * 
+   * Subclasses should override this method if more specialized behavior or different 
+   * command line arguments are needed in order to launch the editor for the given file 
+   * sequence.
+   * 
+   * @param fseq  
+   *   The file sequence to edit.
+   * 
+   * @param env  
+   *   The environment under which the editor is run.  
+   * 
+   * @param dir  
+   *   The working directory where the editor is run.
+   *
+   * @return 
+   *   The controlling <CODE>SubProcess</CODE> instance. 
+   * 
+   * @throws PipelineException
+   *   If unable to launch the editor.
+   */  
+  @SuppressWarnings("deprecation")
+  @Deprecated
+  public SubProcessLight
+  launch
+  (
+   FileSeq fseq,      
+   Map<String, String> env,      
+   File dir        
+  )   
+    throws PipelineException
+  {
+    throw new PipelineException
+      ("This launch() method should never be called since the prep() method returns " + 
+       "a non-null SubProcessLight instance!");
+  }
+
+
+
+  /*----------------------------------------------------------------------------------------*/
+  /*   S T A T I C   I N T E R N A L S                                                      */
+  /*----------------------------------------------------------------------------------------*/
+
+  private static final long serialVersionUID = 4059947840377971319L;
+
+}
+
+
