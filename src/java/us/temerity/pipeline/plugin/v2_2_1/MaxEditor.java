9f07d31dda69d729642a6bee968f5b28366b5152
Jim Callahan <jim@temerity.us>
2007-03-29 19:29:49 +0000

    now derrived from SimpleSingleEditor

--- src/java/us/temerity/pipeline/plugin/v2_2_1/MaxEditor.java
+++ src/java/us/temerity/pipeline/plugin/v2_2_1/MaxEditor.java
@@ -1,4 +1,4 @@
-// $Id: MaxEditor.java,v 1.1 2007/03/29 00:16:54 jim Exp $
+// $Id: MaxEditor.java,v 1.2 2007/03/29 19:29:49 jim Exp $
 
 package us.temerity.pipeline.plugin.v2_2_1;
 
@@ -16,7 +16,7 @@ import java.io.*;
  */
 public
 class MaxEditor
-  extends BaseEditor
+  extends SimpleSingleEditor
 {  
   /*----------------------------------------------------------------------------------------*/
   /*   C O N S T R U C T O R                                                                */
@@ -31,93 +31,11 @@ class MaxEditor
 
     removeSupport(OsType.Unix);
     addSupport(OsType.Windows);
+
+    underDevelopment();
   }
 
-
-  /*----------------------------------------------------------------------------------------*/
-  /*   A C T I O N                                                                          */
-  /*----------------------------------------------------------------------------------------*/
-
-  /** 
-   * Construct a {@link SubProcessLight} instance which when executed will launch an editor
-   * program to view the given file sequence as arguments. <P> 
-   * 
-   * This implementation returns <CODE>null</CODE> to fix the bug
-   * <A href="http://temerity.us/community/forums/viewtopic.php?t=933"><B>Inherited Editor 
-   * Prep Method</B></A>.
-   * 
-   * @param author
-   *   The name of the user owning the files.
-   * 
-   * @param fseq    
-   *   The file sequence to edit.
-   * 
-   * @param env  
-   *   The environment under which the editor is run.  
-   * 
-   * @param dir  
-   *   The working directory where the editor is run.
-   *
-   * @return 
-   *   The controlling <CODE>SubProcessLight</CODE> instance. 
-   * 
-   * @throws PipelineException
-   *   If unable to launch the editor.
-   * 
-   * @see SubProcessLight
-   */  
-  public SubProcessLight
-  prep
-  (
-   String author, 
-   FileSeq fseq,      
-   Map<String, String> env,      
-   File dir        
-  ) 
-    throws PipelineException
-  {
-    ArrayList<String> args = new ArrayList<String>();
-    args.add(fseq.getPath(0).toOsString());
-
-    return new SubProcessLight(author, getName(), getProgram(), args, env, dir);
-  }
-
-  /** 
-   * This implementation always throws a PipelineException, to insure that the {@link #prep
-   * prep} method is used for this Editor instead of this deprecated method.
-   * 
-   * @param fseq  
-   *   The file sequence to edit.
-   * 
-   * @param env  
-   *   The environment under which the editor is run.  
-   * 
-   * @param dir  
-   *   The working directory where the editor is run.
-   *
-   * @return 
-   *   The controlling <CODE>SubProcess</CODE> instance. 
-   * 
-   * @throws PipelineException
-   *   If unable to launch the editor.
-   */  
-  @SuppressWarnings("deprecation")
-  @Deprecated
-  public SubProcessLight
-  launch
-  (
-   FileSeq fseq,      
-   Map<String, String> env,      
-   File dir        
-  )   
-    throws PipelineException
-  {
-    throw new PipelineException
-      ("This launch() method should never be called since the prep() method returns " + 
-       "a non-null SubProcessLight instance!");
-  }
-
-
+  
 
   /*----------------------------------------------------------------------------------------*/
   /*   S T A T I C   I N T E R N A L S                                                      */
