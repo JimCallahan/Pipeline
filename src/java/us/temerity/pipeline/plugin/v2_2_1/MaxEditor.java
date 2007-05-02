7a7a6533bace3216e1ad5052bf464d01976c2262
Jim Callahan <jim@temerity.us>
2007-05-02 03:18:23 +0000

    mostly working

--- src/java/us/temerity/pipeline/plugin/v2_2_1/MaxEditor.java
+++ src/java/us/temerity/pipeline/plugin/v2_2_1/MaxEditor.java
@@ -1,4 +1,4 @@
-// $Id: MaxEditor.java,v 1.3 2007/03/29 19:37:36 jim Exp $
+// $Id: MaxEditor.java,v 1.4 2007/05/02 03:18:22 jim Exp $
 
 package us.temerity.pipeline.plugin.v2_2_1;
 
@@ -16,7 +16,7 @@ import java.io.*;
  */
 public
 class MaxEditor
-  extends SimpleSingleEditor
+  extends BaseEditor
 {  
   /*----------------------------------------------------------------------------------------*/
   /*   C O N S T R U C T O R                                                                */
@@ -33,7 +33,148 @@ class MaxEditor
     addSupport(OsType.Windows);
   }
 
-  
+
+
+  /*----------------------------------------------------------------------------------------*/
+  /*   A C T I O N                                                                          */
+  /*----------------------------------------------------------------------------------------*/
+
+  /** 
+   * Construct a {@link SubProcessLight} instance which when executed will launch an editor
+   * program to view the given file sequence as arguments. <P> 
+   * 
+   * The default implementation executes the editor program obtained with {@link #getProgram 
+   * getProgram} method under the given environment.  Subclasses should override this method 
+   * if more specialized behavior or different command line arguments are needed in order to 
+   * launch the editor for the given file sequence. <P> 
+   * 
+   * @param author
+   *   The name of the user owning the files. 
+   * 
+   * @param fseq  
+   *   The file sequence to edit.
+   * 
+   * @param env  
+   *   The environment under which the editor is run.  
+   * 
+   * @param dir  
+   *   The working directory where the editor is run.
+   *
+   * @return 
+   *   The controlling <CODE>SubProcessLight</CODE> instance. 
+   * 
+   * @throws PipelineException
+   *   If unable to launch the editor.
+   * 
+   * @see SubProcessLight
+   */  
+  public SubProcessLight
+  prep
+  (
+   String author, 
+   FileSeq fseq,      
+   Map<String, String> env,      
+   File dir        
+  ) 
+    throws PipelineException
+  {
+    if(!fseq.isSingle())
+      throw new PipelineException
+        ("The " + getName() + " Editor can only edit a single file at a time!");
+
+    try {
+      /* create a temporary MAXScript to setup the project paths */ 
+      File script = createTemp("ms");
+      try {
+        FileWriter out = new FileWriter(script); 
+        
+        Path current = new Path(dir);
+        if(!current.getName().equals("scenes"))
+          throw new PipelineException
+            ("The " + getName() + " Editor requires that the 3dsmax scene live in a " + 
+             "directory named (scenes) in order for the project paths to be set correctly!");
+
+        Path project = current.getParentPath();
+        out.write("pathConfig.setCurrentProjectFolder " + escPath(project) + "\n");
+
+        out.close();
+      } 
+      catch (IOException ex) {
+        throw new PipelineException
+          ("Unable to write temporary MAXScript file (" + script + ") required by " + 
+           "the " + getName() + " Editor!\n" + 
+           ex.getMessage());
+      }
+
+      /* command line arguments */ 
+      ArrayList<String> args = new ArrayList<String>();
+      args.add("-U");
+      args.add("MAXScript");
+      args.add(script.toString());
+      args.add(fseq.getPath(0).toOsString());
+      
+      return new SubProcessLight(author, getName(), getProgram(), args, env, dir);
+    }
+    catch(Exception ex) {
+      throw new PipelineException
+	("Unable to generate the SubProcess to launch this Editor!\n" +
+	 ex.getMessage());
+    }       
+  }
+
+  /** 
+   * This implementation always throws a PipelineException, to insure that the {@link #prep
+   * prep} method is used for this Editor instead of this deprecated method.
+   * 
+   * @param fseq  
+   *   The file sequence to edit.
+   * 
+   * @param env  
+   *   The environment under which the editor is run.  
+   * 
+   * @param dir  
+   *   The working directory where the editor is run.
+   *
+   * @return 
+   *   The controlling <CODE>SubProcessLight</CODE> instance. 
+   * 
+   * @throws PipelineException
+   *   If unable to launch the editor.
+   */  
+  @SuppressWarnings("deprecation")
+  @Deprecated
+  public SubProcessLight
+  launch
+  (
+   FileSeq fseq,      
+   Map<String, String> env,      
+   File dir        
+  ) 
+    throws PipelineException
+  {
+    throw new PipelineException
+      ("This method should never be called since the prep() method does not return (null)!");
+  }
+
+
+
+  /*----------------------------------------------------------------------------------------*/
+  /*   H E L P E R S                                                                        */
+  /*----------------------------------------------------------------------------------------*/
+
+  /**
+   * Generate a MAXScript string to represent the given file path.
+   */ 
+  private String
+  escPath
+  (
+   Path path
+  ) 
+  {
+    return ("\"" + path.toOsString().replaceAll("\\\\", "\\\\\\\\") + "\"");
+  }
+
+
 
   /*----------------------------------------------------------------------------------------*/
   /*   S T A T I C   I N T E R N A L S                                                      */
--- src/java/us/temerity/pipeline/plugin/v2_2_1/MaxScriptAction.java
+++ src/java/us/temerity/pipeline/plugin/v2_2_1/MaxScriptAction.java
@@ -1,4 +1,4 @@
-// $Id: MaxScriptAction.java,v 1.1 2007/04/30 08:20:58 jim Exp $
+// $Id: MaxScriptAction.java,v 1.2 2007/05/02 03:18:23 jim Exp $
 
 package us.temerity.pipeline.plugin.v2_2_1;
 
@@ -13,21 +13,48 @@ import java.io.*;
 /*   M A X   S C R I P T   A C T I O N                                                      */
 /*------------------------------------------------------------------------------------------*/
 
-/** 
- * Start 3d Studio Max and run a MAXScript.<P> 
+/**
+ * Loads a 3dsmax scene, evaluates a set of MAXScripts and optionally saves the modified 
+ * scene as the primary target file sequence of the node. <P> 
  * 
  * This action defines the following single valued parameters: <BR>
  * 
  * <DIV style="margin-left: 40px;">
- *   Max Script <BR>
+ *   Max Scene <BR>
  *   <DIV style="margin-left: 40px;">
- *     The source node who's primary file sequence is a single MAXScript. 
+ *     The source node which contains the 3dsmax scene file to load.  If this parameter is 
+ *     not set, then no scene will be loaded.  This may be useful in the case where the 
+ *     MAXScripts create the scene from scratch. <BR>
  *   </DIV> <BR>
+ * 
+ *   Use File Units <BR>
+ *   <DIV style="margin-left: 40px;">
+ *     Whether to use the Max Scene's unit scale when loading the scene, ignoring 
+ *     and replacing the System's unit scale.
+ *   </DIV><BR>
+ * 
+ *   Save Result <BR>
+ *   <DIV style="margin-left: 40px;">
+ *     Whether to save the 3dsmax scene as the node's primary target after all MAXScrips have 
+ *     been evaluated.  It may be desirable to not save the Maya scene when the MAXScripts
+ *     generate output during their evaluation. <BR>
+ *   </DIV><BR>
+ * </DIV> <P> 
+ * 
+ * This action defines the following per-source parameters: <BR>
+ * 
+ * <DIV style="margin-left: 40px;">
+ *   Order <BR>
+ *   <DIV style="margin-left: 40px;">
+ *     Each source node sequence which sets this parameter should contain a MAXScript. 
+ *     This parameter determines the order in which the MAXScripts evaluated.  If this 
+ *     parameter is not set for a source node file sequence, it will be ignored.
+ *   </DIV> 
  * </DIV> <P> 
  */
 public
 class MaxScriptAction
-  extends CommonActionUtils
+  extends MaxActionUtils
 {  
   /*----------------------------------------------------------------------------------------*/
   /*   C O N S T R U C T O R                                                                */
@@ -37,22 +64,91 @@ class MaxScriptAction
   MaxScriptAction() 
   {
     super("MaxScript", new VersionID("2.2.1"), "Temerity",
-	  "Start 3d Studio Max and run a MAXScript.");
+	  "Opens a 3d Studio Max scene, runs a set of MAXScript(s) and optionally saves " + 
+          "the resulting scene.");
     
     {
       ActionParam param = 
 	new LinkActionParam
-	(aMaxScript,
-	 "The source node who's primary file sequence is a single MAXScript.", 
+	(aMaxScene,
+	 "The source 3dsmax scene node.", 
 	 null);
       addSingleParam(param);
     }
 
-    addSupport(OsType.Windows); 
-    removeSupport(OsType.Unix); 
+    {
+      ActionParam param = 
+	new BooleanActionParam
+	(aUseFileUnits,
+	 "Whether to use the Max Scene's unit scale when loading the scene, ignoring " + 
+         "and replacing the System's unit scale.", 
+	 true);
+      addSingleParam(param);
+    }
+
+
+    {
+      ActionParam param = 
+	new BooleanActionParam
+	(aSaveResult,
+	 "Whether to save the post-MAXScript 3dsmax scene.",
+	 true);
+      addSingleParam(param);
+    }
+
+    {
+      LayoutGroup layout = new LayoutGroup(true);
+      layout.addEntry(aMaxScene);
+      layout.addEntry(aUseFileUnits);
+      layout.addSeparator();
+      layout.addEntry(aSaveResult);
+      
+      setSingleLayout(layout);
+    }
+
+    addSupport(OsType.Windows);  
+    removeSupport(OsType.Unix);
 
     underDevelopment();
   }
+
+
+  
+  /*----------------------------------------------------------------------------------------*/
+  /*   B A S E   A C T I O N   O V E R R I D E S                                            */
+  /*----------------------------------------------------------------------------------------*/
+  
+  /**
+   * Does this action support per-source parameters?  
+   */ 
+  public boolean 
+  supportsSourceParams()
+  {
+    return true;
+  }
+  
+  /**
+   * Get an initial set of action parameters associated with an upstream node. 
+   */ 
+  public TreeMap<String,ActionParam>
+  getInitialSourceParams()
+  {
+    TreeMap<String,ActionParam> params = new TreeMap<String,ActionParam>();
+    
+    {
+      ActionParam param = 
+	new IntegerActionParam
+	(aOrder, 
+	 "Each source node sequence which sets this parameter should contain a MAXScript. " +
+         "This parameter determines the order in which the MAXScripts evaluated.  If " +
+         "this parameter is not set for a source node file sequence, it will be ignored.", 
+	 100);
+      params.put(param.getName(), param);
+    }
+
+    return params;
+  }
+
 
 
   /*----------------------------------------------------------------------------------------*/
@@ -87,20 +183,120 @@ class MaxScriptAction
    File errFile 
   )
     throws PipelineException
-  { 
-    /* get the MAXScript script path */ 
-    Path scriptPath = getPrimarySourcePath(aMaxScript, agenda, "ms", "MAXScript (.ms) file");
-    if(scriptPath == null) 
-      throw new PipelineException("The MaxScript node was not specified!");
+  {
+    /* the source 3dsmax scene */ 
+    Path sourceScene = getMaxSceneSourcePath(aMaxScene, agenda);
 
-    /* command line arguments */ 
-    ArrayList<String> args = new ArrayList<String>();
-    args.add("-U");
-    args.add("MAXScript");
-    args.add(scriptPath.toOsString());
+    /* the target 3dsmax scene */
+    Path targetScene = null;
+    if(getSingleBooleanParamValue(aSaveResult))
+      targetScene = getMaxSceneTargetPath(agenda);
+    
+    /* the MAXScripts to evaluate */ 
+    MappedLinkedList<Integer,Path> sourceMsPaths = new MappedLinkedList<Integer,Path>();
+    {
+      for(String sname : agenda.getSourceNames()) {
+	if(hasSourceParams(sname)) {
+	  FileSeq fseq = agenda.getPrimarySource(sname);
+	  Integer order = (Integer) getSourceParamValue(sname, aOrder);
+	  addMsPaths(agenda, sname, fseq, order, sourceMsPaths);
+	}
+
+	for(FileSeq fseq : agenda.getSecondarySources(sname)) {
+	  FilePattern fpat = fseq.getFilePattern();
+	  if(hasSecondarySourceParams(sname, fpat)) {
+	    Integer order = (Integer) getSecondarySourceParamValue(sname, fpat, aOrder);
+            addMsPaths(agenda, sname, fseq, order, sourceMsPaths);
+	  }
+	}
+      }
+
+      if(sourceMsPaths.isEmpty()) 
+	throw new PipelineException
+	  ("No MAXScripts where specified using the per-source Order parameter!"); 
+    }
+
+    /* create a temporary MAXScript */ 
+    File script = createTemp(agenda, "ms");
+    try {
+      FileWriter out = new FileWriter(script); 
       
+      if(sourceScene != null) 
+        out.write("loadMaxFile " + escPath(sourceScene) + 
+                  " useFileUnits:" + getSingleBooleanParamValue(aUseFileUnits) + "\n"); 
+      
+      /* evaluate the MAXScripts */ 
+      if(!sourceMsPaths.isEmpty()) {
+	for(LinkedList<Path> paths : sourceMsPaths.values()) {
+	  for(Path spath : paths) 
+	    out.write("include " + escPath(spath) + "\n");
+	}
+	out.write("\n");
+      }
+      
+      /* save the resulting file */ 
+      if(targetScene != null) 
+	out.write("saveMAXFile " + escPath(targetScene) + "\n"); 
+
+      /* all done */
+      out.write("quitMax #noPrompt");
+
+      out.close();
+    } 
+    catch (IOException ex) {
+      throw new PipelineException
+	("Unable to write temporary MAXScript file (" + script + ") for Job " + 
+	 "(" + agenda.getJobID() + ")!\n" +
+	 ex.getMessage());
+    }
+
     /* create the process to run the action */
-    return createSubProcess(agenda, "3dsmax.exe", args, outFile, errFile);
+    return createMaxSubProcess(script, agenda, outFile, errFile);
+  }
+
+
+
+  /*----------------------------------------------------------------------------------------*/
+  /*   H E L P E R S                                                                        */
+  /*----------------------------------------------------------------------------------------*/
+
+  /**
+   * Generate a MAXScript string to represent the given file path.
+   */ 
+  private String
+  escPath
+  (
+   Path path
+  ) 
+  {
+    return ("\"" + path.toOsString().replaceAll("\\\\", "\\\\\\\\") + "\"");
+  }
+
+  /**
+   * A helper method for generating MAXScript filenames.
+   */ 
+  private void 
+  addMsPaths
+  (
+   ActionAgenda agenda, 
+   String sname, 
+   FileSeq fseq, 
+   Integer order, 
+   MappedLinkedList<Integer,Path> sourceMsPaths
+  )
+    throws PipelineException 
+  {
+    if(order == null) 
+      return;
+
+    String suffix = fseq.getFilePattern().getSuffix();
+    if(!fseq.isSingle() || (suffix == null) || !suffix.equals("ms"))
+      throw new PipelineException
+        ("The " + getName() + " Action requires that the file sequence (" + fseq + ") of " + 
+         "the source node (" + sname + ") selected for evaluation must be a single " + 
+         "MAXScript!"); 
+      
+    sourceMsPaths.put(order, getWorkingNodeFilePath(agenda, sname, fseq));
   }
 
 
@@ -111,7 +307,10 @@ class MaxScriptAction
 
   private static final long serialVersionUID = -7740936650142327601L;
 
-  public static final String aMaxScript = "MaxScript"; 
+  public static final String aMaxScene     = "MaxScene"; 
+  public static final String aUseFileUnits = "UseFileUnits"; 
+  public static final String aSaveResult   = "SaveResult"; 
+  public static final String aOrder        = "Order"; 
 
 }
 
