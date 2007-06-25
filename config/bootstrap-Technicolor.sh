dba2ee1b15b91316d1c42e389ae8b90998981bbe
Jim Callahan <jim@temerity.us>
2007-06-25 20:03:29 +0000

    initial revision

---
+++ config/bootstrap-Technicolor.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+../../src/pipeline/config/bootstrap.sh Technicolor 070625 43000 43100
+
---
+++ config/bootstrap-meeotch.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+../../src/pipeline/config/bootstrap.sh meeotch 070624 43000 43100
+
