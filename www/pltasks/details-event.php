44b1bb7299b6c85fc7237f6f4384fc54af4efea8
Jim Callahan <jim@temerity.us>
2007-08-08 03:21:20 +0000

    added thumbnails to each event panel

--- www/pltasks/details-event.php
+++ www/pltasks/details-event.php
@@ -39,7 +39,74 @@
       if($has_enodes) {
         print("\n" . 
               '<TD class="' . $row_color . '" rowspan="2" align="left" valign="top">' . "\n" .
-              '  <TABLE class="bg" width="100%" align="center" cellpadding="4" ' . 
+
+              '<SPAN class="gensmall"><SPAN class="redbold">' . "\n" .
+              '<TABLE class="' . $row_color .'" ' . 
+                     'cellpadding="0" cellspacing="0" border="0">' . "\n" .
+              '<TR><TD><DIV style="height: 8px;"></DIV></TD></TR>' . "\n");
+
+        $thumb_rows = array();
+        {
+          $max_width = 600; 
+          $total_width = 0; 
+
+          $thumbs = array();
+          foreach($e['thumbnails'] as $thumb) {
+            $image = 'prod/repository' . $thumb['thumb_path'];
+            list($width, $height, $type, $attr) = getimagesize($image);
+            
+            $tname = end(explode("/", $thumb['focus_name'])); 
+            $tvid  = $thumb['focus_version']; 
+
+            $script = viewFocusNodeScript($thumb['focus_name'], $tvid, $tname); 
+
+            $thumbs[] = array('name'    => $tname, 
+                              'vid'     => $tvid, 
+                              'script'  => $script, 
+                              'image'   => $image, 
+                              'width'   => $width, 
+                              'height'  => $height); 
+
+            $total_width += $width; 
+            if($total_width > $max_width) {
+              $thumb_rows[] = $thumbs;
+              $thumbs = array();
+              $total_width = 0;
+            }
+          }
+          if(count($thumbs) > 0) 
+            $thumb_rows[] = $thumbs;
+        }
+
+        foreach($thumb_rows as $thumbs) {
+          print('<TR>' . "\n" . 
+                '<TD><DIV style="width: 8px;"></DIV></TD>' . "\n"); 
+          
+          foreach($thumbs as $thumb) {
+            print('<TD class="' . $row_color .'" align="center" valign="middle">' . 
+                  '<A href="' . $thumb['script'] . '">' .
+                  '<IMG alt="" src="' . $temerity_root . 'loadpng.php?' . 
+                  'File=pltasks/' . $thumb['image'] . '&Bg=d3ceb8"></A>' . 
+                  '</TD>' . "\n" . 
+                  '<TD><DIV style="width: 12px;"></DIV></TD>' . "\n"); 
+          }
+          print('</TR>' . "\n\n");
+
+          print('<TR>' . "\n" . 
+                '<TD><DIV style="width: 8px;"></DIV></TD>' . "\n"); 
+
+          foreach($thumbs as $thumb) {
+            print('<TD align="center" valign="middle">' . $thumb['name'] . '</TD>' . "\n" .
+                  '<TD><DIV style="width: 12px;"></DIV></TD>' . "\n"); 
+          }
+          print('</TR>' . "\n\n");
+        }
+
+        print('<TR><TD><DIV style="height: 8px;"></DIV></TD></TR>' . "\n" .
+              '</TABLE>' . "\n" .
+              '</SPAN></SPAN>' . "\n");
+
+        print('  <TABLE class="bg" width="100%" align="center" cellpadding="4" ' . 
               'cellspacing="1" border="0">' . "\n"); 
 
         if($e['nodes'] != NULL) {
