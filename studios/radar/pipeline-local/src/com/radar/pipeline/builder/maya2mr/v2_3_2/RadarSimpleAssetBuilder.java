7aeb5f782c9fa14be1f071495aa1d822ea8a6c79
Jesse Clemens <jesse@temerity.us>
2007-08-22 07:50:51 +0000

    missing radar file

---
+++ studios/radar/pipeline-local/src/com/radar/pipeline/builder/maya2mr/v2_3_2/RadarSimpleAssetBuilder.java
@@ -0,0 +1,53 @@
+package com.radar.pipeline.builder.maya2mr.v2_3_2;
+
+import us.temerity.pipeline.*;
+import us.temerity.pipeline.builder.*;
+import us.temerity.pipeline.builder.maya2mr.v2_3_2.DefaultBuilderAnswers;
+import us.temerity.pipeline.builder.maya2mr.v2_3_2.SimpleAssetBuilder;
+
+/*------------------------------------------------------------------------------------------*/
+/*   R A D A R   S I M P L E   A S S E T   B U I L D E R                                    */
+/*------------------------------------------------------------------------------------------*/
+
+public 
+class RadarSimpleAssetBuilder
+  extends SimpleAssetBuilder
+{
+  /*----------------------------------------------------------------------------------------*/
+  /*   C O N S T R U C T O R                                                                */
+  /*----------------------------------------------------------------------------------------*/
+  public 
+  RadarSimpleAssetBuilder
+  (
+    MasterMgrClient mclient,
+    QueueMgrClient qclient,
+    BuilderInformation info
+  )
+    throws PipelineException
+  {
+    super
+      (mclient,
+       qclient,
+       new DefaultBuilderAnswers(mclient, qclient, UtilContext.getDefaultUtilContext(mclient)), 
+       new RadarAssetNames(mclient, qclient),
+       new RadarProjectNames(mclient, qclient),
+       info);
+  }
+  
+  /**
+   * Overriden to change the default editors.
+   */
+  protected void
+  setDefaultEditors()
+  {
+    setDefaultEditor(StageFunction.MayaScene.toString(), new PluginContext("MayaProject"));
+    setDefaultEditor(StageFunction.None.toString(), new PluginContext("JEdit", "Radar"));
+    setDefaultEditor(StageFunction.TextFile.toString(), new PluginContext("JEdit", "Radar"));
+    setDefaultEditor(StageFunction.ScriptFile.toString(), new PluginContext("JEdit", "Radar"));
+    setDefaultEditor(StageFunction.RenderedImage.toString(), new PluginContext("ImfDisp"));
+    setDefaultEditor(StageFunction.SourceImage.toString(), new PluginContext("Gimp"));
+    setDefaultEditor(StageFunction.MotionBuilderScene.toString(), new PluginContext("JEdit", "Radar"));
+  }
+  
+  private static final long serialVersionUID = 5771734469057685916L;
+}
