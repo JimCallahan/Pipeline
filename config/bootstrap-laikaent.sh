22d68fd41b98efd82cb5d884d8ed7bf99391b8e0
Jim Callahan <jim@temerity.us>
2007-10-17 14:51:23 +0000

    latest profile

--- config/bootstrap-laikaent.sh
+++ config/bootstrap-laikaent.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-../../src/pipeline/config/bootstrap.sh laikaent 070924 43000 43100 --enable-pllocal
+../../src/pipeline/config/bootstrap.sh laikaent 071015 43000 43100 --enable-pllocal
 
