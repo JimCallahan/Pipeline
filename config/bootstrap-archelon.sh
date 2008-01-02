a0cc61e4343e8c4e54360bf9768e098d53728929
Jim Callahan <jim@temerity.us>
2008-01-02 18:22:26 +0000

    initial revision

---
+++ config/bootstrap-archelon.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+../../src/pipeline/config/bootstrap-suse102.sh \
+  archelon 071229 43000 43100 --enable-pllocal-all
---
+++ config/bootstrap-suse102.sh
@@ -0,0 +1,80 @@
+#!/bin/sh
+
+customer=$1
+sitep=$2
+debug_base=$3
+prof_base=$4
+config_extra=$5
+
+echo "---------------------------------------------------------------------------------------"
+echo "  CONFIGURING: $HOSTNAME"
+echo "---------------------------------------------------------------------------------------"
+
+rm -rf i686-pc-linux-gnu-dbg
+mkdir  i686-pc-linux-gnu-dbg
+
+plsrcdir=$HOME/code-$customer/src/pipeline
+plprofile=$plsrcdir/plconfig/customers/$customer/$sitep
+
+pushd $plsrcdir
+  sh autogen.sh
+popd
+
+
+pushd i686-pc-linux-gnu-dbg
+  time \
+  CC="/usr/bin/gcc-4.1 -m32" \
+  CXX="/usr/bin/g++-4.1 -m32" \
+  $plsrcdir/configure \
+    --enable-foundation \
+    --disable-opt \
+    --with-debug-base=$debug_base \
+    --with-prof-base=$prof_base \
+    --with-crypto-app=$plsrcdir/plconfig \
+    --with-customer=$customer \
+    --with-customer-profile=$plprofile \
+    --with-shake=/base/apps/i686-pc-linux-gnu-opt/shake-v4.00.0607 \
+    $config_extra
+popd
+
+
+mac_support=`java -classpath $plsrcdir/plconfig CryptoApp $plprofile --lookup MacSupport`
+if [ "x$mac_support" == "xtrue" ]
+then
+  MAC_HOSTNAME=tadpole
+
+  echo "-------------------------------------------------------------------------------------"
+  echo "  UPDATING: $MAC_HOSTNAME"
+  echo "-------------------------------------------------------------------------------------"
+
+  time \
+  rsync -av --exclude-from=$plsrcdir/config/excluded --delete-excluded \
+    $plsrcdir/ $MAC_HOSTNAME:/Users/$USER/code-$customer/src/pipeline
+
+  time \
+  ssh $MAC_HOSTNAME "source .bash_profile; \
+                     cd code-$customer/build/pipeline; \
+                     ../../src/pipeline/config/bootstrap-mac.sh \
+                       $customer $sitep $debug_base $prof_base"
+fi
+
+
+win_support=`java -classpath $plsrcdir/plconfig CryptoApp $plprofile --lookup WinSupport`
+if [ "x$win_support" == "xtrue" ]
+then
+  WIN_HOSTNAME=lizard
+
+  echo "-------------------------------------------------------------------------------------"
+  echo "  UPDATING: $WIN_HOSTNAME"
+  echo "-------------------------------------------------------------------------------------"
+
+  time \
+  rsync -av --exclude-from=$plsrcdir/config/excluded --delete-excluded \
+    $plsrcdir/ $WIN_HOSTNAME:/home/$USER/code/src/pipeline
+
+  time \
+  ssh $WIN_HOSTNAME "source .bash_profile; \
+                     cd code/build/pipeline; \
+                     ../../src/pipeline/config/bootstrap-win.sh \
+                       $customer $sitep $debug_base $prof_base"
+fi
