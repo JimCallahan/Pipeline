d204e9eb2af9bbe11e8e174922e59df1a61e278d
Jim Callahan <jim@temerity.us>
2008-02-25 22:23:20 +0000

    reenabled windows

--- config/bootstrap-dimetrodon.sh
+++ config/bootstrap-dimetrodon.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 time ../../src/pipeline/config/bootstrap-suse10.1-32bit.sh \
-  dimetrodon 080207nw 45000 45100 --enable-pllocal-all
+  dimetrodon 080207 45000 45100 --enable-pllocal-all
 
