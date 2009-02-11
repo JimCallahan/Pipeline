4cc87e77a5507ba9722872c6a8ab1418d7d119c4
John Lee <jlee@temerity.us>
2009-02-11 16:19:54 +0000

    John's initial CVS commit!  Added my build scripts.

---
+++ config/bootstrap-jlee-suse10.3-64bit.sh
@@ -0,0 +1,137 @@
+#!/bin/sh
+
+if [ -f /etc/SuSE-release ]
+then 
+  suse_release=`head -1 /etc/SuSE-release`
+  if [ "$suse_release" != "openSUSE 10.3 (X86-64)" ]
+  then
+    echo "This configuration should only be run from a openSUSE-10.3 (64-bit) machine!"
+    echo "  Found: $suse_release"
+    exit 1
+  fi
+else
+  echo "Unable to determine if this is an openSUSE-10.3 (64-bit) machine!"
+  exit 1
+fi
+
+customer=$1
+sitep=$2
+debug_base=$3
+prof_base=$4
+config_extra=$5
+
+
+echo "---------------------------------------------------------------------------------------"
+echo "  AUTOGEN: $HOSTNAME"
+echo "---------------------------------------------------------------------------------------"
+
+plsrcdir=$HOME/code-$customer/src/pipeline
+plprofile=$plsrcdir/plconfig/customers/$customer/$sitep
+
+pushd $plsrcdir
+  time sh autogen.sh
+popd
+
+
+echo 
+echo "---------------------------------------------------------------------------------------"
+echo "  CONFIGURING (foundation): $HOSTNAME"
+echo "---------------------------------------------------------------------------------------"
+
+rm -rf debug
+mkdir  debug
+
+pushd debug
+  time \
+  JAVA_HOME=/usr/java/jdk1.6.0_10 \
+  PATH="$JAVA_HOME/bin:$PATH" \
+  CC="/usr/bin/gcc-4.2" \
+  CXX="/usr/bin/g++-4.2" \
+  $plsrcdir/configure \
+    --enable-foundation \
+    --enable-x86-subpass \
+    --disable-opt \
+    --with-debug-base=$debug_base \
+    --with-prof-base=$prof_base \
+    --with-crypto-app=$plsrcdir/plconfig \
+    --with-customer=$customer \
+    --with-customer-profile=$plprofile \
+    ${config_extra}
+popd
+
+
+echo 
+echo "---------------------------------------------------------------------------------------"
+echo "  CONFIGURING (native): $HOSTNAME"
+echo "---------------------------------------------------------------------------------------"
+
+rm -rf debug-native
+mkdir  debug-native
+
+pushd debug-native
+  time \
+  JAVA_HOME=/usr/java/jdk1.6.0_10 \
+  PATH="$JAVA_HOME/bin:$PATH" \
+  CC="/usr/bin/gcc-4.2" \
+  CXX="/usr/bin/g++-4.2" \
+  $plsrcdir/configure \
+    --disable-opt \
+    --with-target-archtype=x86 \
+    --with-debug-base=$debug_base \
+    --with-prof-base=$prof_base \
+    --with-crypto-app=$plsrcdir/plconfig \
+    --with-customer=$customer \
+    --with-customer-profile=$plprofile
+popd
+
+
+
+JAVA_HOME=/usr/java/jdk1.6.0_10
+PATH="$JAVA_HOME/bin:$PATH"
+
+mac_support=`java -classpath $plsrcdir/plconfig CryptoApp $plprofile --lookup MacSupport`
+if [ "x$mac_support" == "xtrue" ]
+then
+  MAC_HOSTNAME=bullfrog
+
+  echo 
+  echo "-------------------------------------------------------------------------------------"
+  echo "  UPDATING: $MAC_HOSTNAME"
+  echo "-------------------------------------------------------------------------------------"
+
+  time \
+  rsync -av --exclude-from=$plsrcdir/config/excluded --delete-excluded \
+    $plsrcdir/ $MAC_HOSTNAME:/Users/$USER/code-$customer/src/pipeline
+
+  #rsync -av --exclude-from=$plsrcdir/config/excluded --delete-excluded \
+  #  $plsrcdir/ -e ssh $MAC_HOSTNAME:/Users/$USER/code-$customer/src/pipeline
+
+
+  time \
+  ssh $MAC_HOSTNAME "source .bash_profile; \
+                     cd code-$customer/build; \
+                     ../src/pipeline/config/bootstrap-mac.sh \
+                       $customer $sitep $debug_base $prof_base"
+fi
+
+
+win_support=`java -classpath $plsrcdir/plconfig CryptoApp $plprofile --lookup WinSupport`
+if [ "x$win_support" == "xtrue" ]
+then
+  WIN_HOSTNAME=lizard
+
+  echo 
+  echo "-------------------------------------------------------------------------------------"
+  echo "  UPDATING: $WIN_HOSTNAME"
+  echo "-------------------------------------------------------------------------------------"
+
+  time \
+  rsync -av --exclude-from=$plsrcdir/config/excluded --delete-excluded \
+    $plsrcdir/ $WIN_HOSTNAME:/home/$USER/code/src/pipeline
+
+  time \
+  ssh $WIN_HOSTNAME "source .bash_profile; \
+                     cd code/build/pipeline; \
+                     ../../src/pipeline/config/bootstrap-win.sh \
+                       $customer $sitep $debug_base $prof_base"
+fi
---
+++ config/bootstrap-rodin.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+time ../src/pipeline/config/bootstrap-jlee-suse10.3-64bit.sh \
+  rodin 090208 43000 43100
+
+#time ~/code-$HOSTNAME/src/pipeline/config/bootstrap-jlee-suse10.3-64bit.sh rodin 090126 43000 43100
+
