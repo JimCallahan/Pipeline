30e9ecd25f81a46f05721d87359cd684be7c5340
John Lee <jlee@temerity.us>
2009-02-17 00:15:11 +0000

    Modified rodin's bootstrap script and added site-profiles.

--- config/bootstrap-rodin.sh
+++ config/bootstrap-rodin.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 time ../src/pipeline/config/bootstrap-jlee-suse10.3-64bit.sh \
-  rodin 090212 43000 43100
+  rodin 090213b 43000 43100
 
 #time ~/code-$HOSTNAME/src/pipeline/config/bootstrap-jlee-suse10.3-64bit.sh rodin 090126 43000 43100
 
