39185e2fc3e0bf17a8e52b9893ae26a1ba69f839
Jesse Clemens <jesse@temerity.us>
2009-03-30 14:36:25 +0000

    implemented 1894 - New List Data Structures.

---
+++ src/java/us/temerity/pipeline/ListSet.java
@@ -0,0 +1,139 @@
+// $Id: ListSet.java,v 1.1 2009/03/30 14:36:25 jesse Exp $
+
+package us.temerity.pipeline;
+
+import java.util.*;
+
+/*------------------------------------------------------------------------------------------*/
+/*   L I S T   S E T                                                                        */
+/*------------------------------------------------------------------------------------------*/
+
+/**
+ *  A {@link Set} that preserves its entries in a user specified order.
+ *  <p>
+ *  The ordering is based upon the order than things are added to the collection. 
+ *  <p>
+ *  This implementation is inherently slower than something like a {@link TreeSet}. 
+ *  The very nature of the keySet in this implementation requires an O(n) cost search
+ *  every time something is inserted or removed as opposed to the O(log(n)) provided by 
+ *  {@link TreeSet}.  Therefore this class should only be used where the ordering qualities 
+ *  it provides are necessary.
+ *  <p>
+ *  One other advantage of this class is that unlike the {@link TreeSet} it does not require
+ *  its dataType to implement {@link Comparable}.
+ *  
+ *  @param <E>
+ *    The type of elements maintained by this set.
+ *    
+ *  @see Set
+ *  @see TreeSet
+ */
+public 
+class ListSet<E>
+  extends AbstractSet<E>
+  implements Set<E>
+{
+  /*----------------------------------------------------------------------------------------*/
+  /*   C O N S T R U C T O R                                                                */
+  /*----------------------------------------------------------------------------------------*/
+
+  /**
+   * Constructs a new, empty set.
+   */
+  public 
+  ListSet() 
+  {
+    pMap = new ListMap<E,Object>();
+  }
+
+  /**
+   * Constructs a new set containing the elements in the specified
+   * collection. 
+   *
+   * @param c
+   *   The collection whose elements are to be placed into this set
+   */
+  public ListSet
+  (
+    Collection<? extends E> c
+  ) 
+  {
+    pMap = new ListMap<E,Object>();
+    addAll(c);
+  }
+
+  @Override
+  public boolean 
+  add
+  (
+    E e
+  )
+  {
+    return pMap.put(e, PRESENT) == null;
+  }
+
+  @Override
+  public void 
+  clear()
+  {
+    pMap.clear();
+  }
+
+  @Override
+  public boolean 
+  contains
+  (
+    Object o
+  )
+  {
+    return pMap.containsKey(o);
+  }
+
+  @Override
+  public boolean 
+  isEmpty()
+  {
+    return pMap.isEmpty();
+  }
+
+  @Override
+  public Iterator<E> 
+  iterator()
+  {
+    return pMap.keySet().iterator();
+  }
+
+  @Override
+  public boolean 
+  remove
+  (
+    Object o
+  )
+  {
+    return pMap.remove(o) == PRESENT;
+  }
+
+  @Override
+  public int 
+  size()
+  {
+    return pMap.size();
+  }
+
+
+  
+  /*----------------------------------------------------------------------------------------*/
+  /*   S T A T I C   I N T E R N A L S                                                      */
+  /*----------------------------------------------------------------------------------------*/
+
+  // Dummy value to associate with an Object in the backing Map
+  private static final Object PRESENT = new Object();
+
+
+  
+  /*----------------------------------------------------------------------------------------*/
+  /*  I N T E R N A L S                                                                     */
+  /*----------------------------------------------------------------------------------------*/
+
+  private transient ListMap<E,Object> pMap;
+}
---
+++ src/java/us/temerity/pipeline/MappedListSet.java
@@ -0,0 +1,188 @@
+// $Id: MappedListSet.java,v 1.1 2009/03/30 14:36:25 jesse Exp $
+
+package us.temerity.pipeline;
+
+import java.util.*;
+
+/*------------------------------------------------------------------------------------------*/
+/*   M A P P E D   L I S T   S E T                                                          */
+/*------------------------------------------------------------------------------------------*/
+
+/**
+ *  A {@link TreeMap} that contains {@link ListSet ListSets}.
+ *  <p>
+ *  This class does automatic lazy creation of the sets when a value is added to them.
+ *  
+ * @param <K> 
+ *   The type of keys maintained by this map.
+ *   
+ * @param <V> 
+ *   The type of the sets that are the mapped values.
+ */
+public 
+class MappedListSet<K, V>
+  extends TreeMap<K, ListSet<V>>
+{
+  /*----------------------------------------------------------------------------------------*/
+  /*   C O N S T R U C T O R                                                                */
+  /*----------------------------------------------------------------------------------------*/
+  
+  /**
+   * Constructs an empty map.
+   */
+  public MappedListSet()
+  {
+    super();
+  }
+  
+  /**
+   * Deep copy constructor.
+   */
+  public MappedListSet
+  (
+    MappedListSet<K, V> mset
+  )
+  {
+    super();
+    putAll(mset);
+  }
+  
+  /**
+   * Constructor to convert a {@link MappedSet} in to a {@link MappedListSet}.
+   * 
+   * @param mset 
+   *   The MappedSet to be converted.
+   */
+  public MappedListSet
+  (
+    MappedSet<K, V> mset
+  )
+  {
+    super();
+    for (K key : mset.keySet()) {
+     TreeSet<V> val = mset.get(key);
+     if (val == null)
+       put(key);
+     else
+       put(key, new ListSet<V>(val));
+    }
+  }
+  
+  /**
+   * Inverts a {@link Map} into a {@link MappedListSet}.
+   * <p>
+   * The values in the map will be converted into the keys of the new
+   * MappedSet.  So if you had a Map with the following values
+   * <ul>
+   *   <li>Boy 1
+   *   <li>Girl 2
+   *   <li>Horse 1
+   *   <li>Duck 4
+   * </ul>
+   * Then the new MappedSet would look like.
+   * <ul>
+   *   <li> 1 Boy, Horse
+   *   <li> 2 Girl
+   *   <li> 4 Duck
+   * </ul>
+   */
+  public MappedListSet
+  (
+    Map<V, K> map
+  )
+  {
+    super();
+    for (V key : map.keySet())
+      put(map.get(key), key);
+  }
+
+  
+  /*----------------------------------------------------------------------------------------*/
+  /*   A C C E S S                                                                          */
+  /*----------------------------------------------------------------------------------------*/
+
+  /**
+   * Associates a value with the specified key.
+   * 
+   * @param key
+   *   The first key.
+   * 
+   * @param value
+   *   The value to insert.
+   */ 
+  public void 
+  put
+  (
+    K key, 
+    V value
+  )
+  {
+    if(key == null) 
+      throw new IllegalArgumentException("The key cannot be (null)!");
+    
+    ListSet<V> set = this.get(key);
+    if(set == null) {
+      set = new ListSet<V>();
+      this.put(key, set);
+    }
+    set.add(value);
+  }
+  
+  /**
+   * Associates a <code>null</code> value with the specified key.
+   *
+   * @param key
+   *   The first key.
+   *
+   * @return
+   *   Any value that might have existed in the now <code>null</code> mapping.
+   */
+  public ListSet<V> 
+  put
+  (
+    K key
+  )
+  {
+    if(key == null)
+      throw new IllegalArgumentException("The key cannot be (null)!");
+   
+    return super.put(key, null);
+  }
+  
+  /**
+   * Inserts all the of key/value mappings from the given map into this map.
+   * 
+   * @param mset
+   *   The map to insert.
+   */ 
+  public void
+  putAll
+  (
+   MappedListSet<K,V> mset
+  )  
+  {
+    for(K a : mset.keySet()) 
+      for (V each : mset.get(a))
+	put(a, each);
+  }
+  
+  /**
+   * Type-safe getter method.
+   */
+  public ListSet<V> 
+  get
+  (
+    K key
+  )
+  {
+    return super.get(key);
+  }
+  
+  
+  
+  /*----------------------------------------------------------------------------------------*/
+  /*   S T A T I C   I N T E R N A L S                                                      */
+  /*----------------------------------------------------------------------------------------*/
+  
+  private static final long serialVersionUID = 2070454439080216811L;
+}
