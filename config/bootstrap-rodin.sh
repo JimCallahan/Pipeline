e56d4da781a70a1a5ddd0480d305f3bbef2241e4
John Lee <jlee@temerity.us>
2009-06-02 18:14:38 +0000

    Updated rodin build files.

--- config/bootstrap-rodin.sh
+++ config/bootstrap-rodin.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 time ../src/pipeline/config/bootstrap-jlee-suse10.3-64bit.sh \
-  rodin 090402 43000 43100 --enable-pllocal-all
+  rodin 090512 43000 43100 --enable-pllocal-all
 
 #time ~/code-$HOSTNAME/src/pipeline/config/bootstrap-jlee-suse10.3-64bit.sh rodin 090126 43000 43100
 
