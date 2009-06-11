2dde65d484f36ee97df680a886dd2a27b3fa395a
Jesse Clemens <jesse@temerity.us>
2009-06-11 05:38:37 +0000

    working version of the DisneyBuild automator

--- src/java/us/temerity/pipeline/jesse/DisneyBuild.java
+++ src/java/us/temerity/pipeline/jesse/DisneyBuild.java
@@ -1,10 +1,11 @@
-// $Id: DisneyBuild.java,v 1.1 2009/05/23 04:47:35 jesse Exp $
+// $Id: DisneyBuild.java,v 1.2 2009/06/11 05:38:37 jesse Exp $
 
 package us.temerity.pipeline.jesse;
 
 import java.util.*;
 
 import us.temerity.pipeline.*;
+import us.temerity.pipeline.LogMgr.*;
 import us.temerity.pipeline.builder.*;
 import us.temerity.pipeline.builder.v2_4_3.*;
 
@@ -14,10 +15,17 @@ public class DisneyBuild
 
   /**
    * @param args
+   * @throws PipelineException 
    */
-  public static void main(
-    String[] args)
+  public static void 
+  main
+  (
+    String[] args
+  ) 
+  throws PipelineException
   {
+    PluginMgrClient.init();
+    
     TreeSet<String> nodeList = new TreeSet<String>();
     {
       nodeList.add("/templates/PROJECT/prod/SUB/SEQ/SHOT/anim/edit/SEQ_SHOT_anim");
@@ -31,6 +39,10 @@ public class DisneyBuild
       nodeList.add("/templates/PROJECT/prod/SUB/SEQ/SHOT/lgt/render/pass/SEQ_SHOT_PASS");
     }
 
+    
+    Integer seqNum = 0;
+    if (args.length > 0)
+      seqNum = Integer.valueOf(args[0]);
     ArrayList<Thread> threads = new ArrayList<Thread>();
     
     ArrayList<String[]> assets = new ArrayList<String[]>();
@@ -99,17 +111,19 @@ public class DisneyBuild
     int assetEnd = assets.size() - 1;
     int passEnd = passes.size() - 1;
     
+    LogMgr.getInstance().setLevel(Kind.Ops, Level.Fine);
+    
     int iters = 20;
     for (int i = 0; i < iters; i++) {
       
       TreeMap<String, String> replacements = new TreeMap<String, String>();
       replacements.put("templates", "projects");
-      replacements.put("PROJECT", "disney");
+      replacements.put("PROJECT_TEST", "disney");
       
       
       {
         String project = projects.get(random(0, 1));
-        int seq = random(0, 20);
+        int seq = random(seqNum, seqNum + 10);
         int shot = random(0, 500);
         while (shots.get(project, seq, shot) != null) {
           shot = random(0, 500);
@@ -174,7 +188,7 @@ public class DisneyBuild
       
       TreeMap<String, FrameRange> ranges =
         new TreeMap<String, FrameRange>();
-      FrameRange range = new FrameRange(1, random(50, 400), 1);
+      FrameRange range = new FrameRange(1, random(400, 800), 1);
       ranges.put("range", range);
       
       TemplateBuildInfo info = new TemplateBuildInfo();
@@ -227,13 +241,16 @@ public class DisneyBuild
       
       try {
         TemplateBuilder builder = 
-          new TemplateBuilder(mclient, qclient, information, pTemplateInfo, pStringReplacements, pContexts, pFrameRanges, pAoeModes);
+          new TemplateBuilder(mclient, qclient, information, pTemplateInfo, pStringReplacements, pContexts, pFrameRanges, pAoeModes, new TreeMap<String, TemplateExternalData>(), null);
         builder.run();
       }
       catch (PipelineException ex) {
         ex.printStackTrace();
       }
-      
+      finally {
+        mclient.disconnect();
+        qclient.disconnect();
+      }
     }
     
     private TemplateBuildInfo pTemplateInfo;
