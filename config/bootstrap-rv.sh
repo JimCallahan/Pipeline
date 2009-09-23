06b5198eaa399b1c8994f3d7d3ffeec4a572bfe3
Jim Callahan <jim@temerity.us>
2009-09-23 22:18:10 +0000

    latest site-profile

--- config/bootstrap-rv.sh
+++ config/bootstrap-rv.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 time ../../src/pipeline/config/bootstrap-centos5.1-64bit.sh \
-  rv 090715 43000 43100 \
-  --with-auth-licenses=10 \
+  rv 090921 43000 43100 \
+  --with-auth-licenses=15 \
   --with-extra-licenses=10
