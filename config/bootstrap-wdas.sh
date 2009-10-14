cadffd27451f867788081aafe06e5b29cc395376
Jim Callahan <jim@temerity.us>
2009-10-14 02:38:07 +0000

    latest site-profile...

--- config/bootstrap-wdas.sh
+++ config/bootstrap-wdas.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 time ../../src/pipeline/config/bootstrap-centos5.3-64bit.sh \
-  wdas 090721 43000 43100 \
+  wdas 091013 43000 43100 \
   --with-auth-licenses=45 \
   --with-extra-licenses=15
