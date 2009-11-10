95e94ef320a9179e6b1e380946cdfde2ac9fbacc
Jim Callahan <jim@temerity.us>
2009-11-10 21:27:55 +0000

    fixed site-profile

--- config/bootstrap-alt.sh
+++ config/bootstrap-alt.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 time ../../src/pipeline/config/bootstrap-suse11.0-64bit.sh \
-  alt 091108 45000 45100 \
+  alt 091110 45000 45100 \
   --enable-pllocal-all \
   --with-auth-licenses=2 \
   --with-extra-licenses=1
