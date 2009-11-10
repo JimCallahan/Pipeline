1c6ab7e9f93bf4e8c209827318ecb07caad10442
Jim Callahan <jim@temerity.us>
2009-11-10 20:38:46 +0000

    initial revision

---
+++ config/bootstrap-alt.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+time ../../src/pipeline/config/bootstrap-suse11.0-64bit.sh \
+  alt 091108 45000 45100 \
+  --enable-pllocal-all \
+  --with-auth-licenses=2 \
+  --with-extra-licenses=1
+
