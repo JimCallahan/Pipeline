da5ac25c34de58cc22b7d0a58374dfac7554237b
Jim Callahan <jim@temerity.us>
2009-11-17 21:07:34 +0000

    latest site-profile, changed to centos5.2 from suse10.2

--- config/bootstrap-lumiere.sh
+++ config/bootstrap-lumiere.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-time ../../src/pipeline/config/bootstrap-suse10.2-64bit.sh \
-  lumiere 091111 43000 43100 \
+time ../../src/pipeline/config/bootstrap-centos5.2-64bit.sh \
+  lumiere 091117 43000 43100 \
   --with-auth-licenses=35 \
   --with-extra-licenses=15
