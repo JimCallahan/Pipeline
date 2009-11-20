d4b91e54f9d24f1e6078ed72b7a24b3d66200f8f
Jim Callahan <jim@temerity.us>
2009-11-20 21:37:05 +0000

    updated site-profile

--- config/bootstrap-lumiere.sh
+++ config/bootstrap-lumiere.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 time ../../src/pipeline/config/bootstrap-centos5.2-64bit.sh \
-  lumiere 091117 43000 43100 \
+  lumiere 091120 43000 43100 \
   --with-auth-licenses=35 \
   --with-extra-licenses=15
