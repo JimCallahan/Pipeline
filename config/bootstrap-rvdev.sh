4fe8efd9a4229b8dbf77bf661f67a5f25d9271e7
Jim Callahan <jim@temerity.us>
2009-12-07 17:02:10 +0000

    latest siite-profile

--- config/bootstrap-rvdev.sh
+++ config/bootstrap-rvdev.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 time ../../src/pipeline/config/bootstrap-centos5.1-64bit.sh \
-  rvdev 091202 43000 43100 \
+  rvdev 091207 43000 43100 \
   --with-auth-licenses=15 \
   --with-extra-licenses=10
