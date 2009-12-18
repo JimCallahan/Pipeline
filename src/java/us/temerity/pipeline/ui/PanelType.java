bb921bca6432516ed8ffe059eea8a1f29bb1dd23
Jim Callahan <jim@temerity.us>
2009-12-18 19:55:56 +0000

    better comments, added toTitle()

--- src/java/us/temerity/pipeline/ui/PanelType.java
+++ src/java/us/temerity/pipeline/ui/PanelType.java
@@ -1,13 +1,15 @@
-// $Id: PanelType.java,v 1.1 2009/12/18 08:44:26 jim Exp $
+// $Id: PanelType.java,v 1.2 2009/12/18 19:55:56 jim Exp $
 
 package us.temerity.pipeline.ui;
+
+import java.util.ArrayList; 
 
 /*------------------------------------------------------------------------------------------*/
 /*   P A N E L   T Y P E                                                                    */
 /*------------------------------------------------------------------------------------------*/
 
-/**
- * The types of panels contained in the top-level windows of plui(1).
+/**x
+ * The types of panels contained in the top-level windows of plui(1).<P> 
  */ 
 public 
 enum PanelType
@@ -29,5 +31,62 @@ enum PanelType
   JobViewer, 
   JobDetails, 
 
-  None;
+  Empty;
+
+  
+  /*----------------------------------------------------------------------------------------*/
+  /*   A C C E S S                                                                          */
+  /*----------------------------------------------------------------------------------------*/
+
+  /**
+   * Get the list of all possible states.
+   */ 
+  public static ArrayList<PanelType>
+  all() 
+  {
+    PanelType values[] = values();
+    ArrayList<PanelType> all = new ArrayList<PanelType>(values.length);
+    int wk;
+    for(wk=0; wk<values.length; wk++)
+      all.add(values[wk]);
+    return all;
+  }
+
+
+
+  /*----------------------------------------------------------------------------------------*/
+  /*   C O N V E R S I O N                                                                  */
+  /*----------------------------------------------------------------------------------------*/
+
+  /**
+   * Convert to a more human friendly string representation.
+   */ 
+  public String
+  toTitle() 
+  {
+    return sTitles[ordinal()];
+  }
+
+
+
+  /*----------------------------------------------------------------------------------------*/
+  /*   S T A T I C   I N T E R N A L S                                                      */
+  /*----------------------------------------------------------------------------------------*/
+
+  private static String sTitles[] = {
+    "Node Browser", 
+    "Node Viewer", 
+    "Node Details", 
+    "Node Files", 
+    "Node Links", 
+    "Node History", 
+    "Node Annotations", 
+    "Queue Stats", 
+    "Queue Servers", 
+    "Queue Slots", 
+    "Job Browser",
+    "Job Viewer", 
+    "Job Details", 
+    "Empty"
+  };
 }
