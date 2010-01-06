62d0aeda06bcd860296e6550f3c851b259f17fec
Jim Callahan <jim@temerity.us>
2010-01-06 23:02:11 +0000

    latest site-profile...

--- config/bootstrap-rvldn.sh
+++ config/bootstrap-rvldn.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 time ../../src/pipeline/config/bootstrap-centos5.1-64bit.sh \
-  rvldn 091208 43000 43100 \
+  rvldn 100106
+   43000 43100 \
   --with-auth-licenses=10 \
   --with-extra-licenses=10
 
