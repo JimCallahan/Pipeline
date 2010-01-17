577ce79ac0e172b116f699412280e36d4f616044
Jim Callahan <jim@temerity.us>
2010-01-17 09:09:11 +0000

    fixed merge errors

--- config/bootstrap-rvldn.sh
+++ config/bootstrap-rvldn.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 
 time ../../src/pipeline/config/bootstrap-centos5.1-64bit.sh \
-  rvldn 100116
-   43000 43100 \
+  rvldn 100116 43000 43100 \
   --with-auth-licenses=10 \
   --with-extra-licenses=10
 
