271380a60263818ef0d7c60c48f8d5a4eb2cc89b
Jim Callahan <jim@temerity.us>
2010-01-28 16:27:26 -0800

    latest SCEACSG site-profile

--- config/bootstrap-SCEACSG.sh
+++ config/bootstrap-SCEACSG.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 time ../../src/pipeline/config/bootstrap-centos5.1-64bit.sh \
-  SCEACSG 091116 43000 43100 \
+  SCEACSG 100125 43000 43100 \
   --with-auth-licenses=45 \
   --with-extra-licenses=15
