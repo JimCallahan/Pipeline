afd7cd703c8bae7b0bd9f44c2986dce3a57ff053
Jim Callahan <jim@temerity.us>
2010-02-13 21:42:24 -0800

    implemented 2468 - Add Human Friendly Formatting for Timestamps

--- src/java/us/temerity/pipeline/TimeStamps.java
+++ src/java/us/temerity/pipeline/TimeStamps.java
@@ -26,7 +26,7 @@ class TimeStamps
   public static long
   now()
   {
-    return ((System.currentTimeMillis() / 1000L) * 1000L);
+    return ((System.currentTimeMillis() / sSecond) * sSecond);
   } 
 
 
@@ -68,7 +68,7 @@ class TimeStamps
 
   /**
    * Convert a timestamp (milliseconds since midnight, January 1, 1970 UTC) into a 
-   * standardized string representation.
+   * standardized string representation.<P> 
    * 
    * The generated string will have the format 
    * (<I>YYYY</I>-<I>MM</I>-<I>DD</I> <I>hh</I>:<I>mm</I>:<I>ss</I>) where: 
@@ -93,8 +93,11 @@ class TimeStamps
     return sFormat.format(new Date(stamp)); 
   }
 
+
+  /*----------------------------------------------------------------------------------------*/
+
   /**
-   * Convert a time interval into a standardized string representation.
+   * Convert a time interval into a standardized string representation.<P> 
    */ 
   public static String
   formatInterval
@@ -107,21 +110,103 @@ class TimeStamps
 
     long time = interval;
 
-    long h = time / 3600000L;
-    time -= h * 3600000L;
-    
-    long m = time / 60000L;
-    time -= m * 60000L;
+    long y = time / sYear; 
+    time -= y / sYear; 
 
-    double s = ((double) time) / 1000.0;
+    long n = time / sMonth; 
+    time -= n / sMonth; 
+
+    long d = time / sDay; 
+    time -= d * sDay;
+
+    long h = time / sHour;
+    time -= h * sHour;
     
-    if(interval > 3600000L) 
+    long m = time / sMinute;
+    time -= m * sMinute;
+
+    double s = ((double) time) / ((double) sSecond); 
+
+    if(interval > sYear) 
+      return String.format("%1$dyrs %2$dmons %3$ddays %4$dh %5$dm %6$.1fs", y, n, d, h, m, s); 
+    else if(interval > sMonth) 
+      return String.format("%1$dmons %2$ddays %3$dh %4$dm %5$.1fs", n, d, h, m, s); 
+    else if(interval > sDay) 
+      return String.format("%1$ddays %2$dh %3$dm %4$.1fs", d, h, m, s); 
+    else if(interval > sHour) 
       return String.format("%1$dh %2$dm %3$.1fs", h, m, s); 
-    else if(interval > 60000L)
+    else if(interval > sMinute)
       return String.format("%1$dm %2$.1fs", m, s); 
     else 
       return String.format("%1$.1fs", s);       
   }
+
+
+  /*----------------------------------------------------------------------------------------*/
+
+  /**
+   * Convert a timestamp (milliseconds since midnight, January 1, 1970 UTC) into a 
+   * relative time expressed in a human friendly string representation. <P> 
+   * 
+   * The timestamp passed is assumed to be in the past with respect to the current time.
+   */ 
+  public static String
+  formatHumanRelative
+  (
+   Long stamp
+  ) 
+  {
+    return formatHumanRelative(stamp, System.currentTimeMillis()); 
+  }
+
+  /**
+   * Convert a timestamp (milliseconds since midnight, January 1, 1970 UTC) into a 
+   * relative time expressed in a human friendly string representation. <P> 
+   * 
+   * The timestamp passed is assumed to be in the past with respect to the given 
+   * <CODE>now<CODE> time.
+   * 
+   * @param now
+   *   The timestamp (milliseconds since midnight, January 1, 1970 UTC) of the current time.
+   */ 
+  public static String
+  formatHumanRelative
+  (
+   Long stamp, 
+   long now
+  ) 
+  {
+    if(stamp == null) 
+      return "-";
+
+    long delta = now - stamp;
+    if(delta < 0) 
+      return "in the future!";
+    
+    long years = delta / sYear; 
+    if(years > 1) 
+      return (years + " years ago.");
+
+    long months = delta / sMonth; 
+    if(months > 1)
+      return (months + " months ago."); 
+
+    long days = delta / sDay;
+    if(days > 1) 
+      return (days + " days ago."); 
+    
+    long hours = (now - stamp) / sHour;
+    if(hours > 1) 
+      return (hours + " hours ago."); 
+
+    long mins = (now - stamp) / sMinute;
+    if(mins > 1) 
+      return (mins + " mins ago."); 
+
+    long secs = (now - stamp) / sSecond;
+    return (secs + " sec ago."); 
+  }
+
 
 
   /*----------------------------------------------------------------------------------------*/
@@ -132,6 +217,18 @@ class TimeStamps
    * A standardized date formatter.
    */ 
   private static SimpleDateFormat  sFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss"); 
+
+  
+  /**
+   * Useful time intervals in milliseconds.
+   */ 
+  private static final long sSecond  = 1000L;
+  private static final long sMinute  = 60000L;
+  private static final long sHour    = 3600000L;
+  private static final long sDay     = 86400000L;
+  private static final long sWeek    = 604800000L;
+  private static final long sMonth   = 2628000000L;
+  private static final long sYear    = 31536000000L;
 
 }
 
