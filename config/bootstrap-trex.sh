83b3731d663c6ff7876a3c05aac70760a6826cad
Jim Callahan <jim@temerity.us>
2010-03-02 07:00:48 -0800

    updated trex site-profile for plconfig-3.9.0

--- config/bootstrap-trex.sh
+++ config/bootstrap-trex.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 time ../../src/pipeline/config/bootstrap-suse11.0-64bit.sh \
-  trex 100112 45000 45100 \
+  trex 100302 45000 45100 \
   --enable-pllocal-all \
   --with-auth-licenses=2 \
   --with-extra-licenses=1
