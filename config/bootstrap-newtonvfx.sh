bc13921b500dc4f1e390ae30d186ce9615fbadd6
Jim Callahan <jim@temerity.us>
2010-04-14 09:40:08 -0700

    latest newtonfx site-profile

--- config/bootstrap-newtonvfx.sh
+++ config/bootstrap-newtonvfx.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 time ../../src/pipeline/config/bootstrap-suse11.2-64bit.sh \
-  newtonvfx 100330 43000 43100 \
+  newtonvfx 100412 43000 43100 \
   --with-auth-licenses=1 \
   --with-extra-licenses=4
