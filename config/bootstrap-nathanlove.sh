568de2352704bcb98f98808729ebdd299d48dcfa
Jim Callahan <jim@temerity.us>
2010-07-16 20:05:15 -0700

    latest nathanlove profile

--- config/bootstrap-nathanlove.sh
+++ config/bootstrap-nathanlove.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 time ../../src/pipeline/config/bootstrap-suse11.2-64bit.sh \
-  nathanlove 100530 43000 43100 \
+  nathanlove 100714 43000 43100 \
   --with-auth-licenses=25 \
   --with-extra-licenses=15
