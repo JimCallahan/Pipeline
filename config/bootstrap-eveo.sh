4812d00d4568881dfe1c05b3bc9f8c09e1542d15
Jim Callahan <jim@temerity.us>
2010-08-10 14:53:42 -0700

    latest eveo profile

--- config/bootstrap-eveo.sh
+++ config/bootstrap-eveo.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 time ../../src/pipeline/config/bootstrap-suse11.2-64bit.sh \
-  eveo 100611 43000 43100 \
+  eveo 100810 43000 43100 \
   --with-auth-licenses=1 \
   --with-extra-licenses=4
