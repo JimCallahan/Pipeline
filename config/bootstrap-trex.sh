884207f0924db51437140003332d22e9033253f7
Jim Callahan <jim@temerity.us>
2011-01-16 17:28:42 -0500

    latest profile

--- config/bootstrap-trex.sh
+++ config/bootstrap-trex.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-time ../../src/pipeline/config/bootstrap-suse11.0-64bit.sh \
-  trex 100416 45000 45100 \
+time ../../src/pipeline/config/bootstrap-suse11.3-64bit.sh \
+  trex 101108 45000 45100 \
   --enable-pllocal-all \
   --with-auth-licenses=2 \
   --with-extra-licenses=1
