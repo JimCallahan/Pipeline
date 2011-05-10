68c6fe2677e02526da867e64a3ff315ae0d5595b
Jim Callahan <jim@temerity.us>
2011-05-10 08:47:02 -0400

    updated SCEA build to use new profile and CentOS 5.3 VM

--- config/bootstrap-SCEACSG.sh
+++ config/bootstrap-SCEACSG.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-time ../../src/pipeline/config/bootstrap-centos5.1-64bit.sh \
-  SCEACSG 100819 43000 43100 \
+time ../../src/pipeline/config/bootstrap-centos5.3-64bit.sh \
+  SCEACSG 110504 43000 43100 \
   --with-auth-licenses=45 \
   --with-extra-licenses=15
