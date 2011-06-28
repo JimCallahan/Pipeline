65c0bcbf3a6fcd5f0756905bd035b6a0df06df12
Jim Callahan <jim@temerity.us>
2011-06-28 17:04:03 -0400

    latest SCEA site-profile

--- config/bootstrap-SCEACSG.sh
+++ config/bootstrap-SCEACSG.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 time ../../src/pipeline/config/bootstrap-centos5.3-64bit.sh \
-  SCEACSG 110523 43000 43100 \
+  SCEACSG 110620 43000 43100 \
   --with-auth-licenses=45 \
   --with-extra-licenses=15
