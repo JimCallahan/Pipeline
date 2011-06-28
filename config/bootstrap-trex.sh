1ebb0f6b6f810d6ad73d78ddbd7e5af6a218f271
Jim Callahan <jim@temerity.us>
2011-06-28 17:04:29 -0400

    latest trex site-profile

--- config/bootstrap-trex.sh
+++ config/bootstrap-trex.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 time ../../src/pipeline/config/bootstrap-suse11.3-64bit.sh \
-  trex 110503 45000 45100 \
+  trex 110627 45000 45100 \
   --enable-pllocal-all \
   --with-auth-licenses=2 \
   --with-extra-licenses=1
