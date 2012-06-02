a91c4e11a76947c225cc03325acd810dec4bd95b
Jim Callahan <jamesmichaelcallahan@gmail.com>
2012-06-01 21:48:23 -0600

    new sony profile

--- config/bootstrap-SCEACSG.sh
+++ config/bootstrap-SCEACSG.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 time ../../src/pipeline/config/bootstrap-centos5.3-64bit.sh \
-  SCEACSG 110525 43000 43100 \
+  SCEACSG 120530 43000 43100 \
   --with-auth-licenses=45 \
   --with-extra-licenses=15
