deab54915ad7ec3c68c85831bc0447707bb301a4
Jim Callahan <jamesmichaelcallahan@gmail.com>
2012-06-01 21:51:13 -0600

    new nathanlove profile

--- config/bootstrap-nathanlove.sh
+++ config/bootstrap-nathanlove.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 time ../../src/pipeline/config/bootstrap-suse11.2-64bit.sh \
-  nathanlove 120222 43000 43100 \
+  nathanlove 120529 43000 43100 \
   --with-auth-licenses=25 \
   --with-extra-licenses=15
