e7bfbba4dec3456b0d631519362b05a61eb15100
Jim Callahan <jim@darius.(none)>
2012-06-02 03:16:52 -0400

    latest profile

--- config/bootstrap-SCEACSG.sh
+++ config/bootstrap-SCEACSG.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-time ../../src/pipeline/config/bootstrap-centos5.3-64bit.sh \
-  SCEACSG 110525 43000 43100 \
+time ../../src/pipeline/config/bootstrap-centos5.8-64bit.sh \
+  SCEACSG 120525 43000 43100 \
   --with-auth-licenses=45 \
   --with-extra-licenses=15
